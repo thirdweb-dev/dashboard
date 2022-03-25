f18d311145392dfb3bf728d5e14aa5a980673f45
Nacho Iacovino <nacho.iacovino@gmail.com>
2022-03-25 14:17:37 +0100

    Fetch gas price from etherscan

--- components/gas-estimator/index.tsx
+++ components/gas-estimator/index.tsx
@@ -1,29 +1,8 @@
-import {
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-  ModalProps,
-  Text,
-} from "@chakra-ui/react";
-import {
-  ContractType,
-  Edition,
-  EditionDrop,
-  NFTCollection,
-  NFTDrop,
-  Split,
-  Token,
-  Vote,
-} from "@thirdweb-dev/sdk";
-import {
-  CONTRACT_TYPE_NAME_MAP,
-  GasEstimatorMap,
-  GasPrice,
-} from "constants/mappings";
+import { Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, ModalProps, Text } from "@chakra-ui/react";
+import { ContractType, Edition, EditionDrop, NFTCollection, NFTDrop, Split, Token, Vote } from "@thirdweb-dev/sdk";
+import { CONTRACT_TYPE_NAME_MAP, GasEstimatorMap, GasPrice } from "constants/mappings";
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
 
 interface GasEstimatorBoxProps {
   contractType: ContractType;
@@ -33,14 +12,42 @@ const GasEstimatorBox: React.FC<GasEstimatorBoxProps> = ({ contractType }) => {
   const { deployContract, setClaimPhase, batchUpload, mint, claim }: GasPrice =
     GasEstimatorMap[contractType];
 
+  const [gasPrice, setGasPrice] = useState<number>(30);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(
+        `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${process.env.NEXT_PUBLIC_ETHERSCAN_KEY}`,
+      );
+      const data = await res.json();
+      console.log(data.result.ProposeGasPrice);
+      const gasNumber = Number(data.result.ProposeGasPrice);
+      setGasPrice(gasNumber ? gasNumber : 30);
+    };
+    fetchData();
+  }, []);
+
+  const formatPrice = (price: number) => {
+    if (price) {
+      return `${ethers.utils.formatUnits(
+        `${(gasPrice as number) * price}`,
+        "gwei",
+      )} ETH`;
+    }
+  };
+
   return (
     <div>
       {CONTRACT_TYPE_NAME_MAP[contractType]}
-      {deployContract && <Text>Deploy Contract: {deployContract}</Text>}
-      {setClaimPhase && <Text>Set Claim Phase: {setClaimPhase}</Text>}
-      {batchUpload && <Text>Batch Upload: {batchUpload}</Text>}
-      {mint && <Text>Mint: {mint}</Text>}
-      {claim && <Text>Claim: {claim}</Text>}
+      {deployContract && (
+        <Text>Deploy Contract: {formatPrice(deployContract)}</Text>
+      )}
+      {setClaimPhase && (
+        <Text>Set Claim Phase: {formatPrice(setClaimPhase)}</Text>
+      )}
+      {batchUpload && <Text>Batch Upload: {formatPrice(batchUpload)}</Text>}
+      {mint && <Text>Mint: {formatPrice(mint)}</Text>}
+      {claim && <Text>Claim: {formatPrice(claim)}</Text>}
     </div>
   );
 };
@@ -58,10 +65,10 @@ export const GasEstimatorModal: React.FC<Partial<GasEstimatorModalProps>> = ({
         <ModalHeader>Gas Estimator</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <GasEstimatorBox contractType={NFTCollection.contractType} />
-          <GasEstimatorBox contractType={Edition.contractType} />
           <GasEstimatorBox contractType={NFTDrop.contractType} />
           <GasEstimatorBox contractType={EditionDrop.contractType} />
+          <GasEstimatorBox contractType={NFTCollection.contractType} />
+          <GasEstimatorBox contractType={Edition.contractType} />
           <GasEstimatorBox contractType={Token.contractType} />
           <GasEstimatorBox contractType={Split.contractType} />
           <GasEstimatorBox contractType={Vote.contractType} />
