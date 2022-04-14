de904d786621c272da8ddaf12b1eeffa6c12800a
Nacho Iacovino <nacho.iacovino@gmail.com>
2022-04-14 14:08:06 +0200

    Use get total count instead

--- components/batch-upload/NftDropBatchUpload.tsx
+++ components/batch-upload/NftDropBatchUpload.tsx
@@ -1,7 +1,7 @@
 import { BatchTable } from "./BatchTable";
 import { SelectReveal } from "./SelectReveal";
 import { UploadStep } from "./UploadStep";
-import { useNFTDropSupply } from "@3rdweb-sdk/react/hooks/useNFTDrop";
+import { useGetTotalCount } from "@3rdweb-sdk/react/hooks/useGetAll";
 import {
   Box,
   Container,
@@ -39,7 +39,7 @@ export const NftDropBatchUpload: React.FC<NftDropBatchUploadProps> = ({
   isOpen,
   onClose,
 }) => {
-  const { data: supplyData } = useNFTDropSupply(contract?.getAddress());
+  const nextTokenIdToMint = useGetTotalCount(contract);
   const [step, setStep] = useState(0);
   const [csvData, setCSVData] = useState<Papa.ParseResult<CSVData>>();
   const [jsonData, setJsonData] = useState<any>();
@@ -142,7 +142,7 @@ export const NftDropBatchUpload: React.FC<NftDropBatchUploadProps> = ({
                       <BatchTable
                         portalRef={paginationPortalRef}
                         data={mergedData}
-                        nextTokenIdToMint={supplyData?.totalSupply}
+                        nextTokenIdToMint={nextTokenIdToMint.data?.toNumber()}
                       />
                     ) : (
                       <UploadStep
