6488b03630cbc7bae4764511a2405232eb8b234e
Jonas Daniels <jonas.daniels@outlook.com>
2022-04-29 16:21:34 -0700

    halfway

--- components/buttons/MismatchButton.tsx
+++ components/buttons/MismatchButton.tsx
@@ -1,8 +1,19 @@
 import { ConnectWallet, useWeb3 } from "@3rdweb-sdk/react";
-import { Tooltip } from "@chakra-ui/react";
-import { useNetworkMismatch } from "@thirdweb-dev/react";
-import React from "react";
-import { Button, ButtonProps } from "tw-components";
+import { Flex, Icon, Tooltip } from "@chakra-ui/react";
+import {
+  useDesiredChainId,
+  useNetwork,
+  useNetworkMismatch,
+} from "@thirdweb-dev/react";
+import React, { useCallback } from "react";
+import { AiOutlineWarning } from "react-icons/ai";
+import { VscDebugDisconnect } from "react-icons/vsc";
+import { Button, ButtonProps, Heading, Text } from "tw-components";
+import {
+  SUPPORTED_CHAIN_ID,
+  SupportedChainIdToNetworkMap,
+  getNetworkFromChainId,
+} from "utils/network";
 
 export const MismatchButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ children, isDisabled, onClick, loadingText, type, ...props }, ref) => {
@@ -14,12 +25,25 @@ export const MismatchButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
       );
     }
 
+    if (networksMismatch) {
+    }
+
     return (
       <Tooltip
+        portalProps={{ containerRef: ref || undefined }}
+        bg="backgroundHighlight"
+        p={4}
+        borderRadius="xl"
+        borderWidth="1px"
+        borderColor="borderColor"
+        mx={6}
         isDisabled={!networksMismatch}
-        label="To do this please switch yout wallet network."
+        label={<MismatchNotice />}
         hasArrow
         shouldWrapChildren
+        // isOpen={stayOpen}
+        closeDelay={500}
+        pointerEvents="all"
       >
         <Button
           w="full"
@@ -38,3 +62,70 @@ export const MismatchButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
 );
 
 MismatchButton.displayName = "MismatchButton";
+
+const MismatchNotice: React.VFC = () => {
+  const { chainId, getNetworkMetadata } = useWeb3();
+  const activeChainId = useDesiredChainId();
+  const signerChainId = chainId as SUPPORTED_CHAIN_ID;
+  const [network, switchNetwork] = useNetwork();
+
+  const actuallyCanAttemptSwitch = !!switchNetwork;
+
+  const signerNetworkIsSupported =
+    signerChainId in SupportedChainIdToNetworkMap;
+
+  const walletNetwork = (
+    signerNetworkIsSupported
+      ? getNetworkFromChainId(signerChainId)
+      : getNetworkMetadata(signerChainId as unknown as number).chainName
+  )
+    .split("")
+    .map((s, idx) => (idx === 0 ? s.toUpperCase() : s))
+    .join("");
+
+  const twNetwork = getNetworkFromChainId(activeChainId)
+    .split("")
+    .map((s, idx) => (idx === 0 ? s.toUpperCase() : s))
+    .join("");
+
+  const onSwitchWallet = useCallback(() => {
+    if (actuallyCanAttemptSwitch && activeChainId) {
+      switchNetwork(activeChainId);
+    }
+  }, [activeChainId, actuallyCanAttemptSwitch, switchNetwork]);
+
+  return (
+    <Flex direction="column" gap={4}>
+      <Heading size="label.lg">
+        <Flex gap={2} align="center">
+          <Icon boxSize={6} as={AiOutlineWarning} />
+          <span>Network Mismatch</span>
+        </Flex>
+      </Heading>
+
+      <Text>
+        You are connected to the <strong>{walletNetwork}</strong> network but
+        you are trying to interact on the <strong>{twNetwork}</strong> network.
+      </Text>
+
+      <Button
+        leftIcon={<Icon as={VscDebugDisconnect} />}
+        size="sm"
+        onClick={onSwitchWallet}
+        isLoading={network.loading}
+        isDisabled={!actuallyCanAttemptSwitch}
+        colorScheme="orange"
+      >
+        Switch wallet to {twNetwork}
+      </Button>
+
+      {!actuallyCanAttemptSwitch && (
+        <Text size="body.sm" fontStyle="italic">
+          Your connected wallet does not support programatic switching.
+          <br />
+          Please manually switch the network in your wallet.
+        </Text>
+      )}
+    </Flex>
+  );
+};
