2895f6925eaee487bff0c3dc50aaabca648a3f9b
Nacho Iacovino <nacho.iacovino@gmail.com>
2022-05-11 19:10:49 +0200

    Use ChakraNextImage

--- components/homepage/CaseStudyCard.tsx
+++ components/homepage/CaseStudyCard.tsx
@@ -16,30 +16,41 @@ export const CaseStudyCard: React.FC<CaseStudyCardProps> = ({
     <Flex
       flexDir="column"
       borderRadius="2xl"
-      bgImage={`url('/assets/case-studies/${title}.png')`}
-      bgPosition="center"
-      bgSize="cover"
-      bgRepeat="no-repeat"
-      height={96}
       overflow="hidden"
+      position="relative"
+      h={96}
     >
-      <Flex flexDir="column" justifyContent="space-between" h="100%">
-        <Box p={6}>
-          <ChakraNextImage
-            alt=""
-            w={title === "yestheory" ? 16 : 32}
-            maxW={title === "yestheory" ? 16 : 32}
-            display={{ base: "none", md: "block" }}
-            placeholder="empty"
-            src={require(`/public/assets/case-studies/${title}-logo.png`)}
-          />
-        </Box>
-        <Box backdropFilter="blur(30px)" p={6} borderBottomRadius="2xl">
-          <Text size="body.xl" color="white">
-            {description}
-          </Text>
-        </Box>
-      </Flex>
+      <ChakraNextImage
+        alt=""
+        w="100%"
+        h="100%"
+        src={require(`/public/assets/case-studies/${title}.png`)}
+        objectFit="cover"
+        layout="fill"
+      />
+      <Box p={6} position="absolute" top="0" left="0">
+        <ChakraNextImage
+          alt=""
+          w={title === "yestheory" ? 16 : 32}
+          maxW={title === "yestheory" ? 16 : 32}
+          placeholder="empty"
+          src={require(`/public/assets/case-studies/${title}-logo.png`)}
+        />
+      </Box>
+
+      <Box
+        backdropFilter="blur(30px)"
+        p={6}
+        borderBottomRadius="2xl"
+        position="absolute"
+        bottom="-1px"
+        left="0"
+        right="0"
+      >
+        <Text size="body.xl" color="white">
+          {description}
+        </Text>
+      </Box>
     </Flex>
   );
 };
