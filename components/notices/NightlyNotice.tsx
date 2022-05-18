6b92903feebe0899081b0e7819539366d63b4958
Nacho Iacovino <nacho.iacovino@gmail.com>
2022-05-19 01:04:41 +0200

    Update privacy/tos notice

--- components/notices/NightlyNotice.tsx
+++ components/notices/NightlyNotice.tsx
@@ -1,21 +1,18 @@
 import {
-  ButtonGroup,
   Divider,
   Flex,
-  ListItem,
   Modal,
   ModalBody,
   ModalContent,
   ModalHeader,
   ModalOverlay,
-  UnorderedList,
   useBreakpointValue,
 } from "@chakra-ui/react";
 import { Logo } from "components/logo";
 import { useLocalStorage } from "hooks/useLocalStorage";
 import React from "react";
-import { FiCheck } from "react-icons/fi";
-import { Button, Heading, LinkButton, Text } from "tw-components";
+import { useForm } from "react-hook-form";
+import { Button, Checkbox, Heading, Text, TrackedLink } from "tw-components";
 
 export const NightlyNotice: React.FC = () => {
   const [hasShownWelcome, setHasShownWelcome] = useLocalStorage(
@@ -23,6 +20,7 @@ export const NightlyNotice: React.FC = () => {
     false,
   );
   const isMobile = useBreakpointValue({ base: true, md: false });
+  const { register, handleSubmit } = useForm<{ accepted: false }>();
 
   return hasShownWelcome ? null : (
     <Modal
@@ -41,65 +39,52 @@ export const NightlyNotice: React.FC = () => {
             <Logo />
           </Flex>
         </ModalHeader>
-        <ModalBody as={Flex} gap={1} flexDirection="column">
-          <Heading size="subtitle.md">
-            Welcome to the new thirdweb dashboard
-          </Heading>
-          <Text>
-            This is the newest version of the thirdweb dashboard and comes with
-            many <strong>new improvements</strong>.
-          </Text>
-          <UnorderedList spacing={0.5}>
-            <Text as={ListItem}>
-              We are now completely <strong>free to use</strong> during early
-              access! We no longer take any fees.
-            </Text>
-            <Text as={ListItem}>
-              Contract deployments are <strong>up to 10x cheaper</strong> -
-              we&apos;re now using proxy contracts to save you gas! Deployed
-              contracts are still 100% owned by you.
-            </Text>
-            <Text as={ListItem}>
-              <strong>Projects have been removed</strong>. Instead, you can
-              deploy contracts directly from your wallet, saving you even more
-              gas.
-            </Text>
-            <Text as={ListItem}>
-              Modules now are re-named to <strong>contracts</strong>.
-            </Text>
-            <Text as={ListItem}>
-              The dashboard now has a <strong>dark mode</strong>! You can use
-              the crescent icon in the top right to toggle it.
-            </Text>
-          </UnorderedList>
-
-          <Divider my={4} />
-
-          <ButtonGroup
-            flexDirection={{ base: "column", md: "row" }}
-            gap={2}
-            spacing={0}
-            w="100%"
+        <ModalBody>
+          <Flex
+            gap={1}
+            flexDirection="column"
+            as="form"
+            onSubmit={handleSubmit(() => setHasShownWelcome(true))}
           >
-            <LinkButton
-              width="100%"
-              href="https://blog.thirdweb.com/thirdweb-v2"
-              borderRadius="md"
-              isExternal
-            >
-              Learn More
-            </LinkButton>
+            <Heading size="subtitle.md">
+              Welcome to the thirdweb dashboard
+            </Heading>
+            <Flex mt={1}>
+              <Checkbox isRequired {...register("accepted")} />
+              <Text ml={3}>
+                Check here to indicate that you have read and agree to our{" "}
+                <TrackedLink
+                  href="/privacy"
+                  isExternal
+                  category="notice"
+                  label="privacy"
+                  textDecoration="underline"
+                >
+                  Privacy Policy
+                </TrackedLink>{" "}
+                and{" "}
+                <TrackedLink
+                  href="/tos"
+                  isExternal
+                  category="notice"
+                  label="terms"
+                  textDecoration="underline"
+                >
+                  Terms of Service
+                </TrackedLink>
+              </Text>
+            </Flex>
+            <Divider my={4} />
             <Button
               autoFocus
               width="100%"
-              onClick={() => setHasShownWelcome(true)}
-              leftIcon={<FiCheck />}
+              type="submit"
               borderRadius="md"
               colorScheme="primary"
             >
               Continue
             </Button>
-          </ButtonGroup>
+          </Flex>
         </ModalBody>
       </ModalContent>
     </Modal>
