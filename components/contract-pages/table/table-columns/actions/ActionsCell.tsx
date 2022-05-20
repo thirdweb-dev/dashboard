5a16a90ce60ffdf0320cef87e373086b3e98774c
Nacho Iacovino <nacho.iacovino@gmail.com>
2022-05-20 16:19:41 +0200

    Fix actions cells width

--- components/contract-pages/table/table-columns/actions/ActionsCell.tsx
+++ components/contract-pages/table/table-columns/actions/ActionsCell.tsx
@@ -4,7 +4,7 @@ import { EditionBurnCell } from "./EditionBurnCell";
 import { EditionTransferCell } from "./EditionTransferCell";
 import { OwnerBurnCell } from "./OwnerBurnCell";
 import { OwnerTransferCell } from "./OwnerTransferCell";
-import { ButtonGroup, Stack } from "@chakra-ui/react";
+import { ButtonGroup, Flex, Stack } from "@chakra-ui/react";
 import { EditionMetadata, NFTMetadataOwner } from "@thirdweb-dev/sdk";
 import { Row } from "react-table";
 
@@ -17,18 +17,18 @@ interface IEditionActionsCell {
 }
 
 const ERC721Cell: React.FC<INFTActionsCell> = ({ row }) => (
-  <>
+  <Flex flexDir="column" gap={1}>
     <OwnerTransferCell row={row} />
     <OwnerBurnCell row={row} />
-  </>
+  </Flex>
 );
 
 const ERC1155Cell: React.FC<IEditionActionsCell> = ({ row }) => (
-  <>
+  <Flex flexDir="column" gap={1}>
     <EditionTransferCell row={row} />
     <EditionAirdropCell row={row} />
     <EditionBurnCell row={row} />
-  </>
+  </Flex>
 );
 
 export const NFTActionsCell: React.FC<INFTActionsCell> = ({ row }) => {
