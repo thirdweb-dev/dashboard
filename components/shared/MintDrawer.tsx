494b30a8e3ead785d848f8debcd81f2e1c49130d
Jonas Daniels <jonas.daniels@outlook.com>
2022-05-25 15:59:39 -0700

    fix types

--- components/shared/MintDrawer.tsx
+++ components/shared/MintDrawer.tsx
@@ -1,9 +1,9 @@
-import { Erc20, Erc721, ValidContractInstance } from "@thirdweb-dev/sdk";
+import { ValidContractInstance } from "@thirdweb-dev/sdk";
 import { MintForm } from "components/contract-pages/forms/mint";
 import { Drawer } from "tw-components";
 
 interface IMintDrawer {
-  contract?: ValidContractInstance | Erc721 | Erc20;
+  contract?: ValidContractInstance;
   isOpen: boolean;
   onClose: () => void;
 }
