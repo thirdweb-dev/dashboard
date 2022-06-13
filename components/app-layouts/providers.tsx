ad46b787bc3677e9944fc1f19dfda15e7024b5d7
Nacho Iacovino <nacho.iacovino@gmail.com>
2022-06-13 14:42:37 -0700

    Add RPC urls

--- components/app-layouts/providers.tsx
+++ components/app-layouts/providers.tsx
@@ -51,16 +51,33 @@ export const alchemyUrlMap: Record<SUPPORTED_CHAIN_ID, string> = {
   [ChainId.Goerli]:
     process.env.NEXT_PUBLIC_RPC_GOERLI ||
     `https://eth-goerli.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`,
-  [ChainId.Fantom]:
-    process.env.NEXT_PUBLIC_RPC_FANTOM || "https://rpc.ftm.tools",
-  [ChainId.Avalanche]:
-    process.env.NEXT_PUBLIC_RPC_AVALANCHE || "https://rpc.ankr.com/avalanche",
   [ChainId.Polygon]:
     process.env.NEXT_PUBLIC_RPC_POLYGON ||
     `https://polygon-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`,
   [ChainId.Mumbai]:
     process.env.NEXT_PUBLIC_RPC_MUMBAI ||
     `https://polygon-mumbai.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`,
+  [ChainId.Fantom]:
+    process.env.NEXT_PUBLIC_RPC_FANTOM || "https://rpc.ftm.tools",
+  [ChainId.FantomTestnet]:
+    process.env.NEXT_PUBLIC_RPC_FANTOM_TESTNET ||
+    "https://rpc.testnet.fantom.network",
+  [ChainId.Avalanche]:
+    process.env.NEXT_PUBLIC_RPC_AVALANCHE || "https://rpc.ankr.com/avalanche",
+  [ChainId.AvalancheFujiTestnet]:
+    process.env.NEXT_PUBLIC_RPC_AVALANCHE_FUJI_TESTNET ||
+    "https://api.avax-test.network/ext/bc/C/rpc	",
+  [ChainId.Optimism]:
+    process.env.NEXT_PUBLIC_RPC_OPTIMISM ||
+    `https://opt-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`,
+  [ChainId.OptimismTestnet]:
+    process.env.NEXT_PUBLIC_RPC_OPTIMISM_TESTNET || "https://kovan.optimism.io",
+  [ChainId.Arbitrum]:
+    process.env.NEXT_PUBLIC_RPC_ARBITRUM ||
+    `https://arb-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`,
+  [ChainId.ArbitrumTestnet]:
+    process.env.NEXT_PUBLIC_RPC_ARBITRUM_TESTNET ||
+    `https://arb-rinkeby.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`,
 };
 
 const walletConnectors: WalletConnector[] = [
