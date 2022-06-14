5db46fde2c68528b6428b80ec029828c88f5f0b0
Nacho Iacovino <nacho.iacovino@gmail.com>
2022-06-13 17:32:52 -0700

    Update embeds

--- components/contract-tabs/embeds/index.tsx
+++ components/contract-tabs/embeds/index.tsx
@@ -35,7 +35,7 @@ interface EmbedSetupProps {
   contract?: ValidContractInstance;
 }
 
-const IPFS_URI = "ipfs://QmfJu3spsSJot6givCK2VjwEgVHymc5RCXHqfG1W5WZyFX";
+const IPFS_URI = "ipfs://QmPggkNM5wmagXZpQbqM96ymH1893doNF67HSmkTjfr9nK";
 
 const getContractEmbedHash = (contract?: ValidContractInstance) => {
   if (contract instanceof NFTDrop) {
