12a476c008d1ace8a3b41cafd97daf4b24a666da
Nacho Iacovino <nacho.iacovino@gmail.com>
2022-06-17 16:54:49 -0700

    Remove unused imports

--- pages/contracts/new/[type]/[contractType].tsx
+++ pages/contracts/new/[type]/[contractType].tsx
@@ -1,26 +1,17 @@
-import { AspectRatio, Box, Divider, Flex, IconButton } from "@chakra-ui/react";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { Box } from "@chakra-ui/react";
 import { useChainId } from "@thirdweb-dev/react";
 import {
   ContractType,
   SUPPORTED_CHAIN_ID,
   SUPPORTED_CHAIN_IDS,
-  ValidContractClass,
 } from "@thirdweb-dev/sdk";
-import { ChakraNextImage } from "components/Image";
 import { AppLayout } from "components/app-layouts/app";
 import BuiltinContractForm from "components/contract-components/contract-deploy-form/built-in-contract";
-import { CONTRACT_TYPE_NAME_MAP, FeatureIconMap } from "constants/mappings";
 import { useSingleQueryParam } from "hooks/useQueryParam";
-import { useRouter } from "next/router";
 import React, { ReactElement, useEffect, useState } from "react";
-import { SubmitErrorHandler, SubmitHandler, useForm } from "react-hook-form";
-import { FiChevronLeft } from "react-icons/fi";
-import { Card, Heading } from "tw-components";
-import { z } from "zod";
+import { Card } from "tw-components";
 
 export default function DeployContractContract() {
-  const router = useRouter();
   const contractType = useSingleQueryParam("contractType") as
     | ContractType
     | undefined;
