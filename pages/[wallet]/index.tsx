10f78ea8a72cf8ab134ecb9f5a7c0a9aa1c0cd99
Nacho Iacovino <nacho.iacovino@gmail.com>
2022-06-16 21:12:00 -0700

    Delete console.log

--- pages/[wallet]/index.tsx
+++ pages/[wallet]/index.tsx
@@ -239,8 +239,6 @@ export default function Dashboard() {
     arbitrumTestnetQuery.isFetched &&
     projectsIsFetched;
 
-  console.log({ isFetched, projectsIsFetched });
-
   useEffect(() => {
     if (isFetched && combinedList.length === 0 && projects?.length === 0) {
       router.replace("/contracts");
