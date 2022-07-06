016617dbd86ec45ca47e09af0531666b6562046b
Nacho Iacovino <nacho.iacovino@gmail.com>
2022-07-06 23:00:03 +0200

    Prettier

--- pages/contracts/[wallet]/index.tsx
+++ pages/contracts/[wallet]/index.tsx
@@ -5,15 +5,14 @@ import { ReactElement, useEffect } from "react";
 
 export default function UserPage() {
   const wallet = useSingleQueryParam("wallet");
-  const router = useRouter()
+  const router = useRouter();
 
   // We do this so it doesn't break for users that haven't updated their CLI
   useEffect(() => {
-    const previousPath = router.asPath.split("/")[2]
-    if(!wallet?.startsWith("0x") && previousPath !== "[wallet]") {
-      router.replace(`/contracts/deploy/${previousPath}`)
+    const previousPath = router.asPath.split("/")[2];
+    if (!wallet?.startsWith("0x") && previousPath !== "[wallet]") {
+      router.replace(`/contracts/deploy/${previousPath}`);
     }
-
   }, [wallet]);
 
   return null;
