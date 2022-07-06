964e91e2efaae7c5cc16b5e11eb2f2f28cfa54c5
Nacho Iacovino <nacho.iacovino@gmail.com>
2022-07-06 22:54:50 +0200

    Redirect to /deploy namespace

--- pages/contracts/[wallet]/index.tsx
+++ pages/contracts/[wallet]/index.tsx
@@ -1,11 +1,22 @@
 import { AppLayout } from "components/app-layouts/app";
 import { useSingleQueryParam } from "hooks/useQueryParam";
-import { ReactElement } from "react";
+import { useRouter } from "next/router";
+import { ReactElement, useEffect } from "react";
 
 export default function UserPage() {
   const wallet = useSingleQueryParam("wallet");
-  console.log(wallet);
-  return "hi";
+  const router = useRouter()
+
+  // We do this so it doesn't break for users that haven't updated their CLI
+  useEffect(() => {
+    const previousPath = router.asPath.split("/")[2]
+    if(!wallet?.startsWith("0x") && previousPath !== "[wallet]") {
+      router.replace(`/contracts/deploy/${previousPath}`)
+    }
+
+  }, [wallet]);
+
+  return null;
 }
 
 UserPage.getLayout = (page: ReactElement) => <AppLayout>{page}</AppLayout>;
