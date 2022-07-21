37ec8658cdb80d9ca33dbf62c8a287292b76f898
Nacho Iacovino <nacho.iacovino@gmail.com>
2022-07-21 21:24:43 +0200

    Prettify markdown renderer

--- components/contract-components/released-contract/markdown-renderer.tsx
+++ components/contract-components/released-contract/markdown-renderer.tsx
@@ -11,29 +11,76 @@ import {
   UnorderedList,
   chakra,
 } from "@chakra-ui/react";
+import { onlyText } from "react-children-utilities";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-import { Card, Heading, Text } from "tw-components";
+import { Card, CodeBlock, Heading, Text } from "tw-components";
 
 const ChakraReactMarkdown = chakra(ReactMarkdown);
 
 export const MarkdownRenderer: React.FC<
   BoxProps & { markdownText: string }
 > = ({ markdownText, ...restProps }) => {
+  const commonHeadingProps = {
+    lineHeight: "1.25",
+    mb: 4,
+    pb: 2,
+  };
+
   return (
     <ChakraReactMarkdown
       {...restProps}
       remarkPlugins={[remarkGfm]}
       components={{
-        h1: (props) => <Heading size="title.lg" {...props} />,
-        h2: (props) => <Heading size="title.md" {...props} />,
-        h3: (props) => <Heading size="title.sm" {...props} />,
-        h4: (props) => <Heading size="subtitle.md" {...props} />,
-        h5: (props) => <Heading size="subtitle.sm" {...props} />,
-        h6: (props) => <Heading size="label.md" {...props} />,
-        p: (props) => <Text size="body.md" {...props} />,
+        h1: (props) => (
+          <Heading
+            size="title.lg"
+            borderBottom="1px solid"
+            borderBottomColor="borderColor"
+            {...commonHeadingProps}
+            {...props}
+          />
+        ),
+        h2: (props) => (
+          <Heading
+            size="title.md"
+            borderBottom="1px solid"
+            borderBottomColor="borderColor"
+            mt={6}
+            {...commonHeadingProps}
+            {...props}
+          />
+        ),
+        h3: (props) => (
+          <Heading size="title.sm" {...commonHeadingProps} {...props} />
+        ),
+        h4: (props) => (
+          <Heading size="subtitle.md" {...commonHeadingProps} {...props} />
+        ),
+        h5: (props) => (
+          <Heading size="subtitle.sm" {...commonHeadingProps} {...props} />
+        ),
+        h6: (props) => (
+          <Heading size="label.md" {...commonHeadingProps} {...props} />
+        ),
+        pre: (props) => (
+          <CodeBlock
+            code={onlyText(props.children).trim()}
+            language="jsx"
+            mb={4}
+            {...props}
+          />
+        ),
+        p: (props) => <Text size="body.md" mb={4} {...props} />,
         table: (props) => (
-          <Card maxW="100%" overflowX="auto" position="relative" px={0} py={0}>
+          <Card
+            maxW="100%"
+            overflowX="auto"
+            position="relative"
+            px={0}
+            py={0}
+            mb={4}
+          >
             <Table {...props} />
           </Card>
         ),
