f29eddc9bc84ebeea50ab3627a04454606137121
Jonas Daniels <jonas.daniels@outlook.com>
2022-08-12 16:05:29 -0700

    fix error in useTrack

--- hooks/analytics/useTrack.ts
+++ hooks/analytics/useTrack.ts
@@ -19,6 +19,15 @@ export function useTrack() {
       console.debug(`[PH.capture]:${catActLab}`, restData);
     }
 
-    posthog.capture(catActLab, flat(restData));
+    const restDataSafe = Object.fromEntries(
+      Object.entries(restData).map(([key, value]) => {
+        if (value instanceof Error) {
+          return [key, { message: value.message, stack: value.stack }];
+        }
+        return [key, value];
+      }),
+    );
+
+    posthog.capture(catActLab, flat(restDataSafe));
   }, []);
 }
