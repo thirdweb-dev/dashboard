fd4f564e56dde989547f9552adab69571241c92d
Nacho Iacovino <nacho.iacovino@gmail.com>
2022-08-17 00:11:02 +0200

    Simplify logic

--- components/contract-components/shared/sources-panel.tsx
+++ components/contract-components/shared/sources-panel.tsx
@@ -1,9 +1,8 @@
 import { SourceFile } from "../types";
 import { SourcesAccordion } from "./sources-accordion";
-import { Flex, Icon } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import { AbiSchema } from "@thirdweb-dev/sdk";
-import { FiXCircle } from "react-icons/fi";
-import { Card, CodeBlock, Heading, Link } from "tw-components";
+import { CodeBlock, Heading, Link, Text } from "tw-components";
 import { z } from "zod";
 
 interface SourcesPanelProps {
@@ -14,7 +13,7 @@ interface SourcesPanelProps {
 export const SourcesPanel: React.FC<SourcesPanelProps> = ({ sources, abi }) => {
   return (
     <Flex flexDir="column" gap={8}>
-      <Flex direction="column" gap={2} justify="space-between">
+      {abi && (
         <Flex direction="column" gap={4} mb={{ base: 2, md: 0 }}>
           <Heading size="title.sm">ABI</Heading>
           <CodeBlock
@@ -24,31 +23,25 @@ export const SourcesPanel: React.FC<SourcesPanelProps> = ({ sources, abi }) => {
             overflow="auto"
           />
         </Flex>
-      </Flex>
-
-      {sources && sources.length > 0 ? (
-        <Flex flexDir="column" gap={4}>
-          <Heading size="title.sm">Solidity</Heading>
-          <SourcesAccordion sources={sources} />
-        </Flex>
-      ) : (
-        <Card>
-          <Flex direction="column" align="left" gap={2}>
-            <Flex direction="row" align="center" gap={2}>
-              <Icon as={FiXCircle} color="red.500" />
-              <Heading size="title.sm">
-                Contract source code not available
-              </Heading>
-            </Flex>
-            <Heading size="subtitle.sm">
-              Try deploying with{" "}
-              <Link href="https://portal.thirdweb.com/cli" isExternal>
-                thirdweb CLI v0.5+
-              </Link>
-            </Heading>
-          </Flex>
-        </Card>
       )}
+
+      <Flex flexDir="column" gap={4}>
+        <Heading size="title.sm">Solidity</Heading>
+        {sources && sources?.length > 0 ? (
+          <SourcesAccordion sources={sources} />
+        ) : (
+          <Text>
+            Contract source code not available. Try deploying with{" "}
+            <Link
+              href="https://portal.thirdweb.com/cli"
+              isExternal
+              color="blue.500"
+            >
+              thirdweb CLI v0.5+
+            </Link>
+          </Text>
+        )}
+      </Flex>
     </Flex>
   );
 };
