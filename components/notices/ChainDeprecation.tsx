335c9c5d3ac7bdbd4b06c4b241523bfee1be7508
Jonas Daniels <jonas.daniels@outlook.com>
2022-09-28 10:11:14 -0700

    merge in main

--- components/notices/ChainDeprecation.tsx
+++ components/notices/ChainDeprecation.tsx
@@ -1,4 +1,4 @@
-import { useActiveChainId } from "@3rdweb-sdk/react";
+import { useDashboardEVMChainId } from "@3rdweb-sdk/react";
 import { Flex } from "@chakra-ui/react";
 import { SUPPORTED_CHAIN_ID } from "@thirdweb-dev/sdk";
 import { deprecatedChains } from "constants/mappings";
@@ -7,7 +7,7 @@ import { Text } from "tw-components";
 export const ChainDeprecation = () => {
   const currentDate = new Date();
   const deprecationDate = new Date("2021-10-05T00:00:00.000Z");
-  const chainId = useActiveChainId();
+  const chainId = useDashboardEVMChainId();
 
   if (!deprecatedChains.includes(chainId as SUPPORTED_CHAIN_ID)) {
     return null;
