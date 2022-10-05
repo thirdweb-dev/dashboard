b5aabf2279743665507e2d78657cf01864068bd6
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2022-10-05 22:23:29 +0530

    refactor: refactor faq

--- components/faucet/FAQSection.tsx
+++ components/faucet/FAQSection.tsx
@@ -9,44 +9,37 @@ import {
 } from "@chakra-ui/react";
 import { Heading } from "tw-components";
 
+const FAQs = [
+  {
+    question: "What is the Solana faucet?",
+    answer:
+      "The Solana faucet is a tool that allows you to get free Solana tokens for testing and development purposes. You can use the tokens to build your web3 app on the Solana blockchain.",
+  },
+  {
+    question: "How do I get free Solana tokens?",
+    answer:
+      "To get free Solana tokens, you need to enter your Solana address and click on the Get tokens button. You will receive 1 SOL token in your wallet.",
+  },
+];
+
 export const FaqSection: React.FC = () => {
   return (
-    <Flex my={12} flexDir="column">
+    <Flex mt={12} flexDir="column">
       <Heading>FAQ</Heading>
       <Accordion mt="2" allowMultiple>
-        <AccordionItem>
-          <h2>
-            <AccordionButton>
-              <Box flex="1" textAlign="left">
-                Section 1 title
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </AccordionPanel>
-        </AccordionItem>
-
-        <AccordionItem>
-          <h2>
-            <AccordionButton>
-              <Box flex="1" textAlign="left">
-                Section 2 title
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </AccordionPanel>
-        </AccordionItem>
+        {FAQs.map(({ question, answer }) => (
+          <AccordionItem key={question}>
+            <h2>
+              <AccordionButton>
+                <Box flex="1" textAlign="left">
+                  {question}
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>{answer}</AccordionPanel>
+          </AccordionItem>
+        ))}
       </Accordion>
     </Flex>
   );
