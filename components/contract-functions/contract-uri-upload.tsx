be831048297e24e5903c552ed2ed86e0542637f8
adam-maj <mr.adam.maj@gmail.com>
2022-10-18 10:23:13 -0700

    Add upload button

--- components/contract-functions/contract-uri-upload.tsx
+++ components/contract-functions/contract-uri-upload.tsx
@@ -1,17 +1,8 @@
-import {
-  Center,
-  Icon,
-  Input,
-  InputGroup,
-  InputRightElement,
-  Textarea,
-  useDisclosure,
-} from "@chakra-ui/react";
+import { Icon, Input, InputGroup, InputRightElement } from "@chakra-ui/react";
 import { useStorageUpload } from "@thirdweb-dev/react";
 import { FileInput } from "components/shared/FileInput";
-import { useState } from "react";
 import { FiUpload } from "react-icons/fi";
-import { Button, Drawer, Heading } from "tw-components";
+import { Button } from "tw-components";
 
 interface ContractUriUploadProps {
   value: string;
@@ -22,87 +13,36 @@ export const ContractUriUpload: React.FC<ContractUriUploadProps> = ({
   value,
   setValue,
 }) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { mutateAsync: upload, isLoading } = useStorageUpload();
 
-  return (
-    <>
-      <UploadDrawer isOpen={isOpen} onClose={onClose} setValue={setValue} />
-      <InputGroup>
-        <Input
-          defaultValue={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
-        <InputRightElement width="96px">
-          <Button
-            size="xs"
-            padding="10px"
-            paddingY="14px"
-            colorScheme="purple"
-            rightIcon={<Icon as={FiUpload} />}
-            onClick={onOpen}
-          >
-            Upload
-          </Button>
-        </InputRightElement>
-      </InputGroup>
-    </>
-  );
-};
-
-interface UploadDrawerProps {
-  isOpen: boolean;
-  onClose: () => void;
-  setValue: (value: string) => void;
-}
-
-const UploadDrawer: React.FC<UploadDrawerProps> = ({
-  isOpen,
-  onClose,
-  setValue,
-}) => {
-  const { mutateAsync: upload } = useStorageUpload();
-  const [data, setData] = useState<any>("");
-
-  const handleUpload = async () => {
-    const [uri] = await upload(data);
+  const handleUpload = async (file: any) => {
+    const [uri] = await upload({ data: [file] });
     setValue(uri);
-    onClose();
   };
 
   return (
-    <Drawer
-      isOpen={isOpen}
-      size="md"
-      onClose={onClose}
-      header={{
-        children: <Heading>Upload Metadata to IPFS</Heading>,
-      }}
-      footer={{
-        children: (
-          <>
-            <Button variant="outline" mr={3} onClick={onClose}>
-              Cancel
-            </Button>
-            <Button colorScheme="blue" onClick={handleUpload}>
+    <>
+      <InputGroup>
+        <Input
+          defaultValue={value}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <InputRightElement width="96px">
+          <FileInput setValue={(file) => handleUpload(file)}>
+            <Button
+              size="xs"
+              padding="10px"
+              paddingY="14px"
+              colorScheme="purple"
+              rightIcon={<Icon as={FiUpload} />}
+              isLoading={isLoading}
+            >
               Upload
             </Button>
-          </>
-        ),
-      }}
-    >
-      <Textarea value={data} onChange={(e) => setData(e.target.value)} />
-      <FileInput setValue={(file) => setData(file)}>
-        <Center
-          borderRadius="md"
-          width="200px"
-          height="200px"
-          border="1px solid"
-          borderColor="gray.500"
-          bg="gray.700"
-        >
-          <Icon as={FiUpload} color="gray.600" _hover={{ color: "gray.500" }} />
-        </Center>
-      </FileInput>
-    </Drawer>
+          </FileInput>
+        </InputRightElement>
+      </InputGroup>
+    </>
   );
 };
