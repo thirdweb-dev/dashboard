8fc25b47e145a356b1d41316d4ed025cf7fd3c13
Jonas Daniels <jonas.daniels@outlook.com>
2022-10-18 17:20:21 -0700

    refinement

--- components/notices/welcome-screen.tsx
+++ components/notices/welcome-screen.tsx
@@ -19,6 +19,30 @@ import React, { useState } from "react";
 import { FiArrowRight } from "react-icons/fi";
 import { Button, Heading, Text } from "tw-components";
 
+const steps = [
+  {
+    key: "step-1",
+    action: "Build",
+    title: "Build contracts & applications",
+    text: "Ship your web3 apps faster by reducing development time. Build smart contracts and applications effortlessly with our prebuilt contracts, build your own contract with ContractKit, and powerful SDKs.",
+    gradient: "linear-gradient(147.15deg, #00C850 0%, #AD00FF 100%)",
+  },
+  {
+    key: "step-2",
+    action: "Launch",
+    title: "Launch contracts & applications",
+    text: "Deploy your smart contracts on-chain easily with a single command or with our Dashboard without requiring private keys. Publish your contracts with Release for other web3 devs to discover and deploy your contract with one-click.",
+    gradient: "linear-gradient(147.15deg, #000AFF 0%, #FF3D00 130%)",
+  },
+  {
+    key: "step-3",
+    action: "Manage",
+    title: "Manage contracts",
+    text: "After you have deployed your contracts on-chain, you can monitor, configure and interact with your contracts directly from the Dashboard. Invite your team to collaborate and manage their permissions for interacting with contracts.",
+    gradient: "linear-gradient(147.15deg, #8F00FF 0%, #FFB800 130%)",
+  },
+] as const;
+
 export const WelcomeScreen: React.FC = () => {
   const [showWelcomeScreen, setShowWelcomeScreen] = useLocalStorage(
     "welcome-screen",
@@ -52,38 +76,38 @@ export const WelcomeScreen: React.FC = () => {
 
   return (
     <MotionConfig transition={{ duration: 0.3 }}>
-      <Modal
-        size="3xl"
-        closeOnEsc={false}
-        allowPinchZoom
-        closeOnOverlayClick={false}
-        isOpen
-        onClose={() => undefined}
-      >
-        <ModalOverlay />
-        <ModalContent
-          borderTopRadius={{ base: "2xl", md: "lg" }}
-          overflow="hidden"
-          borderBottomRadius={{ base: "none", md: "lg" }}
-          mb={{ base: 0, md: "auto" }}
-          minH={{ base: "80vh", md: "auto" }}
-          position={{ base: "fixed", md: "relative" }}
-          bottom={{ base: 0, md: undefined }}
-          mt="auto"
+      <AnimatePresence initial={step !== 0}>
+        <Modal
+          size="4xl"
+          closeOnEsc={false}
+          allowPinchZoom
+          closeOnOverlayClick={false}
+          isOpen
+          onClose={() => undefined}
         >
-          <ModalBody p={0}>
-            <SimpleGrid
-              overflow="hidden"
-              columns={{ base: 1, md: 2 }}
-              placeItems="center"
-              p={0}
-            >
-              <AspectRatio
-                ratio={{ base: 16 / 9, md: 1 }}
-                w="100%"
-                position="relative"
+          <ModalOverlay />
+          <ModalContent
+            borderTopRadius={{ base: "2xl", md: "lg" }}
+            overflow="hidden"
+            borderBottomRadius={{ base: "none", md: "lg" }}
+            mb={{ base: 0, md: "auto" }}
+            minH={{ base: "80vh", md: "auto" }}
+            position={{ base: "fixed", md: "relative" }}
+            bottom={{ base: 0, md: undefined }}
+            mt="auto"
+          >
+            <ModalBody p={0}>
+              <SimpleGrid
+                overflow="hidden"
+                columns={{ base: 1, md: 2 }}
+                placeItems="center"
+                p={0}
               >
-                <AnimatePresence initial={step !== 0}>
+                <AspectRatio
+                  ratio={{ base: 16 / 9, md: 1 }}
+                  w="100%"
+                  position="relative"
+                >
                   <Center
                     as={motion.div}
                     initial={`step-${step}`}
@@ -93,18 +117,10 @@ export const WelcomeScreen: React.FC = () => {
                         background:
                           "linear-gradient(147.15deg, #410AB6 30.17%, #B4F1FF 100.01%)",
                       },
-                      "step-1": {
-                        background:
-                          "linear-gradient(147.15deg, #00C850 0%, #AD00FF 100%)",
-                      },
-                      "step-2": {
-                        background:
-                          "linear-gradient(147.15deg, #000AFF 0%, #FF3D00 130%)",
-                      },
-                      "step-3": {
-                        background:
-                          "linear-gradient(147.15deg, #8F00FF 0%, #FFB800 130%)",
-                      },
+                      ...steps.reduce((acc, curr) => {
+                        acc[curr.key] = { background: curr.gradient };
+                        return acc;
+                      }, {} as Record<typeof steps[number]["key"], { background: typeof steps[number]["gradient"] }>),
                     }}
                     w="100%"
                     h="100%"
@@ -150,62 +166,47 @@ export const WelcomeScreen: React.FC = () => {
                         exit={{ opacity: 0, y: "100%" }}
                       >
                         <Flex gap={6} direction="column" textAlign="center">
-                          <Heading
-                            cursor="pointer"
-                            onClick={() => setStep(1)}
-                            opacity={step === 1 ? 1 : 0.5}
-                            fontWeight={step === 1 ? 700 : 400}
-                            letterSpacing={step === 1 ? "0em" : "0.02em"}
-                            size="title.2xl"
-                            transition="all 0.2s"
-                          >
-                            Build
-                          </Heading>
-
-                          <Heading
-                            cursor="pointer"
-                            onClick={() => setStep(2)}
-                            fontWeight={step === 2 ? 700 : 400}
-                            opacity={step === 2 ? 1 : 0.5}
-                            letterSpacing={step === 2 ? "0em" : "0.02em"}
-                            size="title.2xl"
-                            transition="all 0.2s"
-                          >
-                            Launch
-                          </Heading>
-
-                          <Heading
-                            cursor="pointer"
-                            onClick={() => setStep(3)}
-                            opacity={step === 3 ? 1 : 0.5}
-                            fontWeight={step === 3 ? 700 : 400}
-                            letterSpacing={step === 3 ? "0em" : "0.02em"}
-                            size="title.2xl"
-                            transition="all 0.2s"
-                          >
-                            Manage
-                          </Heading>
+                          {steps.map((s, i) => (
+                            <Heading
+                              key={s.key}
+                              cursor="pointer"
+                              onClick={() => setStep(i + 1)}
+                              size="title.2xl"
+                              transition="all 0.2s"
+                              {...(step === i + 1
+                                ? {
+                                    opacity: 1,
+                                    fontWeight: 700,
+                                    letterSpacing: "0em",
+                                  }
+                                : {
+                                    opacity: 0.5,
+                                    fontWeight: 400,
+                                    letterSpacing: "0.02em",
+                                  })}
+                            >
+                              {s.action}
+                            </Heading>
+                          ))}
                         </Flex>
                       </motion.div>
                     )}
                   </Center>
-                </AnimatePresence>
-              </AspectRatio>
+                </AspectRatio>
 
-              <Flex
-                direction="column"
-                gap={4}
-                p={6}
-                w="100%"
-                h="100%"
-                justify="space-between"
-              >
-                <Box placeSelf="start" flexShrink={0} h="30px">
-                  <Logo forceShowWordMark />
-                </Box>
+                <Flex
+                  direction="column"
+                  gap={4}
+                  p={6}
+                  w="100%"
+                  h="100%"
+                  justify="space-between"
+                >
+                  <Box placeSelf="start" flexShrink={0} h="30px">
+                    <Logo forceShowWordMark />
+                  </Box>
 
-                <Center flexGrow={1} position="relative">
-                  <AnimatePresence initial={step !== 0}>
+                  <Center flexGrow={1} position="relative">
                     <Box position="relative">
                       {step === 0 && (
                         <Flex
@@ -254,96 +255,55 @@ export const WelcomeScreen: React.FC = () => {
                             y: 20,
                           }}
                         >
-                          {step === 1 && (
-                            <motion.div key="step-1-title" {...titleAnimation}>
-                              <Heading size="title.sm">
-                                Build contracts & applications
-                              </Heading>
-                            </motion.div>
-                          )}
-                          {step === 2 && (
-                            <motion.div key="step-2-title" {...titleAnimation}>
-                              <Heading size="title.sm">
-                                Launch contracts & applications
-                              </Heading>
-                            </motion.div>
-                          )}
-                          {step === 3 && (
-                            <motion.div key="step-3-title" {...titleAnimation}>
-                              <Heading size="title.sm">
-                                Manage contracts
-                              </Heading>
-                            </motion.div>
-                          )}
-                          {step === 1 && (
-                            <motion.div key="step-1-text" {...textAnimation}>
-                              <Text size="body.lg">
-                                Ship your web3 apps faster by reducing
-                                development time. Build smart contracts and
-                                applications effortlessly with our prebuilt
-                                contracts, build your own contract with
-                                ContractKit, and powerful SDKs.
-                              </Text>
-                            </motion.div>
-                          )}
-                          {step === 2 && (
-                            <motion.div key="step-2-text" {...textAnimation}>
-                              <Text size="body.lg">
-                                Deploy your smart contracts on-chain easily with
-                                a single command or with our Dashboard without
-                                requiring private keys. Publish your contracts
-                                with Release for other web3 devs to discover and
-                                deploy your contract with one-click.
-                              </Text>
-                            </motion.div>
-                          )}
-                          {step === 3 && (
-                            <motion.div key="step-3-text" {...textAnimation}>
-                              <Text size="body.lg">
-                                After you have deployed your contracts on-chain,
-                                you can monitor, configure and interact with
-                                your contracts directly from the Dashboard.
-                                Invite your team to collaborate and manage their
-                                permissions for interacting with contracts.
-                              </Text>
-                            </motion.div>
+                          {steps.map(
+                            (s, i) =>
+                              step === i + 1 && (
+                                <React.Fragment key={s.key}>
+                                  <motion.div {...titleAnimation}>
+                                    <Heading size="title.sm">{s.title}</Heading>
+                                  </motion.div>
+                                  <motion.div {...textAnimation}>
+                                    <Text size="body.lg">{s.text}</Text>
+                                  </motion.div>
+                                </React.Fragment>
+                              ),
                           )}
                         </Flex>
                       )}
                     </Box>
-                  </AnimatePresence>
-                </Center>
-                <Box h="30px" />
-              </Flex>
-            </SimpleGrid>
-          </ModalBody>
-          <ModalFooter borderTop="1px solid" borderColor="borderColor">
-            <Flex w="full" align="center" justify="space-between">
-              <Button
-                onClick={() => setShowWelcomeScreen(false)}
-                fontWeight={400}
-                variant="link"
-                colorScheme="gray"
-              >
-                Skip for now
-              </Button>
-              <ButtonGroup variant="solid">
-                <Button onClick={prevStep} isDisabled={step === 0}>
-                  Back
-                </Button>
+                  </Center>
+                  <Box h="30px" />
+                </Flex>
+              </SimpleGrid>
+            </ModalBody>
+            <ModalFooter borderTop="1px solid" borderColor="borderColor">
+              <Flex w="full" align="center" justify="space-between">
                 <Button
-                  autoFocus
-                  onClick={nextStep}
-                  rightIcon={step === 3 ? undefined : <FiArrowRight />}
-                  colorScheme="blue"
+                  onClick={() => setShowWelcomeScreen(false)}
+                  fontWeight={400}
+                  variant="link"
+                  colorScheme="gray"
                 >
-                  {step === 3 ? "Start Now" : "Next"}
+                  Skip for now
                 </Button>
-              </ButtonGroup>
-            </Flex>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+                <ButtonGroup variant="solid">
+                  <Button onClick={prevStep} isDisabled={step === 0}>
+                    Back
+                  </Button>
+                  <Button
+                    autoFocus
+                    onClick={nextStep}
+                    rightIcon={step === 3 ? undefined : <FiArrowRight />}
+                    colorScheme="blue"
+                  >
+                    {step === 3 ? "Start Now" : "Next"}
+                  </Button>
+                </ButtonGroup>
+              </Flex>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </AnimatePresence>
     </MotionConfig>
   );
 };
