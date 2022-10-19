27215a2d178503a56e3d614e945203b10026d46f
Nacho Iacovino <nacho.iacovino@gmail.com>
2022-10-19 20:55:01 +0200

    Adding audited css

--- pages/api/badges/contract.tsx
+++ pages/api/badges/contract.tsx
@@ -20,10 +20,10 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     );
 
     const address = searchParams.get("address") || "0x...";
-    const audited = Boolean(searchParams.get("audited"));
     const theme = searchParams.get("theme") || "dark";
+    // Ignoring audited for now
+    const audited = false;
 
-    console.log({ address, audited, theme });
     const fontBuffer = await readFile(
       resolve("./public/assets/fonts", "Inter-medium.ttf"),
     );
@@ -62,24 +62,42 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         <div
           style={{
             display: "flex",
-            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
             height: "100%",
+            flexDirection: "column",
           }}
         >
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            {shortenIfAddress(address)}
-          </div>
+          <div>{shortenIfAddress(address)}</div>
+          {audited ? (
+            <div
+              style={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "flex-end",
+                marginTop: "3px",
+              }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill={`${theme === "dark" ? "#00FFB3" : "#0DAE4E"}`}
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.777 11.777 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7.159 7.159 0 0 0 1.048-.625 11.775 11.775 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.541 1.541 0 0 0-1.044-1.263 62.467 62.467 0 0 0-2.887-.87C9.843.266 8.69 0 8 0zm2.146 5.146a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 7.793l2.646-2.647z"
+                />
+              </svg>
+              <div style={{ marginLeft: "3px" }}>Audited</div>
+            </div>
+          ) : null}
         </div>
       </div>,
       {
         width: 200,
-        height: 50,
+        height: audited ? 60 : 45,
         fonts: [
           {
             name: "Inter",
