fd7b66230ac1a3cc4bafb5c1f7ee8f77c298b752
Manan Tank <manantankm@gmail.com>
2022-12-08 22:04:38 +0530

    Footer: Code Refactor + UI Adjustments

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,125 @@
+import {
+  FAUCETS,
+  FooterLinkInfo,
+  LEGAL,
+  NETWORKS,
+  RESOURCES,
+  SDKs,
+  SOLUTIONS,
+} from "./footerLinks";
+import { SOCIALS } from "./socialLinks";
+import {
+  Box,
+  ButtonGroup,
+  Container,
+  Flex,
+  SimpleGrid,
+  Stack,
+} from "@chakra-ui/react";
+import { Logo } from "components/logo";
+import { PRODUCTS } from "components/product-pages/common/nav/DesktopMenu";
+import {
+  Heading,
+  LinkButton,
+  TrackedIconButton,
+  TrackedLink,
+} from "tw-components";
+
+type FooterLinkGroupProps = {
+  heading: string;
+  links: FooterLinkInfo[];
+};
+
+function FooterLinkGroup(props: FooterLinkGroupProps) {
+  return (
+    <Stack spacing="4" minW="36" flex="1" mb={{ md: 12, base: 14 }}>
+      <Heading as="h5" size="label.lg">
+        {props.heading}
+      </Heading>
+      <Stack spacing="3" shouldWrapChildren>
+        {props.links.map((link) => (
+          <TrackedLink
+            isExternal={link.isExternal}
+            key={link.label}
+            href={link.link}
+            category="footer"
+            label={link.label}
+          >
+            {link.name}
+          </TrackedLink>
+        ))}
+      </Stack>
+    </Stack>
+  );
+}
+
+function FooterLinksGrid() {
+  return (
+    <Flex
+      mb={10}
+      direction={{ base: "column-reverse", md: "column", lg: "row" }}
+      gap={{ base: "12", md: "8" }}
+    >
+      <SimpleGrid columns={{ base: 2, lg: 4 }} spacing="8">
+        <FooterLinkGroup heading="Products" links={PRODUCTS} />
+        <FooterLinkGroup heading="Resources" links={RESOURCES} />
+        <div>
+          <FooterLinkGroup heading="SDKs" links={SDKs} />
+          <FooterLinkGroup heading="Solutions" links={SOLUTIONS} />
+        </div>
+        <div>
+          <FooterLinkGroup heading="Networks" links={NETWORKS} />
+          <FooterLinkGroup heading="Faucets" links={FAUCETS} />
+          <FooterLinkGroup heading="Legal" links={LEGAL} />
+        </div>
+      </SimpleGrid>
+    </Flex>
+  );
+}
+
+function SocialIcons() {
+  return (
+    <ButtonGroup variant="ghost">
+      {SOCIALS.map((data) => (
+        <TrackedIconButton
+          key={data.link}
+          as={LinkButton}
+          isExternal
+          noIcon
+          href={data.link}
+          icon={data.icon}
+          category="footer"
+          aria-label={data.ariaLabel}
+          label={data.label}
+        />
+      ))}
+    </ButtonGroup>
+  );
+}
+
+export function HomepageFooter() {
+  return (
+    <Box bgColor="#111315" zIndex="100">
+      <Container as="footer" maxW="container.page" color="gray.500">
+        <Stack
+          spacing="8"
+          direction={{ base: "column", md: "row" }}
+          justify="space-between"
+          py={{ base: "12", md: "16" }}
+        >
+          {/* logo + social icons */}
+          <Stack
+            spacing={{ base: "6", md: "8" }}
+            align={{ base: "center", md: "start" }}
+            mb={12}
+          >
+            <Logo color="#fff" />
+            <SocialIcons />
+          </Stack>
+
+          <FooterLinksGrid />
+        </Stack>
+      </Container>
+    </Box>
+  );
+}
---
+++ components/footer/footerLinks.tsx
@@ -0,0 +1,115 @@
+export type FooterLinkInfo = {
+  label: string;
+  name: string;
+  link: string;
+  isExternal?: boolean;
+};
+
+export const SOLUTIONS: FooterLinkInfo[] = [
+  {
+    name: "CommerceKit",
+    label: "commerce",
+    link: "/solutions/commerce",
+  },
+  {
+    link: "/solutions/gaming",
+    name: "GamingKit",
+    label: "gaming",
+  },
+];
+
+export const RESOURCES: FooterLinkInfo[] = [
+  {
+    label: "about",
+    name: "About",
+    link: "/about",
+  },
+  {
+    link: "https://thirdweb.typeform.com/to/ZV3gUhiP",
+    isExternal: true,
+    name: "Partner with us",
+    label: "sales-form",
+  },
+  {
+    name: "Docs",
+    isExternal: true,
+    link: "https://portal.thirdweb.com",
+    label: "portal",
+  },
+  {
+    name: "Guides",
+    label: "guides",
+    link: "https://blog.thirdweb.com/guides",
+    isExternal: true,
+  },
+  {
+    name: "Blog",
+    label: "blog",
+    link: "https://blog.thirdweb.com/",
+    isExternal: true,
+  },
+  {
+    name: "Careers",
+    label: "careers",
+    link: "https://careers.thirdweb.com/",
+    isExternal: true,
+  },
+];
+
+export const SDKs: FooterLinkInfo[] = [
+  {
+    label: "javascript",
+    name: "JavaScript",
+    link: "https://portal.thirdweb.com/typescript",
+    isExternal: true,
+  },
+  {
+    label: "react",
+    name: "React",
+    link: "https://portal.thirdweb.com/react",
+    isExternal: true,
+  },
+  {
+    label: "python",
+    name: "Python",
+    link: "https://portal.thirdweb.com/python",
+    isExternal: true,
+  },
+  {
+    label: "contracts",
+    name: "Contracts",
+    link: "https://portal.thirdweb.com/contracts",
+    isExternal: true,
+  },
+];
+
+export const NETWORKS: FooterLinkInfo[] = [
+  {
+    name: "Solana",
+    label: "network-solana",
+    link: "/network/solana",
+  },
+];
+
+export const FAUCETS: FooterLinkInfo[] = [
+  {
+    name: "Solana",
+    label: "faucet-solana",
+    link: "/faucet/solana",
+  },
+];
+
+export const LEGAL: FooterLinkInfo[] = [
+  {
+    name: "Privacy Policy",
+    label: "privacy",
+    isExternal: true,
+    link: "/privacy",
+  },
+  {
+    name: "Terms of Service",
+    label: "terms",
+    isExternal: true,
+    link: "/tos",
+  },
+];
