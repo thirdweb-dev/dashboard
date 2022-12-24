67cf2ea4486fc49b3ab64a8f717c31f197036629
Manan Tank <manantankm@gmail.com>
2022-12-24 22:41:44 +0530

    PWA: app name correction

--- components/PWA/PWAMeta.tsx
+++ components/PWA/PWAMeta.tsx
@@ -1,10 +1,10 @@
 export function PWAMeta() {
   return (
     <>
-      <meta name="application-name" content="Thirdweb" />
+      <meta name="application-name" content="thirdweb" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-      <meta name="apple-mobile-web-app-title" content="Thirdweb" />
+      <meta name="apple-mobile-web-app-title" content="thirdweb" />
       <meta name="format-detection" content="telephone=no" />
       <meta name="mobile-web-app-capable" content="yes" />
       <meta name="theme-color" content="#1b2129" />
