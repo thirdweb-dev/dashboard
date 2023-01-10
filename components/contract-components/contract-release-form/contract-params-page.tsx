fd4cde534782193afa1151ad11c2b011393490d3
Nacho Iacovino <nacho.iacovino@gmail.com>
2023-01-10 20:00:18 +0100

    Extra contract params page

---
+++ components/contract-components/contract-release-form/contract-params-page.tsx
@@ -0,0 +1,130 @@
+import {
+  useConstructorParamsFromABI,
+  useFunctionParamsFromABI,
+} from "../hooks";
+import {
+  Card,
+  Code,
+  Divider,
+  Flex,
+  FormControl,
+  Input,
+  Textarea,
+} from "@chakra-ui/react";
+import { getTemplateValuesForType } from "lib/deployment/tempalte-values";
+import { useFormContext } from "react-hook-form";
+import { FormHelperText, FormLabel, Heading, Text } from "tw-components";
+
+interface ContractParamsPageProps {
+  isDisabled?: boolean;
+  deployParams:
+    | ReturnType<typeof useFunctionParamsFromABI>
+    | ReturnType<typeof useConstructorParamsFromABI>;
+}
+
+export const ContractParamsPage: React.FC<ContractParamsPageProps> = ({
+  deployParams,
+  isDisabled = false,
+}) => {
+  const form = useFormContext();
+  return (
+    <Flex gap={16} direction="column">
+      <Flex gap={2} direction="column">
+        <Heading size="title.lg">Contract Parameters</Heading>
+        <Text fontStyle="normal">
+          These are the parameters users will need to fill inwhen deploying this
+          contract.
+        </Text>
+      </Flex>
+      <Flex flexDir="column" gap={10}>
+        {deployParams.map((param, idx) => {
+          const paramTemplateValues = getTemplateValuesForType(param.type);
+          return (
+            <Flex flexDir="column" gap={6} key={`implementation_${param.name}`}>
+              <Flex justify="space-between" align="center">
+                <Heading size="title.sm">{param.name}</Heading>
+                <Text size="body.sm">{param.type}</Text>
+              </Flex>
+              <Flex gap={6}>
+                <Flex flexDir="column" gap={4} w="60%">
+                  <FormControl isInvalid={!!form.formState.errors[param.name]}>
+                    <FormLabel flex="1" as={Text}>
+                      Display Name
+                    </FormLabel>
+                    <Input
+                      {...form.register(
+                        `constructorParams.${param.name}.displayName`,
+                      )}
+                      placeholder={param.name}
+                      disabled={isDisabled}
+                    />
+                  </FormControl>
+                  <FormControl isInvalid={!!form.formState.errors[param.name]}>
+                    <FormLabel as={Text}>Default Value</FormLabel>
+
+                    <Input
+                      {...form.register(
+                        `constructorParams.${param.name}.defaultValue`,
+                      )}
+                      disabled={isDisabled}
+                    />
+
+                    <FormHelperText>
+                      This value will be pre-filled in the deploy form.
+                      {paramTemplateValues.length > 0 && (
+                        <Flex
+                          as={Card}
+                          mt={3}
+                          borderRadius="md"
+                          py={3}
+                          px={3}
+                          direction="column"
+                          gap={2}
+                        >
+                          <Heading as="h5" size="label.sm">
+                            Supported template variables
+                          </Heading>
+                          <Flex direction="column">
+                            {paramTemplateValues.map((val) => (
+                              <Text size="body.sm" key={val.value}>
+                                <Code
+                                  as="button"
+                                  type="button"
+                                  display="inline"
+                                  onClick={() => {
+                                    form.setValue(
+                                      `constructorParams.${param.name}.defaultValue`,
+                                      val.value,
+                                    );
+                                  }}
+                                >
+                                  {val.value}
+                                </Code>{" "}
+                                - {val.helperText}
+                              </Text>
+                            ))}
+                          </Flex>
+                        </Flex>
+                      )}
+                    </FormHelperText>
+                  </FormControl>
+                </Flex>
+                <Flex flexDir="column" w="full">
+                  <FormLabel as={Text}>Description</FormLabel>
+                  <Textarea
+                    {...form.register(
+                      `constructorParams.${param.name}.description`,
+                    )}
+                    disabled={isDisabled}
+                    h="full"
+                  />
+                </Flex>
+              </Flex>
+              {idx !== deployParams.length - 1 ? <Divider mt={8} /> : null}
+            </Flex>
+          );
+        })}
+      </Flex>
+    </Flex>
+  );
+};
--- components/contract-components/contract-release-form/index.tsx
+++ components/contract-components/contract-release-form/index.tsx
@@ -11,6 +11,7 @@ import {
 import { MarkdownRenderer } from "../released-contract/markdown-renderer";
 import { ContractId } from "../types";
 import { PasteInput } from "./PasteInput";
+import { ContractParamsPage } from "./contract-params-page";
 import { useWeb3 } from "@3rdweb-sdk/react";
 import {
   Box,
@@ -598,116 +599,7 @@ export const ContractReleaseForm: React.FC<ContractReleaseFormProps> = ({
             </Flex>
           )}
           {pageToShow === "contractParams" && (
-            <Flex gap={16} direction="column">
-              <Flex gap={2} direction="column">
-                <Heading size="title.lg">Contract Parameters</Heading>
-                <Text fontStyle="normal">
-                  These are the parameters users will need to fill inwhen
-                  deploying this contract.
-                </Text>
-              </Flex>
-              <Flex flexDir="column" gap={10}>
-                {deployParams.map((param, idx) => {
-                  const paramTemplateValues = getTemplateValuesForType(
-                    param.type,
-                  );
-                  return (
-                    <Flex
-                      flexDir="column"
-                      gap={6}
-                      key={`implementation_${param.name}`}
-                    >
-                      <Flex justify="space-between" align="center">
-                        <Heading size="title.sm">{param.name}</Heading>
-                        <Text size="body.sm">{param.type}</Text>
-                      </Flex>
-                      <Flex gap={6}>
-                        <Flex flexDir="column" gap={4} w="60%">
-                          <FormControl
-                            isInvalid={!!form.formState.errors[param.name]}
-                          >
-                            <FormLabel flex="1" as={Text}>
-                              Display Name
-                            </FormLabel>
-                            <Input
-                              {...form.register(
-                                `constructorParams.${param.name}.displayName`,
-                              )}
-                              placeholder={param.name}
-                              disabled={isDisabled}
-                            />
-                          </FormControl>
-                          <FormControl
-                            isInvalid={!!form.formState.errors[param.name]}
-                          >
-                            <FormLabel as={Text}>Default Value</FormLabel>
-
-                            <Input
-                              {...form.register(
-                                `constructorParams.${param.name}.defaultValue`,
-                              )}
-                              disabled={isDisabled}
-                            />
-
-                            <FormHelperText>
-                              This value will be pre-filled in the deploy form.
-                              {paramTemplateValues.length > 0 && (
-                                <Flex
-                                  as={Card}
-                                  mt={3}
-                                  borderRadius="md"
-                                  py={3}
-                                  px={3}
-                                  direction="column"
-                                  gap={2}
-                                >
-                                  <Heading as="h5" size="label.sm">
-                                    Supported template variables
-                                  </Heading>
-                                  <Flex direction="column">
-                                    {paramTemplateValues.map((val) => (
-                                      <Text size="body.sm" key={val.value}>
-                                        <Code
-                                          as="button"
-                                          type="button"
-                                          display="inline"
-                                          onClick={() => {
-                                            form.setValue(
-                                              `constructorParams.${param.name}.defaultValue`,
-                                              val.value,
-                                            );
-                                          }}
-                                        >
-                                          {val.value}
-                                        </Code>{" "}
-                                        - {val.helperText}
-                                      </Text>
-                                    ))}
-                                  </Flex>
-                                </Flex>
-                              )}
-                            </FormHelperText>
-                          </FormControl>
-                        </Flex>
-                        <Flex flexDir="column" w="full">
-                          <FormLabel as={Text}>Description</FormLabel>
-                          <Textarea
-                            {...form.register(
-                              `constructorParams.${param.name}.description`,
-                            )}
-                            disabled={isDisabled}
-                            h="full"
-                          />
-                        </Flex>
-                      </Flex>
-                      {idx !== deployParams.length - 1 ? (
-                        <Divider mt={8} />
-                      ) : null}
-                    </Flex>
-                  );
-                })}
-              </Flex>
-            </Flex>
+            <ContractParamsPage deployParams={deployParams} />
           )}
           {pageToShow === "proxy" && (
             <Flex gap={12} direction="column">
