50e9821be6ae09a646165cc919a62d6120a26e6d
Manan Tank <manantankm@gmail.com>
2023-01-17 17:02:28 +0530

    split partners logo among two rows on mobile

--- components/partners/carousel.tsx
+++ components/partners/carousel.tsx
@@ -1,10 +1,14 @@
 import { PartnerLogo } from "./partner-logo";
 import styles from "./partner-logo.module.css";
-import { Box } from "@chakra-ui/react";
+import { Box, LayoutProps } from "@chakra-ui/react";
+import React from "react";
 
 const gap = { base: "40px", lg: "60px" };
 
-const MarqueeGroup: React.FC<{ ariaHidden: boolean }> = ({ ariaHidden }) => {
+const MarqueeGroup: React.FC<{
+  ariaHidden: boolean;
+  children: React.ReactNode;
+}> = ({ ariaHidden, children }) => {
   return (
     <Box
       display="flex"
@@ -14,18 +18,26 @@ const MarqueeGroup: React.FC<{ ariaHidden: boolean }> = ({ ariaHidden }) => {
       aria-hidden={ariaHidden}
       className={styles.marqueeGroup}
     >
-      <PartnerLogo partner="rarible" />
-      <PartnerLogo partner="fractal" />
-      <PartnerLogo partner="buildspace" />
-      <PartnerLogo partner="shopify" />
-      <PartnerLogo partner="paradigm" />
-      <PartnerLogo partner="unlock" />
-      <PartnerLogo partner="minted" />
-      <PartnerLogo partner="nyfw" />
-      <PartnerLogo partner="gala_games" />
-      <PartnerLogo partner="mirror" />
-      <PartnerLogo partner="heroic_story" />
-      <PartnerLogo partner="layer3" />
+      {children}
+    </Box>
+  );
+};
+
+const Marquee: React.FC<{
+  children: React.ReactNode;
+  className?: string;
+  display?: LayoutProps["display"];
+}> = ({ children, className, display }) => {
+  return (
+    <Box
+      gap={gap}
+      overflow="hidden"
+      py={4}
+      display={display}
+      className={className}
+    >
+      <MarqueeGroup ariaHidden={false}> {children} </MarqueeGroup>
+      <MarqueeGroup ariaHidden={true}> {children} </MarqueeGroup>
     </Box>
   );
 };
@@ -40,22 +52,43 @@ export const PartnerCarousel: React.FC = () => {
       pt={10}
       mb={{ base: 20, md: 24 }}
     >
-      <Box display="flex" gap={gap} overflow="hidden" py={4}>
-        <MarqueeGroup ariaHidden={false} />
-        <MarqueeGroup ariaHidden={true} />
-      </Box>
+      {/* desktop - 1 row with all logos */}
+      <Marquee display={{ base: "none", lg: "flex" }}>
+        <PartnerLogo partner="rarible" />
+        <PartnerLogo partner="fractal" />
+        <PartnerLogo partner="buildspace" />
+        <PartnerLogo partner="shopify" />
+        <PartnerLogo partner="paradigm" />
+        <PartnerLogo partner="unlock" />
+        <PartnerLogo partner="minted" />
+        <PartnerLogo partner="nyfw" />
+        <PartnerLogo partner="gala_games" />
+        <PartnerLogo partner="mirror" />
+        <PartnerLogo partner="heroic_story" />
+        <PartnerLogo partner="layer3" />
+      </Marquee>
 
-      {/* Add extra row for mobile */}
-      <Box
-        gap={gap}
-        py={4}
-        overflow="hidden"
-        display={{ base: "flex", md: "none" }}
+      {/* mobile - 2 rows with logos split in two rows */}
+      <Marquee display={{ base: "flex", lg: "none" }}>
+        <PartnerLogo partner="rarible" />
+        <PartnerLogo partner="fractal" />
+        <PartnerLogo partner="buildspace" />
+        <PartnerLogo partner="shopify" />
+        <PartnerLogo partner="paradigm" />
+        <PartnerLogo partner="unlock" />
+      </Marquee>
+
+      <Marquee
         className={styles.marqueeReverse}
+        display={{ base: "flex", lg: "none" }}
       >
-        <MarqueeGroup ariaHidden={false} />
-        <MarqueeGroup ariaHidden={true} />
-      </Box>
+        <PartnerLogo partner="minted" />
+        <PartnerLogo partner="nyfw" />
+        <PartnerLogo partner="gala_games" />
+        <PartnerLogo partner="mirror" />
+        <PartnerLogo partner="heroic_story" />
+        <PartnerLogo partner="layer3" />
+      </Marquee>
     </Box>
   );
 };
