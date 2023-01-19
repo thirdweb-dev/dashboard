de3bab84236e833c93457661c5cc564db48d81ef
Nacho Iacovino <nacho.iacovino@gmail.com>
2023-01-19 06:53:48 +0100

    Add ENS resolve to address input

--- contract-ui/components/solidity-inputs/address-input.tsx
+++ contract-ui/components/solidity-inputs/address-input.tsx
@@ -1,32 +1,68 @@
-import { Input, InputProps } from "@chakra-ui/react";
+import {
+  Input,
+  InputGroup,
+  InputProps,
+  InputRightElement,
+} from "@chakra-ui/react";
+import { useEns } from "components/contract-components/hooks";
 import { utils } from "ethers";
 import { useFormContext } from "react-hook-form";
+import { Button } from "tw-components";
 
 export const SolidityAddressInput: React.FC<InputProps> = ({
   ...inputProps
 }) => {
-  const { setValue, setError, clearErrors } = useFormContext();
+  const { setValue, setError, clearErrors, watch } = useFormContext();
+  const ensQuery = useEns(watch(inputProps.name as string));
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const val = e.target.value;
-    if (utils.isAddress(val) === false) {
+    setValue(inputProps.name as string, val);
+    if (utils.isAddress(val) === false && !val.endsWith(".eth")) {
       setError(inputProps.name as string, {
         type: "pattern",
         message: "Address is not a valid address.",
       });
     } else {
-      setValue(inputProps.name as string, val);
       clearErrors(inputProps.name as string);
     }
   };
 
+  const handleConversion = () => {
+    if (ensQuery?.data?.address) {
+      setValue(inputProps.name as string, ensQuery.data.address);
+      clearErrors(inputProps.name as string);
+    } else {
+      setError(inputProps.name as string, {
+        type: "pattern",
+        message: "ENS couldn't be resolved. Please try again.",
+      });
+    }
+  };
+
   return (
-    <Input
-      {...inputProps}
-      pattern="^0x[a-fA-F0-9]{40}$"
-      maxLength={42}
-      type="text"
-      onChange={handleChange}
-    />
+    <InputGroup>
+      <Input
+        {...inputProps}
+        value={watch(inputProps.name as string)}
+        onChange={handleChange}
+      />
+      {watch(inputProps.name as string).endsWith(".eth") && (
+        <InputRightElement width="96px">
+          <Button
+            size="xs"
+            padding={3.5}
+            paddingY="3.5"
+            colorScheme="purple"
+            aria-label="Convert to bytes"
+            onClick={handleConversion}
+            mr={2}
+            disabled={ensQuery.isLoading}
+          >
+            Resolve ENS
+          </Button>
+        </InputRightElement>
+      )}
+    </InputGroup>
   );
 };
