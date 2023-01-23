e9dce95648aecd17c4b5ab1cfca506cc582f5e42
Nacho Iacovino <nacho.iacovino@gmail.com>
2023-01-23 23:57:06 +0100

    Fix validation

--- contract-ui/components/solidity-inputs/bytes-input.tsx
+++ contract-ui/components/solidity-inputs/bytes-input.tsx
@@ -3,33 +3,31 @@ import { Input } from "@chakra-ui/react";
 import { isBytesLike } from "ethers/lib/utils";
 
 const isValidBytes = (value: string, solidityType: string) => {
+  const isBytesType = solidityType === "bytes";
+
   const maxLength =
     solidityType === "byte"
       ? 1
-      : solidityType === "bytes"
-      ? 32
-      : parseInt(solidityType.replace("bytes", ""));
+      : parseInt(solidityType.replace("bytes", "") || "0", 10);
 
-  if (value === "0x") {
+  if (value === "[]" || value === "0x00") {
     return true;
   }
 
   if (value.startsWith("[") && value.endsWith("]")) {
     try {
       const arrayify = JSON.parse(value);
-      return arrayify.length === maxLength;
+      return isBytesType ? !!arrayify.length : arrayify.length === maxLength;
     } catch (error) {
       return false;
     }
   }
 
-  if (value.length !== maxLength) {
+  if (!isBytesType && value.length !== maxLength) {
     return false;
   }
 
-  if (isBytesLike(value)) {
-    return true;
-  }
+  return isBytesLike(value);
 };
 
 export const SolidityBytesInput: React.FC<SolidityInputWithTypeProps> = ({
@@ -43,10 +41,15 @@ export const SolidityBytesInput: React.FC<SolidityInputWithTypeProps> = ({
     const { value } = e.target;
     form.setValue(inputName, value, { shouldDirty: true });
 
-    if (!isValidBytes(value, solidityType)) {
+    if (!value.startsWith("0x") && !value.startsWith("[")) {
       form.setError(inputName, {
         type: "pattern",
-        message: `Value is not a valid ${solidityType}.`,
+        message: `Invalid input. Accepted formats are hex strings (0x...) or array of numbers ([...]).`,
+      });
+    } else if (!isValidBytes(value, solidityType)) {
+      form.setError(inputName, {
+        type: "pattern",
+        message: `Value is not a valid ${solidityType}. Please check the length.`,
       });
     } else {
       form.clearErrors(inputName);
