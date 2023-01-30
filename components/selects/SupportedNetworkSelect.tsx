e5692ac7a8dfa686b0404eb03a4538a09b95879a
Manan Tank <manantankm@gmail.com>
2023-01-31 00:15:56 +0530

    disable custom chains from dropdown on On non thirdweb contracts deploy page

--- components/selects/SupportedNetworkSelect.tsx
+++ components/selects/SupportedNetworkSelect.tsx
@@ -1,6 +1,8 @@
 import { Select, SelectProps, forwardRef } from "@chakra-ui/react";
+import { defaultChains } from "@thirdweb-dev/chains";
 import { ChainId, SUPPORTED_CHAIN_ID } from "@thirdweb-dev/sdk/evm";
 import { deprecatedChains } from "constants/mappings";
+import { StoredChain } from "contexts/configured-chains";
 import { useConfiguredChains } from "hooks/chains/configureChains";
 import { useMemo } from "react";
 
@@ -27,45 +29,70 @@ export const SupportedNetworkSelect = forwardRef<
     const mainnets = useMemo(() => {
       return configuredNetworks.filter((n) => !n.testnet);
     }, [configuredNetworks]);
-
     return (
       <Select {...selectProps} ref={ref}>
         <option disabled value={-1}>
           Select Network
         </option>
-        <optgroup label="Mainnets">
-          {mainnets.map((network) => (
-            <option
-              key={network.chainId}
-              value={network.chainId}
-              disabled={disabledChainIds?.includes(network.chainId)}
-            >
-              {network.name} ({network.nativeCurrency.symbol})
-              {disabledChainIds?.includes(network.chainId)
-                ? ` - ${disabledChainIdText}`
-                : ""}
-            </option>
-          ))}
-        </optgroup>
-        <optgroup label="Testnets">
-          {testnets.map((network) => (
-            <option
-              key={network.chainId}
-              value={network.chainId}
-              disabled={disabledChainIds?.includes(network.chainId)}
-            >
-              {network.name} ({network.nativeCurrency.symbol})
-              {deprecatedChains.includes(
-                network.chainId as SUPPORTED_CHAIN_ID,
-              ) && " - Deprecated"}
-              {disabledChainIds?.includes(network.chainId) &&
-              !deprecatedChains.includes(network.chainId as SUPPORTED_CHAIN_ID)
-                ? ` - ${disabledChainIdText}`
-                : ""}
-            </option>
-          ))}
-        </optgroup>
+        <NetworkOptGroup
+          label="Mainnets"
+          chains={mainnets}
+          disabledChainIdText={disabledChainIdText}
+          disabledChainIds={disabledChainIds}
+        />
+
+        <NetworkOptGroup
+          label="Testnets"
+          chains={testnets}
+          disabledChainIdText={disabledChainIdText}
+          disabledChainIds={disabledChainIds}
+        />
       </Select>
     );
   },
 );
+
+const isDefaultChain = (chainId: number) =>
+  defaultChains.some((c) => c.chainId === chainId);
+
+const NetworkOptGroup: React.FC<{
+  disabledChainIds?: ChainId[];
+  chains: StoredChain[];
+  disabledChainIdText: string;
+  label: string;
+}> = ({ disabledChainIds, chains, disabledChainIdText, label }) => {
+  return (
+    <optgroup label={label}>
+      {chains.map((network) => {
+        const isDisabledChain = disabledChainIds?.includes(network.chainId);
+        const isDeprecaredChain = deprecatedChains.includes(
+          network.chainId as SUPPORTED_CHAIN_ID,
+        );
+
+        // disable the option it if it is either in the list of disabled chains
+        // or if the list of disabled chains is given and chain is not a default chain ( custom chain )
+        const disableOption =
+          isDisabledChain ||
+          (disabledChainIds &&
+            disabledChainIds.length > 0 &&
+            !isDefaultChain(network.chainId));
+
+        const tag = isDeprecaredChain
+          ? " - Deprecated"
+          : disableOption
+          ? ` - ${disabledChainIdText}`
+          : "";
+
+        return (
+          <option
+            key={network.chainId}
+            value={network.chainId}
+            disabled={disableOption}
+          >
+            {network.name} ({network.nativeCurrency.symbol}) {tag}
+          </option>
+        );
+      })}
+    </optgroup>
+  );
+};
