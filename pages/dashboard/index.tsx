e6aa0ef2576f22cad30b306d5f696eaa3875d6d1
Nacho Iacovino <nacho.iacovino@gmail.com>
2023-02-14 00:40:54 +0100

    Add dashboard cards

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,9 +1,81 @@
+import { Flex, GridItem, LinkOverlay, SimpleGrid } from "@chakra-ui/react";
+import { ChakraNextImage } from "components/Image";
 import { AppLayout } from "components/app-layouts/app";
 import { PageId } from "page-id";
+import { Card, Heading, Text, TrackedLink } from "tw-components";
 import { ThirdwebNextPage } from "utils/types";
 
+const TRACKING_CATEGORY = "dashboard";
+
+const GET_STARTED_SECTIONS = [
+  {
+    title: "Deploy a contract",
+    description: "Start deploying contracts on-chain in just a few minutes.",
+    image: require("public/assets/dashboard/home-deploy.png"),
+    href: "/contracts",
+  },
+  {
+    title: "Browse contracts",
+    description:
+      "Explore contracts from world-class web3 protocols & engineers- all deployable with 1-click.",
+    image: require("public/assets/dashboard/home-contracts.png"),
+    href: "/explore",
+  },
+  {
+    title: "Browse templates",
+    description:
+      "Get inspired and start building your own web3 apps on top of our templates.",
+    image: require("public/assets/dashboard/home-templates.png"),
+    href: "https://portal.thirdweb.com/templates",
+    isExternal: true,
+  },
+];
+
 const Dashboard: ThirdwebNextPage = () => {
-  return <>hi</>;
+  return (
+    <SimpleGrid columns={{ base: 1, xl: 4 }} gap={8} mt={12}>
+      <GridItem as={Flex} colSpan={{ xl: 3 }} direction="column" gap={8}>
+        <Heading>Get started quickly</Heading>
+        <SimpleGrid columns={{ base: 1, md: 2 }} gap={6}>
+          {GET_STARTED_SECTIONS.map(
+            ({ title, description, image, href, isExternal }) => (
+              <LinkOverlay
+                key={title}
+                as={TrackedLink}
+                category={TRACKING_CATEGORY}
+                label={title}
+                href={href}
+                isExternal={isExternal}
+                _hover={{ textDecor: "none" }}
+                role="group"
+                overflow="hidden"
+                position="relative"
+              >
+                <Card p={0}>
+                  <Flex direction="column" gap={3} p={6}>
+                    <Heading
+                      size="title.xs"
+                      _groupHover={{ color: "blue.500" }}
+                      transitionDuration="200ms"
+                    >
+                      {title}
+                    </Heading>
+                    <Text>{description}</Text>
+                  </Flex>
+                  <Flex justifyContent="center">
+                    <ChakraNextImage src={image} alt="" />
+                  </Flex>
+                </Card>
+              </LinkOverlay>
+            ),
+          )}
+        </SimpleGrid>
+      </GridItem>
+      <GridItem as={Flex} direction="column" gap={6}>
+        <Heading size="title.sm">Latest changes</Heading>
+      </GridItem>
+    </SimpleGrid>
+  );
 };
 
 Dashboard.getLayout = (page, props) => <AppLayout {...props}>{page}</AppLayout>;
