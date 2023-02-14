64074ec44b08e92a4ecf72350166ed6991e4800c
Nacho Iacovino <nacho.iacovino@gmail.com>
2023-02-14 01:58:25 +0100

    Better styles

--- components/dashboard/Changelog.tsx
+++ components/dashboard/Changelog.tsx
@@ -6,18 +6,31 @@ import { Link, Text } from "tw-components";
 export const Changelog = () => {
   const changelogQuery = useChangelog();
   return (
-    <Flex flexDir="column" gap={2}>
+    <Flex flexDir="column" gap={2} position="relative">
+      <Flex
+        position="absolute"
+        h="96%"
+        borderRight="1px solid"
+        borderColor="gray.700"
+        left={{ base: "6px", md: "7px" }}
+        top="14px"
+      />
       {changelogQuery.data?.map((item) => (
-        <Flex key={item.title} flexDir="column">
-          <Link href={item.url}>{item.title}</Link>
-          <Text>
-            {formatDistance(new Date(item.published_at), Date.now(), {
-              addSuffix: true,
-            })}
+        <Flex key={item.title} gap={4}>
+          <Text color="gray.700" size="body.2xl">
+            &#9679;
           </Text>
+          <Flex flexDir="column">
+            <Link href={item.url}>{item.title}</Link>
+            <Text>
+              {formatDistance(new Date(item.published_at), Date.now(), {
+                addSuffix: true,
+              })}
+            </Text>
+          </Flex>
         </Flex>
       ))}
-      <Link href="https://blog.thirdweb.com/changelog/" isExternal>
+      <Link href="https://blog.thirdweb.com/changelog/" isExternal ml={8}>
         <Text>View all changes ➝</Text>
       </Link>
     </Flex>
