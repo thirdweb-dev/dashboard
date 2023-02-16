9612ab746bfd7355ed842d2991b858b3164ba363
sid <hi@sid.fyi>
2023-02-15 19:10:05 -0800

    fix

--- contract-ui/tabs/overview/components/MarketplaceTable.tsx
+++ contract-ui/tabs/overview/components/MarketplaceTable.tsx
@@ -1,24 +1,9 @@
 import { MediaCell } from "../../../../components/contract-pages/table/table-columns/cells/media-cell";
 import { ListingCards } from "../../listings/components/listings-cards";
-import {
-  Box,
-  Flex,
-  SimpleGrid,
-  Table,
-  Tbody,
-  Td,
-  Th,
-  Thead,
-  Tr,
-} from "@chakra-ui/react";
-import {
-  useActiveListings,
-  useAllRoleMembers,
-  useContract,
-} from "@thirdweb-dev/react";
+import { Flex, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import { useActiveListings, useContract } from "@thirdweb-dev/react";
 import { AuctionListing, DirectListing } from "@thirdweb-dev/sdk/evm";
 import { useTabHref } from "contract-ui/utils";
-import { motion } from "framer-motion";
 import React, { useMemo } from "react";
 import { Cell, CellProps, Column, useTable } from "react-table";
 import { Card, Heading, Text, TrackedLinkProps } from "tw-components";
@@ -39,8 +24,8 @@ export const MarketplaceTable: React.FC<MarketplaceTableProps> = ({
   const listingsHref = useTabHref("listings");
   const listingsQuery = useActiveListings(contract?.contract);
   const listingsToShow = useMemo(
-    () => listingsQuery?.data?.slice(0, 5) || [],
-    [listingsQuery?.data],
+    () => listingsQuery.data?.slice(0, 5) || [],
+    [listingsQuery.data],
   );
 
   const tableColumns: Column<Listing>[] = [
@@ -106,35 +91,6 @@ export const MarketplaceTable: React.FC<MarketplaceTableProps> = ({
       <ListingCards contract={contract?.contract} />
       {contract && (
         <Card p={0} overflow="hidden">
-          <SimpleGrid
-            gap={2}
-            columns={18}
-            borderBottomWidth="1px"
-            borderColor="borderColor"
-            padding={4}
-            bg="blackAlpha.50"
-            _dark={{ bg: "whiteAlpha.50" }}
-          >
-            <Heading gridColumn="span 2" size="label.md">
-              ID
-            </Heading>
-            <Heading gridColumn="span 3" size="label.md">
-              Media
-            </Heading>
-            <Heading gridColumn="span 3" size="label.md">
-              Name
-            </Heading>
-            <Heading gridColumn="span 4" size="label.md">
-              Seller
-            </Heading>
-            <Heading gridColumn="span 3" size="label.md">
-              Price
-            </Heading>
-            <Heading gridColumn="span 2" size="label.md">
-              Type
-            </Heading>
-          </SimpleGrid>
-
           <Table {...tableInstance.getTableProps()}>
             <Thead>
               {tableInstance.headerGroups.map((headerGroup) => (
@@ -178,57 +134,5 @@ export const MarketplaceTable: React.FC<MarketplaceTableProps> = ({
         </Card>
       )}
     </Flex>
-  );
-};
-
-interface ListingProps {
-  data: Listing;
-}
-
-export const Listing: React.FC<ListingProps> = ({ data }) => {
-  return (
-    <Box>
-      <SimpleGrid
-        columns={16}
-        gap={2}
-        as={motion.li}
-        initial={{
-          y: -30,
-          opacity: 0,
-          paddingTop: 0,
-          paddingBottom: 0,
-          height: 0,
-        }}
-        animate={{
-          y: 0,
-          opacity: 1,
-          height: "auto",
-          paddingTop: "var(--chakra-space-3)",
-          paddingBottom: "var(--chakra-space-3)",
-          transition: { duration: 0.15 },
-        }}
-        exit={{
-          y: 30,
-          opacity: 0,
-          paddingTop: 0,
-          paddingBottom: 0,
-          height: 0,
-          transition: { duration: 0.3 },
-        }}
-        willChange="opacity, height, paddingTop, paddingBottom"
-        borderBottomWidth="1px"
-        borderColor="borderColor"
-        padding={4}
-        overflow="hidden"
-        alignItems="center"
-        _last={{ borderBottomWidth: 0 }}
-      >
-        <Box gridColumn="span 1">
-          <Text>{data.id}</Text>
-        </Box>
-
-        <Box gridColumn="span 1" />
-      </SimpleGrid>
-    </Box>
   );
 };
