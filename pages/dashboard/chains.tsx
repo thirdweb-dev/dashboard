accefd9b830b6b53df796f3026c06eded8e37522
Nacho Iacovino <nacho.iacovino@gmail.com>
2023-02-17 19:26:07 +0100

    Link to chain page

--- pages/dashboard/chains.tsx
+++ pages/dashboard/chains.tsx
@@ -1,4 +1,4 @@
-import { Flex, Icon, SimpleGrid } from "@chakra-ui/react";
+import { Flex, Icon, LinkOverlay, SimpleGrid } from "@chakra-ui/react";
 import { Chain, allChains } from "@thirdweb-dev/chains";
 import { AppLayout } from "components/app-layouts/app";
 import { ChainIcon } from "components/icons/ChainIcon";
@@ -18,29 +18,43 @@ export const DashboardChains: ThirdwebNextPage = () => {
       <Flex>search</Flex>
       <SimpleGrid columns={{ base: 1, lg: 3 }} gap={6}>
         {chains.map((chain) => (
-          <Card key={chain.chainId} as={Flex} flexDir="column" gap={6} p={6}>
-            <Flex justifyContent="space-between">
-              <Flex alignItems="center" gap={2}>
-                <ChainIcon size={20} ipfsSrc={chain?.icon?.url} sizes={[]} />
-                <Heading size="subtitle.sm" as="h3" noOfLines={1}>
-                  {chain.name}
-                </Heading>
+          <LinkOverlay
+            key={chain.chainId}
+            href={`/${chain.slug}`}
+            position="relative"
+            role="group"
+          >
+            <Card
+              as={Flex}
+              flexDir="column"
+              gap={6}
+              p={6}
+              bg="transparent"
+              _groupHover={{ borderColor: "blue.500" }}
+            >
+              <Flex justifyContent="space-between">
+                <Flex alignItems="center" gap={2}>
+                  <ChainIcon size={20} ipfsSrc={chain?.icon?.url} sizes={[]} />
+                  <Heading size="subtitle.sm" as="h3" noOfLines={1}>
+                    {chain.name}
+                  </Heading>
+                </Flex>
+                <Flex alignItems="center">
+                  <Icon as={BsCheck2Circle} color="green.500" boxSize={6} />
+                </Flex>
               </Flex>
-              <Flex alignItems="center">
-                <Icon as={BsCheck2Circle} color="green.500" boxSize={6} />
+              <Flex gap={12}>
+                <Flex flexDir="column" gap={1}>
+                  <Text color="paragraph">Chain ID</Text>
+                  <Text size="label.md">{chain.chainId}</Text>
+                </Flex>
+                <Flex flexDir="column" gap={1}>
+                  <Text color="paragraph">Native Token</Text>
+                  <Text size="label.md">{chain.nativeCurrency.symbol}</Text>
+                </Flex>
               </Flex>
-            </Flex>
-            <Flex gap={12}>
-              <Flex flexDir="column" gap={1}>
-                <Text color="paragraph">Chain ID</Text>
-                <Text size="label.md">{chain.chainId}</Text>
-              </Flex>
-              <Flex flexDir="column" gap={1}>
-                <Text color="paragraph">Native Token</Text>
-                <Text size="label.md">{chain.nativeCurrency.symbol}</Text>
-              </Flex>
-            </Flex>
-          </Card>
+            </Card>
+          </LinkOverlay>
         ))}
       </SimpleGrid>
     </Flex>
