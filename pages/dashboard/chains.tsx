2e594363601e19a9327e805fe7ce780b3fe269a2
Nacho Iacovino <nacho.iacovino@gmail.com>
2023-02-17 19:18:14 +0100

    Render all chains

--- pages/dashboard/chains.tsx
+++ pages/dashboard/chains.tsx
@@ -1,4 +1,5 @@
 import { Flex, Icon, SimpleGrid } from "@chakra-ui/react";
+import { Chain, allChains } from "@thirdweb-dev/chains";
 import { AppLayout } from "components/app-layouts/app";
 import { ChainIcon } from "components/icons/ChainIcon";
 import { PageId } from "page-id";
@@ -7,6 +8,8 @@ import { Card, Heading, Text } from "tw-components";
 import { ThirdwebNextPage } from "utils/types";
 
 export const DashboardChains: ThirdwebNextPage = () => {
+  const chains: Chain[] = allChains;
+
   return (
     <Flex flexDir="column" gap={8} mt={10}>
       <Heading size="title.lg" as="h1">
@@ -14,29 +17,31 @@ export const DashboardChains: ThirdwebNextPage = () => {
       </Heading>
       <Flex>search</Flex>
       <SimpleGrid columns={{ base: 1, lg: 3 }} gap={6}>
-        <Card as={Flex} flexDir="column" gap={6} p={6}>
-          <Flex justifyContent="space-between">
-            <Flex alignItems="center" gap={2}>
-              <ChainIcon size={20} ipfsSrc="" sizes={[]} />
-              <Heading size="subtitle.sm" as="h3">
-                Ethereum
-              </Heading>
+        {chains.map((chain) => (
+          <Card key={chain.chainId} as={Flex} flexDir="column" gap={6} p={6}>
+            <Flex justifyContent="space-between">
+              <Flex alignItems="center" gap={2}>
+                <ChainIcon size={20} ipfsSrc={chain?.icon?.url} sizes={[]} />
+                <Heading size="subtitle.sm" as="h3" noOfLines={1}>
+                  {chain.name}
+                </Heading>
+              </Flex>
+              <Flex alignItems="center">
+                <Icon as={BsCheck2Circle} color="green.500" boxSize={6} />
+              </Flex>
             </Flex>
-            <Flex alignItems="center">
-              <Icon as={BsCheck2Circle} color="green.500" boxSize={6} />
+            <Flex gap={12}>
+              <Flex flexDir="column" gap={1}>
+                <Text color="paragraph">Chain ID</Text>
+                <Text size="label.md">{chain.chainId}</Text>
+              </Flex>
+              <Flex flexDir="column" gap={1}>
+                <Text color="paragraph">Native Token</Text>
+                <Text size="label.md">{chain.nativeCurrency.symbol}</Text>
+              </Flex>
             </Flex>
-          </Flex>
-          <Flex gap={12}>
-            <Flex flexDir="column" gap={1}>
-              <Text color="paragraph">Chain ID</Text>
-              <Text size="label.md">5</Text>
-            </Flex>
-            <Flex flexDir="column" gap={1}>
-              <Text color="paragraph">Native Token</Text>
-              <Text size="label.md">ETH</Text>
-            </Flex>
-          </Flex>
-        </Card>
+          </Card>
+        ))}
       </SimpleGrid>
     </Flex>
   );
