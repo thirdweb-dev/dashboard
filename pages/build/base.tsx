937f59cf3ac74f520b0623069bbcd5ff879e43cf
sid <hi@sid.fyi>
2023-03-13 20:54:06 -0700

    small 2 col

--- pages/build/base.tsx
+++ pages/build/base.tsx
@@ -210,7 +210,7 @@ const Base: ThirdwebNextPage = () => {
                 >
                   Clone the code
                 </Heading>
-                <SimpleGrid columns={2} gap={4}>
+                <SimpleGrid columns={{ sm: 2 }} gap={4}>
                   {[
                     {
                       title: "Web",
