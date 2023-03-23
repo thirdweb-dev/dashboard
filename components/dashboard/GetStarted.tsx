ead414952a69c34b3e5de1a20628c4944b4c320c
sid <hi@sid.fyi>
2023-03-22 21:28:17 -0700

    small fix

--- components/dashboard/GetStarted.tsx
+++ components/dashboard/GetStarted.tsx
@@ -41,103 +41,103 @@ export const GetStarted: React.FC<GetStartedProps> = ({
     }
   }, [firstIncomplete, setIsHidden]);
 
-  return (
-    !isHidden && (
-      <Card
-        flexDir="column"
-        p={8}
-        gap={4}
-        position="relative"
-        maxW="3xl"
-        onClick={() => !isOpen && setIsOpen(true)}
+  return isHidden ? (
+    <></>
+  ) : (
+    <Card
+      flexDir="column"
+      p={8}
+      gap={4}
+      position="relative"
+      maxW="3xl"
+      onClick={() => !isOpen && setIsOpen(true)}
+    >
+      <Box
+        onClick={() => setIsHidden(true)}
+        as="button"
+        position="absolute"
+        top={2}
+        right={2}
+        color="gray.700"
       >
-        <Box
-          onClick={() => setIsHidden(true)}
+        <FiX />
+      </Box>
+      <Flex justifyContent="space-between" alignItems="center">
+        <Text size="label.xl" color="white">
+          {title}
+        </Text>
+        <Flex
           as="button"
-          position="absolute"
-          top={2}
-          right={2}
-          color="gray.700"
+          type="button"
+          alignItems="center"
+          onClick={() => setIsOpen((prev) => !prev)}
         >
-          <FiX />
-        </Box>
-        <Flex justifyContent="space-between" alignItems="center">
-          <Text size="label.xl" color="white">
-            {title}
-          </Text>
-          <Flex
-            as="button"
-            type="button"
-            alignItems="center"
-            onClick={() => setIsOpen((prev) => !prev)}
+          <Heading size="subtitle.xs" color="white" mr="2">
+            {isOpen ? "Collapse" : "Expand"}
+          </Heading>
+          <Box
+            transition="transform 0.2s"
+            transform={`rotate(${isOpen ? "0" : "180"}deg)`}
           >
-            <Heading size="subtitle.xs" color="white" mr="2">
-              {isOpen ? "Collapse" : "Expand"}
-            </Heading>
-            <Box
-              transition="transform 0.2s"
-              transform={`rotate(${isOpen ? "0" : "180"}deg)`}
-            >
-              <FiChevronUp />
-            </Box>
-          </Flex>
+            <FiChevronUp />
+          </Box>
         </Flex>
-        {isOpen && (
-          <>
-            <Text mt={4}>{description}</Text>
-            <Progress value={percentage} mt={8} mb={2} rounded="full" />
-            <Text size="body.sm" mt={2} color="whiteAlpha.500">
-              {lastStepCompleted + 1}/{steps.length} Tasks Completed
-            </Text>
-            {steps.map(({ children, ...step }, index) => {
-              return (
+      </Flex>
+      {isOpen && (
+        <>
+          <Text mt={4}>{description}</Text>
+          <Progress value={percentage} mt={8} mb={2} rounded="full" />
+          <Text size="body.sm" mt={2} color="whiteAlpha.500">
+            {lastStepCompleted + 1}/{steps.length} Tasks Completed
+          </Text>
+          {steps.map(({ children, ...step }, index) => {
+            return (
+              <Flex
+                flexDir={{ base: "column", md: "row" }}
+                opacity={index === lastStepCompleted + 1 ? 1 : 0.5}
+                key={index}
+                mt={8}
+                gap={4}
+                w="full"
+              >
                 <Flex
-                  flexDir={{ base: "column", md: "row" }}
-                  opacity={index === lastStepCompleted + 1 ? 1 : 0.5}
-                  key={index}
-                  mt={8}
-                  gap={4}
-                  w="full"
+                  w={6}
+                  h={6}
+                  shrink={0}
+                  rounded="full"
+                  alignItems="center"
+                  justifyContent="center"
+                  border="1px solid"
+                  borderColor={step.completed ? "white" : "whiteAlpha.500"}
                 >
-                  <Flex
-                    w={6}
-                    h={6}
-                    shrink={0}
-                    rounded="full"
-                    alignItems="center"
-                    justifyContent="center"
-                    border="1px solid"
-                    borderColor={step.completed ? "white" : "whiteAlpha.500"}
-                  >
-                    {step.completed ? (
-                      <FiCheck />
-                    ) : (
-                      <Text size="label.sm" color="white">
-                        {index + 1}
-                      </Text>
-                    )}
-                  </Flex>
-                  <Flex flexDir="column" w="full">
-                    <Text color="white">{step.title}</Text>
-                    {index === lastStepCompleted + 1 && (
-                      <>
-                        {step.description && (
-                          <Text my={2} size="body.sm" color="whiteAlpha.500">
-                            {step.description}
-                          </Text>
-                        )}
-                        <Box mt={4} w="full">
-                          {children}
-                        </Box>
-                      </>
-                    )}
-                  </Flex>
+                  {step.completed ? (
+                    <FiCheck />
+                  ) : (
+                    <Text size="label.sm" color="white">
+                      {index + 1}
+                    </Text>
+                  )}
                 </Flex>
-              );
-            })}
-          </>
-        )}
-      </Card>
-    )
+                <Flex flexDir="column" w="full">
+                  <Text color="white">{step.title}</Text>
+                  {index === lastStepCompleted + 1 && (
+                    <>
+                      {step.description && (
+                        <Text my={2} size="body.sm" color="whiteAlpha.500">
+                          {step.description}
+                        </Text>
+                      )}
+                      <Box mt={4} w="full">
+                        {children}
+                      </Box>
+                    </>
+                  )}
+                </Flex>
+              </Flex>
+            );
+          })}
+        </>
+      )}
+    </Card>
   );
 };
