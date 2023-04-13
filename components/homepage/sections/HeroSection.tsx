18251f449d8342d6b4d871f2ff65a6c6bbcbf60a
Mariano Fuentes <marfuen98@gmail.com>
2023-04-13 19:05:15 -0400

    Copy suggestions

--- components/homepage/sections/HeroSection.tsx
+++ components/homepage/sections/HeroSection.tsx
@@ -57,8 +57,9 @@ export const HeroSection = () => {
             textAlign={{ base: "center", lg: "left" }}
             maxW="500px"
           >
-            Everything you need to connect your apps or games to decentralized
-            networks.
+            Leverage our powerful SDKs, CLI and robust dashboard to streamline
+            your web3 app development. <br />
+            <br /> Who said making a web3 app was supposed to be hard?
           </Heading>
 
           <LightMode>
@@ -90,7 +91,7 @@ export const HeroSection = () => {
                     background: "rgba(255,255,255,0.9)!important",
                   }}
                 >
-                  Start Building
+                  Go to Dashboard
                 </LinkButton>
                 <Link
                   href="#pricing"
