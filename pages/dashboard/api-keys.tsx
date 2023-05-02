87f60dc91b1ad0602d6bf7ccd4b19d9a561e3814
Adam Majmudar <mr.adam.maj@gmail.com>
2023-05-02 14:31:27 -0700

    Add a getting started flow

--- pages/dashboard/api-keys.tsx
+++ pages/dashboard/api-keys.tsx
@@ -1,69 +1,128 @@
 import { ConnectWallet } from "@3rdweb-sdk/react/components/connect-wallet";
-import { useApiKeys, useCreateApiKey } from "@3rdweb-sdk/react/hooks/useApi";
-import { Flex, Icon, SimpleGrid } from "@chakra-ui/react";
-import { useUser } from "@thirdweb-dev/react";
+import { useApiKeys } from "@3rdweb-sdk/react/hooks/useApi";
+import {
+  Center,
+  Container,
+  Divider,
+  Flex,
+  Icon,
+  Spinner,
+} from "@chakra-ui/react";
+import { useAddress, useLogout, useUser } from "@thirdweb-dev/react";
 import { AppLayout } from "components/app-layouts/app";
+import { StepsCard } from "components/dashboard/StepsCard";
 import { ApiKeyTable } from "components/settings/ApiKeyTable";
-import { useTxNotifications } from "hooks/useTxNotifications";
+import { CreateApiKeyButton } from "components/settings/CreateApiKeyButton";
 import { PageId } from "page-id";
+import { useEffect, useMemo } from "react";
 import { FiPlus } from "react-icons/fi";
-import { Button, Heading } from "tw-components";
+import { Button, Card, Heading, Text } from "tw-components";
 import { ThirdwebNextPage } from "utils/types";
 
 const DashboardApiKeys: ThirdwebNextPage = () => {
+  const address = useAddress();
   const { user } = useUser();
+  const { logout } = useLogout();
   const keysQuery = useApiKeys();
-  const createKeyMutation = useCreateApiKey();
-  const { onSuccess, onError } = useTxNotifications(
-    "API key created",
-    "Failed to create API key",
+
+  useEffect(() => {
+    if (address !== user?.address) {
+      logout();
+    }
+  }, [address, user?.address]);
+
+  const steps = useMemo(
+    () => [
+      {
+        title: "Connect and sign-in with your wallet to get started",
+        description:
+          "In order to create and manage your developer API keys, you need to sign-in with a wallet.",
+        children: <ConnectWallet ecosystem="evm" requireLogin={true} />,
+        completed: !!user?.address,
+      },
+      {
+        title: "Create your first API key",
+        description:
+          "Your API key can be used to access thirdweb infrastructure services like smart wallets, storage, analytics, and more.",
+        children: <CreateApiKeyButton />,
+        completed: (keysQuery.data?.length || 0) > 0,
+      },
+    ],
+    [user?.address],
   );
+
+  if (!user?.address || (keysQuery.isFetched && keysQuery.data?.length === 0)) {
+    return (
+      <StepsCard
+        title="Get started with thirdweb developer API keys"
+        description="Create a new API key to get started with thirdweb infrastructure services"
+        steps={steps}
+      />
+    );
+  }
 
   return (
     <Flex flexDir="column" gap={8} mt={{ base: 2, md: 6 }}>
-      <Flex
-        justifyContent="space-between"
-        direction={{ base: "column", md: "row" }}
-        gap={4}
-      >
-        <Flex flexDir="column" gap={2}>
+      <Flex direction="column" gap={2}>
+        <Flex
+          justifyContent="space-between"
+          direction={{ base: "column", md: "row" }}
+          gap={4}
+        >
           <Heading size="title.lg" as="h1">
             API Keys
           </Heading>
+          <CreateApiKeyButton />
         </Flex>
-        {user?.address ? (
-          <Button
-            onClick={() =>
-              createKeyMutation.mutate(undefined, {
-                onSuccess: () => {
-                  onSuccess();
-                },
-                onError: (err) => {
-                  onError(err);
-                },
-              })
-            }
-            colorScheme="blue"
-            leftIcon={<Icon as={FiPlus} boxSize={4} />}
-            isLoading={createKeyMutation.isLoading}
-            isDisabled={createKeyMutation.isLoading}
-          >
-            Create new key
-          </Button>
-        ) : (
-          <ConnectWallet
-            ml={{ base: 0, md: 2 }}
-            colorScheme="blue"
-            ecosystem="evm"
-            requireLogin={true}
-          />
-        )}
+        <Text>
+          These API keys are used to work with thirdweb infrastructure services
+          like smart wallet, storage, analytics, and more.
+        </Text>
       </Flex>
 
-      <ApiKeyTable
-        isLoading={keysQuery.isLoading}
-        keys={keysQuery.data || []}
-      />
+      {!user ? (
+        <Center>
+          <Container maxW="lg">
+            <Card p={6} as={Flex} flexDir="column" gap={2}>
+              <Heading as="h2" size="title.sm">
+                Sign in to get started
+              </Heading>
+              <Text>
+                In order to interact with your contracts you need to connect a
+                Solana compatible wallet.
+              </Text>
+              <Divider my={4} />
+              <ConnectWallet
+                ml={{ base: 0, md: 2 }}
+                colorScheme="blue"
+                ecosystem="evm"
+                requireLogin={true}
+              />
+            </Card>
+          </Container>
+        </Center>
+      ) : (
+        <ApiKeyTable
+          isLoading={keysQuery.isLoading}
+          keys={keysQuery.data || []}
+        >
+          {keysQuery.isLoading && (
+            <Center>
+              <Flex py={4} direction="row" gap={4} align="center">
+                <Spinner size="sm" />
+                <Text>Loading API Keys</Text>
+              </Flex>
+            </Center>
+          )}
+          {keysQuery.data?.length === 0 && keysQuery.isFetched && (
+            <Center>
+              <Flex py={4} direction="column" gap={4} align="center">
+                <Text>No API keys found. Create one to get started!</Text>
+              </Flex>
+            </Center>
+          )}
+        </ApiKeyTable>
+      )}
     </Flex>
   );
 };
