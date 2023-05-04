1ce485157499c9d91707a54c9fd3dcba354329da
Mariano Fuentes <marfuen98@gmail.com>
2023-05-04 12:09:36 -0400

    added supported wallets section

--- pages/wallet-sdk.tsx
+++ pages/wallet-sdk.tsx
@@ -3,22 +3,32 @@ import { ProductSection } from "../components/product-pages/common/ProductSectio
 import {
   Flex,
   GridItem,
+  LinkBox,
+  LinkOverlay,
   ListItem,
   SimpleGrid,
   UnorderedList,
 } from "@chakra-ui/react";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import {
+  ConnectWallet,
+  ThirdwebProvider,
+  coinbaseWallet,
+  localWallet,
+  metamaskWallet,
+  safeWallet,
+  walletConnect,
+} from "@thirdweb-dev/react";
 import { ChakraNextImage } from "components/Image";
+import { ChainIcon } from "components/icons/ChainIcon";
 import { Hero } from "components/product-pages/common/Hero";
 import { HighlightedButton } from "components/product-pages/common/HighlightedButton";
 import { ProductCard } from "components/product-pages/common/ProductCard";
 import { ProductPage } from "components/product-pages/common/ProductPage";
 import { ProductValueWithHighlight } from "components/product-pages/common/ProductValueWithHighlight";
-import { DemoCodeSnippet } from "components/wallet-sdk/DemoCodeSnippet";
 import { getAbsoluteUrl } from "lib/vercel-utils";
 import { PageId } from "page-id";
 import React, { useState } from "react";
-import { Heading, Text, TrackedLink } from "tw-components";
+import { Card, Heading, Link, Text, TrackedLink } from "tw-components";
 import { ThirdwebNextPage } from "utils/types";
 
 const GRIDS = {
@@ -120,26 +130,91 @@ const GRIDS = {
   ),
 };
 
-// const GUIDES = [
-//   {
-//     title: "How to verify a Custom Contract on Etherscan using the dashboard",
-//     image:
-//       "https://blog.thirdweb.com/content/images/size/w2000/2022/12/verification.png",
-//     link: "https://blog.thirdweb.com/guides/how-to-verify-a-custom-contract-on-etherscan/",
-//   },
-//   {
-//     title: "How to Add Permissions to Your Smart Contract in Solidity",
-//     image:
-//       "https://blog.thirdweb.com/content/images/size/w2000/2022/10/This-is-the-one--13-.png",
-//     link: "https://blog.thirdweb.com/guides/how-to-add-permissions-to-your-smart-contract-contractkit/",
-//   },
-//   {
-//     title: "How to Create an NFT Drop on Solana without writing any code",
-//     image:
-//       "https://blog.thirdweb.com/content/images/size/w2000/2022/10/This-is-the-one--12-.png",
-//     link: "https://blog.thirdweb.com/guides/how-to-create-an-nft-collection-on-solana-without-code/",
-//   },
-// ];
+const WALLETS = [
+  {
+    name: "Smart Wallet",
+    description: "Deploy smart contract wallets for your users",
+    iconUrl:
+      "ipfs://QmeAJVqn17aDNQhjEU3kcWVZCFBrfta8LzaDGkS8Egdiyk/smart-wallet.svg",
+    link: "https://portal.thirdweb.com/wallet/smart-wallet",
+  },
+  {
+    name: "Local Wallet",
+    description: "Generate wallets for new users on the fly",
+    iconUrl:
+      "ipfs://QmeAJVqn17aDNQhjEU3kcWVZCFBrfta8LzaDGkS8Egdiyk/local-wallet-desktop.svg",
+    link: "https://portal.thirdweb.com/wallet/local-wallet",
+  },
+  {
+    name: "Coinbase Wallet",
+    description: "Connect with Coinbase Wallet",
+    iconUrl:
+      "ipfs://QmcJBHopbwfJcLqJpX2xEufSS84aLbF7bHavYhaXUcrLaH/coinbase.svg",
+    link: "https://portal.thirdweb.com/wallet/coinbase-wallet",
+  },
+  {
+    name: "MetaMask",
+    description: "Connect with MetaMask",
+    iconUrl:
+      "ipfs://QmZZHcw7zcXursywnLDAyY6Hfxzqop5GKgwoq8NB9jjrkN/metamask.svg",
+    link: "https://portal.thirdweb.com/wallet/metamask",
+  },
+  {
+    name: "Paper",
+    description: "Connect with email via Paper",
+    iconUrl:
+      "ipfs://QmNrLXtPoFrh4yjZbXui39zUMozS1oetpgU8dvZhFAxfRa/paper-logo-icon.svg",
+    link: "https://portal.thirdweb.com/wallet/paper",
+  },
+  {
+    name: "Ethers.js",
+    description: "Connect any ethers.js compatible wallet",
+    iconUrl: "ipfs://QmTWXcv6XnRqwUwEQxWp21oCrXZJ5QomiSTVBjKPQAv92k/ethers.png",
+    link: "https://portal.thirdweb.com/wallet",
+  },
+  {
+    name: "Private Key",
+    description: "Connect a wallet directly by private key",
+    iconUrl:
+      "ipfs://QmNrycnX15y8EwxDPrwSxnwQgTBWRxUgwSirmhAFoGSod7/private-key.png",
+    link: "https://portal.thirdweb.com/wallet",
+  },
+  {
+    name: "AWS KMS",
+    description: "Connect with AWS Key Management Service",
+    iconUrl:
+      "ipfs://QmVuWYpq5CaMfmbB1qMXXgc4dtUUGY31xiG6sxwvNafoZg/aws-kms.png",
+    link: "https://portal.thirdweb.com/wallet",
+  },
+  {
+    name: "AWS Secrets Manager",
+    description: "Connect with AWS Secrets Manager",
+    iconUrl:
+      "ipfs://QmVuWYpq5CaMfmbB1qMXXgc4dtUUGY31xiG6sxwvNafoZg/aws-secrets-manager.png",
+    link: "https://portal.thirdweb.com/wallet",
+  },
+  {
+    name: "WalletConnect",
+    description: "Connect with WalletConnect (v1 & v2)",
+    iconUrl:
+      "ipfs://QmX58KPRaTC9JYZ7KriuBzeoEaV2P9eZcA3qbFnTHZazKw/wallet-connect.svg",
+    link: "https://portal.thirdweb.com/wallet/wallet-connect-v1",
+  },
+  {
+    name: "Gnosis Safe",
+    description: "Connect to multi-sig wallets via Gnosis Safe",
+    iconUrl:
+      "ipfs://QmbbyxDDmmLQh8DzzeUR6X6B75bESsNUFmbdvS3ZsQ2pN1/SafeToken.svg",
+    link: "https://portal.thirdweb.com/wallet/safe",
+  },
+  {
+    name: "Magic Link",
+    description: "Connect with email or phone number via Magic",
+    iconUrl:
+      "ipfs://QmUMBFZGXxBpgDmZzZAHhbcCL5nYvZnVaYLTajsNjLcxMU/1-Icon_Magic_Color.svg",
+    link: "https://portal.thirdweb.com/wallet",
+  },
+];
 
 const TRACKING_CATEGORY = "wallet-sdk";
 
@@ -235,29 +310,98 @@ const WalletSDK: ThirdwebNextPage = () => {
         </SimpleGrid>
 
         {/* Connect Wallet Button */}
-        {/* <ProductSection py={{ base: 12, lg: 24 }}>
+        <ProductSection py={{ base: 12, lg: 24 }}>
           <Heading
             as="h2"
             size="display.sm"
             fontWeight={700}
             textAlign="center"
-            mb={{ base: 8, lg: 16 }}
           >
             Connect Wallet Button
           </Heading>
-          <ThirdwebProvider activeChain="goerli">
-            <DemoCodeSnippet
-              text={`\
-              <ConnectWallet
-                theme='dark'
-                dropdownPosition={{
-                  align: 'center',
-                  side: 'bottom',
-                }}
-              />`}
-            />
+          <ThirdwebProvider
+            activeChain="goerli"
+            supportedWallets={[
+              localWallet(),
+              coinbaseWallet(),
+              metamaskWallet(),
+              walletConnect(),
+              safeWallet(),
+            ]}
+          >
+            <Flex flexDir="column" gap={12} mt={{ base: 2, md: 6 }}>
+              <Flex
+                direction={"column"}
+                gap={4}
+                justifyContent="center"
+                alignItems="center"
+              >
+                <Text>
+                  One line of code to add a{" "}
+                  <Link
+                    href="https://portal.thirdweb.com/react/react.connectwallet"
+                    color="blue.400"
+                    isExternal
+                  >
+                    Connect Wallet UI component
+                  </Link>{" "}
+                  to React, React Native and Unity apps.
+                </Text>
+                <Text>
+                  Access the largest catalog of wallets, from custodial to MPC
+                  to smart contracts.{" "}
+                </Text>
+                <Flex direction="row" justifyContent={"left"} mt={4}>
+                  <ConnectWallet
+                    theme="dark"
+                    btnTitle="Try Connect Wallet"
+                    auth={{
+                      loginOptional: true,
+                    }}
+                  />
+                </Flex>
+              </Flex>
+
+              <Flex direction={"column"} gap={4}>
+                <SimpleGrid columns={{ base: 1, md: 3 }} gap={4}>
+                  {WALLETS.map((wallet) => (
+                    <LinkBox key={wallet.name} position="relative" role="group">
+                      <LinkOverlay href={wallet.link} isExternal>
+                        <Card
+                          as={Flex}
+                          flexDir="column"
+                          gap={3}
+                          p={6}
+                          _groupHover={{ borderColor: "blue.500" }}
+                          position="relative"
+                        >
+                          <Flex justifyContent="space-between">
+                            <Flex alignItems="center" gap={3}>
+                              <ChainIcon
+                                size={25}
+                                ipfsSrc={wallet.iconUrl}
+                                sizes={[]}
+                              />
+
+                              <Heading size="subtitle.sm" as="h3" noOfLines={1}>
+                                {wallet.name}
+                              </Heading>
+                            </Flex>
+                          </Flex>
+                          <Flex>
+                            <Flex flexDir="column" gap={1} w="full">
+                              <Text opacity={0.6}>{wallet.description}</Text>
+                            </Flex>
+                          </Flex>
+                        </Card>
+                      </LinkOverlay>
+                    </LinkBox>
+                  ))}
+                </SimpleGrid>
+              </Flex>
+            </Flex>
           </ThirdwebProvider>
-        </ProductSection> */}
+        </ProductSection>
 
         {/* Products */}
         <ProductSection py={{ base: 12, lg: 24 }}>
@@ -285,8 +429,6 @@ const WalletSDK: ThirdwebNextPage = () => {
             <ProductCard
               title="ConnectWallet"
               icon={require("/public/assets/product-pages/wallet-sdk/connect-wallet.png")}
-              titleLink="https://portal.thirdweb.com/unity/connectwalletnative"
-              linkCategory={TRACKING_CATEGORY}
             >
               <UnorderedList>
                 <ListItem>
@@ -304,8 +446,6 @@ const WalletSDK: ThirdwebNextPage = () => {
             <ProductCard
               title="Local Wallet"
               icon={require("/public/assets/product-pages/wallet-sdk/local-wallet.png")}
-              titleLink="https://portal.thirdweb.com/wallet/local-wallet"
-              linkCategory={TRACKING_CATEGORY}
             >
               <UnorderedList>
                 <ListItem>
@@ -323,14 +463,10 @@ const WalletSDK: ThirdwebNextPage = () => {
             <ProductCard
               title="Smart Wallet"
               icon={require("/public/assets/product-pages/wallet-sdk/smart-wallet.png")}
-              titleLink="https://portal.thirdweb.com/wallet/smart-wallet"
-              linkCategory={TRACKING_CATEGORY}
             >
               <UnorderedList>
                 <ListItem>
-                  Build, launch, and manage your own smart contract wallets
-                  easily using Solidity SDK, which includes 3 base starter
-                  templates specific for{" "}
+                  Deploy and manage ERC-4337{" "}
                   <TrackedLink
                     href="https://portal.thirdweb.com/wallet/smart-wallet"
                     category={TRACKING_CATEGORY}
@@ -338,11 +474,11 @@ const WalletSDK: ThirdwebNextPage = () => {
                     color="blue.200"
                     target="_blank"
                   >
-                    smart wallets
-                  </TrackedLink>
-                  . Easily deploy ERC-4337 smart contracts wallets for your
-                  users. Programmable wallets that you can customize to your
-                  application needs.
+                    smart contract wallets
+                  </TrackedLink>{" "}
+                  for you users. Unlock fully programmable wallets with
+                  transaction batching, multiple owners, conditional gasless and
+                  your own custom functionality.
                 </ListItem>
               </UnorderedList>
             </ProductCard>
