4e8b78d92077ec2395b8b93102ea74cbb22b9545
Nacho Iacovino <nacho.iacovino@gmail.com>
2023-05-12 01:35:08 +0200

    Add step 3

--- pages/dashboard/wallet.tsx
+++ pages/dashboard/wallet.tsx
@@ -10,6 +10,8 @@ import {
   walletConnectV1,
 } from "@thirdweb-dev/react";
 import { AppLayout } from "components/app-layouts/app";
+import { CodeSegment } from "components/contract-tabs/code/CodeSegment";
+import { CodeEnvironment } from "components/contract-tabs/code/types";
 import { ChainIcon } from "components/icons/ChainIcon";
 import { PageId } from "page-id";
 import React, { useMemo, useState } from "react";
@@ -19,57 +21,207 @@ import { ThirdwebNextPage } from "utils/types";
 
 const WALLETS = [
   {
-    id: "smartWallet",
+    id: "smart-wallet",
     name: "Smart Wallet",
     description: "Deploy smart contract wallets for your users",
     iconUrl:
       "ipfs://QmeAJVqn17aDNQhjEU3kcWVZCFBrfta8LzaDGkS8Egdiyk/smart-wallet.svg",
     link: "https://portal.thirdweb.com/wallet/smart-wallet",
-    supportedLanguages: ["JavaScript", "React", "React Native"],
+    supportedLanguages: {
+      javascript: `import { LocalWallet, SmartWallet } from "@thirdweb-dev/wallets";
+import { Goerli } from "@thirdweb-dev/chains";
+
+// First, connect the personal wallet, which can be any wallet (metamask, walletconnect, etc.)
+// Here we're just generating a new local wallet which can be saved later
+const personalWallet = new LocalWallet();
+await personalWallet.generate();
+await personalWallet.connect();
+
+// Setup the Smart Wallet configuration
+const config: SmartWalletConfig = {
+  chain: Goerli, // the chain where your smart wallet will be or is deployed
+  factoryAddress: "{{factory_address}}", // your own deployed account factory address
+  thirdwebApiKey: "{{api_key}}", // obtained from the thirdweb dashboard
+  gasless: true, // enable or disable gasless transactions
+};
+
+// Then, connect the Smart wallet
+const wallet = new SmartWallet(config);
+await wallet.connect({
+  personalWallet,
+});`,
+      react: `import { ThirdwebProvider, ConnectWallet, smartWallet } from "@thirdweb/react";
+
+export default function App() {
+return (
+    <ThirdwebProvider supportedWallets={[ smartWallet() ]}>
+      <ConnectWallet theme="{{theme}}" />
+    </ThirdwebProvider>
+  );
+}`,
+      "react-native": `import { ThirdwebProvider, ConnectWallet, smartWallet } from "@thirdweb/react-native";
+
+export default function App() {
+return (
+    <ThirdwebProvider supportedWallets={[ smartWallet() ]}>
+      <ConnectWallet theme="{{theme}}" />
+    </ThirdwebProvider>
+  );
+}`,
+    },
   },
   {
-    id: "localWallet",
+    id: "local-wallet",
     name: "Local Wallet",
     description: "Generate wallets for new users on the fly",
     iconUrl:
       "ipfs://QmeAJVqn17aDNQhjEU3kcWVZCFBrfta8LzaDGkS8Egdiyk/local-wallet-desktop.svg",
     link: "https://portal.thirdweb.com/wallet/local-wallet",
-    supportedLanguages: ["JavaScript", "React", "Unity", "React Native"],
+    supportedLanguages: {
+      javascript: `import { LocalWallet } from "@thirdweb-dev/wallets";
+
+const wallet = new LocalWallet();
+
+// generate a random wallet
+await wallet.generate();
+// connect the wallet to the application
+await wallet.connect();
+
+// at any point, you can save the wallet to persistent storage
+await wallet.save(config);
+// and load it back up
+await wallet.load(config);
+
+// you can also export the wallet out of the application
+const exportedWallet = await wallet.export(config);
+// and import it back in
+await wallet.import(exportedWallet, config);`,
+      react: `import { ThirdwebProvider, ConnectWallet, localWallet } from "@thirdweb/react";
+
+export default function App() {
+return (
+    <ThirdwebProvider supportedWallets={[ localWallet() ]}>
+      <ConnectWallet theme="{{theme}}" />
+    </ThirdwebProvider>
+  );
+}`,
+      "react-native": `import { ThirdwebProvider, ConnectWallet, localWallet } from "@thirdweb/react-native";
+
+export default function App() {
+return (
+    <ThirdwebProvider supportedWallets={[ localWallet() ]}>
+      <ConnectWallet theme="{{theme}}" />
+    </ThirdwebProvider>
+  );
+}`,
+      unity: ``,
+    },
   },
   {
-    id: "coinbaseWallet",
+    id: "coinbase-wallet",
     name: "Coinbase Wallet",
     description: "Connect with Coinbase Wallet",
     iconUrl:
       "ipfs://QmcJBHopbwfJcLqJpX2xEufSS84aLbF7bHavYhaXUcrLaH/coinbase.svg",
     link: "https://portal.thirdweb.com/wallet/coinbase-wallet",
-    supportedLanguages: ["JavaScript", "React", "Unity", "React Native"],
+    supportedLanguages: {
+      javascript: `import { CoinbaseWallet } from "@thirdweb-dev/wallets";
+
+const wallet = new CoinbaseWallet();
+
+wallet.connect();`,
+      react: `import { ThirdwebProvider, ConnectWallet, coinbaseWallet } from "@thirdweb/react";
+
+export default function App() {
+return (
+    <ThirdwebProvider supportedWallets={[ coinbaseWallet() ]}>
+      <ConnectWallet theme="{{theme}}" />
+    </ThirdwebProvider>
+  );
+}`,
+      "react-native": `import { ThirdwebProvider, ConnectWallet, coinbaseWallet } from "@thirdweb/react-native";
+
+export default function App() {
+return (
+    <ThirdwebProvider supportedWallets={[ coinbaseWallet() ]}>
+      <ConnectWallet theme="{{theme}}" />
+    </ThirdwebProvider>
+  );
+}`,
+      unity: ``,
+    },
   },
   {
-    id: "metamaskWallet",
+    id: "metamask",
     name: "MetaMask",
     description: "Connect with MetaMask",
     iconUrl:
       "ipfs://QmZZHcw7zcXursywnLDAyY6Hfxzqop5GKgwoq8NB9jjrkN/metamask.svg",
     link: "https://portal.thirdweb.com/wallet/metamask",
-    supportedLanguages: ["JavaScript", "React", "Unity", "React Native"],
+    supportedLanguages: {
+      javascript: `import { MetaMaskWallet } from "@thirdweb-dev/wallets";
+
+const wallet = new MetaMaskWallet();
+
+wallet.connect();`,
+      react: `import { ThirdwebProvider, ConnectWallet, metamaskWallet } from "@thirdweb/react";
+
+export default function App() {
+return (
+    <ThirdwebProvider supportedWallets={[ metamaskWallet() ]}>
+      <ConnectWallet theme="{{theme}}" />
+    </ThirdwebProvider>
+  );
+}`,
+      "react-native": `import { ThirdwebProvider, ConnectWallet, metamaskWallet } from "@thirdweb/react-native";
+
+export default function App() {
+return (
+    <ThirdwebProvider supportedWallets={[ metamaskWallet() ]}>
+      <ConnectWallet theme="{{theme}}" />
+    </ThirdwebProvider>
+  );
+}`,
+      unity: ``,
+    },
   },
   {
-    id: "paperWallet",
+    id: "paper",
     name: "Paper",
     description: "Connect with email via Paper",
     iconUrl:
       "ipfs://QmNrLXtPoFrh4yjZbXui39zUMozS1oetpgU8dvZhFAxfRa/paper-logo-icon.svg",
     link: "https://portal.thirdweb.com/wallet/paper",
-    supportedLanguages: ["JavaScript", "React"],
+    supportedLanguages: {
+      javascript: `import { PaperWallet } from "@thirdweb-dev/wallets";
+import { Ethereum } from "@thirdweb-dev/chains";
+
+const wallet = new PaperWallet({
+  chain: Ethereum, //  chain to connect to
+  clientId: "client_id", // Paper SDK client ID
+});
+
+wallet.connect();`,
+      react: `import { ThirdwebProvider, ConnectWallet, paperWallet } from "@thirdweb/react";
+
+export default function App() {
+return (
+    <ThirdwebProvider supportedWallets={[ paperWallet({ clientId: "client_id" }) ]}>
+      <ConnectWallet theme="{{theme}}" />
+    </ThirdwebProvider>
+  );
+}`,
+    },
   },
-  {
+  /*   {
     id: "ethers",
     name: "Ethers.js",
     description: "Connect any ethers.js compatible wallet",
     iconUrl: "ipfs://QmTWXcv6XnRqwUwEQxWp21oCrXZJ5QomiSTVBjKPQAv92k/ethers.png",
     link: "https://portal.thirdweb.com/wallet",
-    supportedLanguages: ["JavaScript", "React"],
+    supportedLanguages: {
+      javascript: ``,
+    },
   },
   {
     id: "private-key",
@@ -78,8 +230,10 @@ const WALLETS = [
     iconUrl:
       "ipfs://QmNrycnX15y8EwxDPrwSxnwQgTBWRxUgwSirmhAFoGSod7/private-key.png",
     link: "https://portal.thirdweb.com/wallet",
-    supportedLanguages: ["JavaScript", "React", "Unity", "React Native"],
-  },
+    supportedLanguages: {
+      javascript: ``,
+    },
+  }, */
   {
     id: "aws-kms",
     name: "AWS KMS",
@@ -87,7 +241,17 @@ const WALLETS = [
     iconUrl:
       "ipfs://QmVuWYpq5CaMfmbB1qMXXgc4dtUUGY31xiG6sxwvNafoZg/aws-kms.png",
     link: "https://portal.thirdweb.com/wallet/aws-kms",
-    supportedLanguages: ["JavaScript"],
+    supportedLanguages: {
+      javascript: `import { AwsKmsWallet } from "@thirdweb-dev/wallets/evm/wallets/aws-kms";
+
+const wallet = new AwsKmsWallet({
+  region: "us-east-1",
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  sessionToken: process.env.AWS_SESSION_TOKEN,
+  keyId: process.env.AWS_KEY_ID,
+});`,
+    },
   },
   {
     id: "aws-secrets-manager",
@@ -96,59 +260,153 @@ const WALLETS = [
     iconUrl:
       "ipfs://QmVuWYpq5CaMfmbB1qMXXgc4dtUUGY31xiG6sxwvNafoZg/aws-secrets-manager.png",
     link: "https://portal.thirdweb.com/wallet/aws-secrets-manager",
-    supportedLanguages: ["JavaScript"],
+    supportedLanguages: {
+      javascript: `import { AwsSecretsManagerWallet } from "@thirdweb-dev/wallets/evm/wallets/aws-secrets-manager";
+
+const wallet = new AwsSecretsManagerWallet({
+  secretId: "{{secret-id}}", // ID of the secret value
+  secretKeyName: "{{secret-key-name}}", // Name of the secret value
+  awsConfig: {
+    region: "us-east-1", // Region where your secret is stored
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID, // Add environment variables to store your AWS credentials
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, // Add environment variables to store your AWS credentials
+    },
+  },
+});`,
+    },
   },
   {
-    id: "walletConnect",
+    id: "wallet-connect",
     name: "WalletConnect",
     description: "Connect with WalletConnect (v1 & v2)",
     iconUrl:
       "ipfs://QmX58KPRaTC9JYZ7KriuBzeoEaV2P9eZcA3qbFnTHZazKw/wallet-connect.svg",
     link: "https://portal.thirdweb.com/wallet/wallet-connect-v1",
-    supportedLanguages: ["JavaScript", "React", "Unity", "React Native"],
+    supportedLanguages: {
+      javascript: `import { WalletConnect } from "@thirdweb-dev/wallets";
+
+const wallet = new WalletConnect();
+
+wallet.connect();`,
+      react: `import { ThirdwebProvider, ConnectWallet, walletConnect } from "@thirdweb/react";
+
+export default function App() {
+return (
+    <ThirdwebProvider supportedWallets={[ walletConnect() ]}>
+      <ConnectWallet theme="{{theme}}" />
+    </ThirdwebProvider>
+  );
+}`,
+      "react-native": `import { ThirdwebProvider, ConnectWallet, walletConnect } from "@thirdweb/react-native";
+
+export default function App() {
+return (
+    <ThirdwebProvider supportedWallets={[ walletConnect() ]}>
+      <ConnectWallet theme="{{theme}}" />
+    </ThirdwebProvider>
+  );
+}`,
+      unity: ``,
+    },
   },
   {
-    id: "safeWallet",
+    id: "safe-wallet",
     name: "Safe",
     description: "Connect to multi-sig wallets via Safe",
     iconUrl:
       "ipfs://QmbbyxDDmmLQh8DzzeUR6X6B75bESsNUFmbdvS3ZsQ2pN1/SafeToken.svg",
     link: "https://portal.thirdweb.com/wallet/safe",
-    supportedLanguages: ["JavaScript", "React"],
+    supportedLanguages: {
+      javascript: `import { CoinbaseWallet, SafeWallet } from "@thirdweb-dev/wallets";
+import { Ethereum } from "@thirdweb-dev/chains";
+
+// First, connect the personal wallet
+const personalWallet = new CoinbaseWallet();
+await personalWallet.connect();
+
+// Then, connect the Safe wallet
+const wallet = new SafeWallet();
+await wallet.connect({
+  personalWallet: personalWallet,
+  chain: Ethereum,
+  safeAddress: "{{contract_address}}",
+});`,
+      react: `import { ThirdwebProvider, ConnectWallet, safeWallet } from "@thirdweb/react";
+
+export default function App() {
+return (
+    <ThirdwebProvider supportedWallets={[ safeWallet() ]}>
+      <ConnectWallet theme="{{theme}}" />
+    </ThirdwebProvider>
+  );
+}`,
+    },
   },
   {
-    id: "magicLink",
+    id: "magic-link",
     name: "Magic Link",
     description: "Connect with email or phone number via Magic",
     iconUrl:
       "ipfs://QmUMBFZGXxBpgDmZzZAHhbcCL5nYvZnVaYLTajsNjLcxMU/1-Icon_Magic_Color.svg",
     link: "https://portal.thirdweb.com/wallet/magic",
-    supportedLanguages: ["JavaScript", "React", "Unity"],
+    supportedLanguages: {
+      javascript: `import { MagicLink } from "@thirdweb-dev/wallets";
+
+// create a wallet instance
+const wallet = new MagicLink({
+  apiKey: "YOUR_API_KEY",
+});
+
+// now connect using email or phone number
+
+wallet.connect({
+  email: "user@example.com",
+});
+
+// OR
+
+wallet.connect({
+  phoneNumber: "+123456789",
+});`,
+      react: `import { ThirdwebProvider, ConnectWallet, magicLink } from "@thirdweb/react";
+
+export default function App() {
+return (
+    <ThirdwebProvider supportedWallets={[ magicLink({ apiKey: "YOUR_API_KEY" }) ]}>
+      <ConnectWallet theme="{{theme}}" />
+    </ThirdwebProvider>
+  );
+}`,
+      unity: ``,
+    },
   },
 ] as const;
 
-type WalletLanguage = (typeof WALLETS)[number]["supportedLanguages"][number];
+const WALLET_LANGUAGES = [
+  "javascript",
+  "react",
+  "react-native",
+  /*   "unity", */
+] as const;
 
-const WALLET_LANGUAGES = WALLETS.reduce<WalletLanguage[]>(
-  (acc, wallet) => [...new Set([...acc, ...wallet.supportedLanguages])].sort(),
-  [],
-);
+type WalletLanguage = (typeof WALLET_LANGUAGES)[number];
 
 const langToIconMap: Record<WalletLanguage, JSX.Element> = {
-  JavaScript: <SiJavascript />,
-  React: <SiReact />,
-  Unity: <SiUnity />,
-  "React Native": <SiReact />,
+  javascript: <SiJavascript />,
+  react: <SiReact />,
+  "react-native": <SiReact />,
+  /*   unity: <SiUnity />, */
 };
 
 interface WalletLanguageSelectorProps {
-  setSelectedLanguage: (language: WalletLanguage) => void;
-  selectedLanuage: WalletLanguage;
+  setSelectedLanguage: (language: CodeEnvironment) => void;
+  selectedLanguage: CodeEnvironment;
 }
 
 const WalletLanguageSelector: React.FC<WalletLanguageSelectorProps> = ({
   setSelectedLanguage,
-  selectedLanuage,
+  selectedLanguage,
 }) => {
   return (
     <Flex gap={4}>
@@ -162,10 +420,15 @@ const WalletLanguageSelector: React.FC<WalletLanguageSelectorProps> = ({
             color: "bgWhite",
           }}
           leftIcon={langToIconMap[language]}
-          isActive={language === selectedLanuage}
+          isActive={language === selectedLanguage}
           onClick={() => setSelectedLanguage(language)}
+          textTransform="capitalize"
         >
-          {language}
+          {language === "react-native"
+            ? "React Native"
+            : language === "javascript"
+            ? "JavaScript"
+            : language}
         </Button>
       ))}
     </Flex>
@@ -173,7 +436,7 @@ const WalletLanguageSelector: React.FC<WalletLanguageSelectorProps> = ({
 };
 
 interface SupportedWalletsSelectorProps {
-  selectedLanguage: WalletLanguage;
+  selectedLanguage: CodeEnvironment;
   selectedWallet: (typeof WALLETS)[number] | null;
   setSelectedWallet: (wallet: (typeof WALLETS)[number]) => void;
 }
@@ -185,11 +448,19 @@ const SupportedWalletsSelector: React.FC<SupportedWalletsSelectorProps> = ({
 }) => {
   const sortedWallets = useMemo(() => {
     // sort by language being supported or not
-    const supportedWallets = WALLETS.filter((wallet) =>
-      wallet.supportedLanguages.includes(selectedLanguage as any),
+    const supportedWallets = WALLETS.filter(
+      (wallet) =>
+        selectedLanguage in wallet.supportedLanguages &&
+        wallet.supportedLanguages[
+          selectedLanguage as keyof typeof wallet.supportedLanguages
+        ],
     );
     const unsupportedWallets = WALLETS.filter(
-      (wallet) => !wallet.supportedLanguages.includes(selectedLanguage as any),
+      (wallet) =>
+        !(selectedLanguage in wallet.supportedLanguages) &&
+        !wallet.supportedLanguages[
+          selectedLanguage as keyof typeof wallet.supportedLanguages
+        ],
     );
     return [
       ...supportedWallets,
@@ -247,7 +518,14 @@ const SupportedWalletsSelector: React.FC<SupportedWalletsSelectorProps> = ({
                 zIndex={1}
               >
                 <Heading as="p" size="label.sm">
-                  Not yet supported in {selectedLanguage}
+                  Not yet supported in{" "}
+                  <Box as="span" textTransform="capitalize">
+                    {selectedLanguage === "react-native"
+                      ? "React Native"
+                      : selectedLanguage === "javascript"
+                      ? "JavaScript"
+                      : selectedLanguage}
+                  </Box>
                 </Heading>
               </SimpleGrid>
             )}
@@ -260,16 +538,19 @@ const SupportedWalletsSelector: React.FC<SupportedWalletsSelectorProps> = ({
 
 const DashboardWallets: ThirdwebNextPage = () => {
   const [selectedLanguage, setSelectedLanguage] =
-    useState<WalletLanguage>("JavaScript");
+    useState<CodeEnvironment>("javascript");
 
   const [selectedWallet, setSelectedWallet] = useState<
     (typeof WALLETS)[number] | null
   >(null);
 
-  const onLanguageSelect = (language: WalletLanguage) => {
+  const onLanguageSelect = (language: CodeEnvironment) => {
     if (
       selectedWallet &&
-      !selectedWallet.supportedLanguages.includes(language as any)
+      !(language in selectedWallet.supportedLanguages) &&
+      !selectedWallet.supportedLanguages[
+        language as keyof typeof selectedWallet.supportedLanguages
+      ]
     ) {
       setSelectedWallet(null);
     }
@@ -306,7 +587,7 @@ const DashboardWallets: ThirdwebNextPage = () => {
           Step 1: Pick a language to get started
         </Heading>
         <WalletLanguageSelector
-          selectedLanuage={selectedLanguage}
+          selectedLanguage={selectedLanguage}
           setSelectedLanguage={onLanguageSelect}
         />
       </Flex>
@@ -322,7 +603,23 @@ const DashboardWallets: ThirdwebNextPage = () => {
         />
       </Flex>
 
-      <ConnectWalletWithPreview />
+      {/*       <ConnectWalletWithPreview /> */}
+      {selectedWallet && WALLETS.find((w) => w.id === selectedWallet?.id) && (
+        <Flex direction={"column"} gap={4}>
+          <Heading size="subtitle.sm" as="h3">
+            Step 3: Integrate into your app
+          </Heading>
+          <CodeSegment
+            snippet={
+              (WALLETS.find((w) => w.id === selectedWallet?.id) as any)
+                .supportedLanguages
+            }
+            environment={selectedLanguage}
+            setEnvironment={setSelectedLanguage}
+            hideTabs
+          />
+        </Flex>
+      )}
 
       {/* <Flex direction={"column"} gap={2}>
         <Heading size="title.sm" as="h3">
