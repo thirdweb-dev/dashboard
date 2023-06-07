bb9cab445561962e24b4d1adb787c9d577d4c0a5
Manan Tank <manantankm@gmail.com>
2023-06-07 16:38:29 +0530

    move /check-verification-status api to edge

--- pages/api/email-signup.ts
+++ pages/api/email-signup.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 import invariant from "tiny-invariant";
 
 interface EmailSignupPayload {
