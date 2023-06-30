3a7d9b26a0cc6c7c3b5d9f821f570203efff2eb1
Nacho Iacovino <nacho.iacovino@gmail.com>
2023-06-30 20:55:21 +0200

    Fix build

--- contract-ui/hooks/useRouteConfig.tsx
+++ contract-ui/hooks/useRouteConfig.tsx
@@ -228,7 +228,7 @@ export function useContractRouteConfig(
       path: "account",
       isEnabled: extensionDetectedState({
         contractQuery,
-        feature: ["SmartWallet"],
+        feature: ["Account"],
       }),
       component: LazyContractAccountPage,
     },
