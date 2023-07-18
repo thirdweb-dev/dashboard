1a3313ef9514b26aa057a6e657aeee0aaa482d1d
Nacho Iacovino <nacho.iacovino@gmail.com>
2023-07-18 22:19:43 +0200

    Invert colors

--- components/notices/AnnouncementCard.tsx
+++ components/notices/AnnouncementCard.tsx
@@ -1,4 +1,10 @@
-import { Box, Container, Flex, IconButton } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Flex,
+  IconButton,
+  useColorMode,
+} from "@chakra-ui/react";
 import { useLocalStorage } from "hooks/useLocalStorage";
 import { FiX } from "react-icons/fi";
 import { Card, Heading, Text, TrackedLinkButton } from "tw-components";
@@ -6,21 +12,22 @@ import { Card, Heading, Text, TrackedLinkButton } from "tw-components";
 export const AnnouncementCard = () => {
   const [hasDismissedAnnouncement, setHasDismissedAnnouncement] =
     useLocalStorage("dismissed-api-keys", false, true);
+  const { colorMode } = useColorMode();
 
   if (hasDismissedAnnouncement) {
     return null;
   }
 
   return (
-    <Card p={6}>
+    <Card p={6} bg="bgBlack">
       <Flex w="full" justifyContent="space-between" gap={{ base: 1, md: 2 }}>
         <Container maxW="container.page" display="flex" px={0}>
-          <Flex mx="auto" gap={4} color="white" flexDir="column">
+          <Flex gap={4} color="white" flexDir="column">
             <Flex flexDir="column" gap={3}>
-              <Heading size="label.lg" as="p">
+              <Heading size="label.lg" as="p" color="bgWhite">
                 thirdweb services now require an API key
               </Heading>
-              <Text>
+              <Text color="bgWhite">
                 Action <strong>required</strong> for all users: use of client
                 API keys <strong>mandatory</strong> by August 1st to continue
                 using thirdweb infrastructure services.
@@ -32,6 +39,8 @@ export const AnnouncementCard = () => {
                 category="announcement"
                 label="api-keys"
                 colorScheme="blackAlpha"
+                bg="bgBlack"
+                color="bgWhite"
               >
                 Learn More
               </TrackedLinkButton>
@@ -44,6 +53,7 @@ export const AnnouncementCard = () => {
           aria-label="Close announcement"
           icon={<FiX />}
           variant="ghost"
+          color="bgWhite"
           onClick={() => setHasDismissedAnnouncement(true)}
         />
       </Flex>
