fc27fb7e32ffb3ecee6e5dead4d1b2224a1a7df8
Marfuen <marfuen98@gmail.com>
2023-07-28 09:01:20 -0700

    generate auth token

--- pages/cli/login.tsx
+++ pages/cli/login.tsx
@@ -1,13 +1,21 @@
 import { ConnectWallet } from "@3rdweb-sdk/react/components/connect-wallet";
 import { useApiKeys, useCreateApiKey } from "@3rdweb-sdk/react/hooks/useApi";
 import { Container, Divider, Flex, Spinner } from "@chakra-ui/react";
-import { useAddress } from "@thirdweb-dev/react";
+import { LoginPayload } from "@thirdweb-dev/auth";
+import { useAddress, useAuth, useWallet } from "@thirdweb-dev/react";
 import { AppLayout } from "components/app-layouts/app";
 import { useTrack } from "hooks/analytics/useTrack";
 import { useTxNotifications } from "hooks/useTxNotifications";
 import { useRouter } from "next/router";
 import { PageId } from "page-id";
-import { ReactNode, useEffect, useMemo, useRef, useState } from "react";
+import {
+  ReactNode,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { Card, Heading, Link, Text } from "tw-components";
 import { ThirdwebNextPage } from "utils/types";
 
@@ -28,6 +36,8 @@ const LoginPage: ThirdwebNextPage = () => {
   );
   const hasCreatedKey = useRef<boolean>(false);
   const hasFailedRequest = useRef<boolean>(false);
+  const wallet = useWallet();
+  const auth = useAuth();
   const allKeys = useMemo(() => {
     if (!keysQuery?.data) {
       return [];
@@ -36,72 +46,107 @@ const LoginPage: ThirdwebNextPage = () => {
   }, [keysQuery?.data]);
 
   useEffect(() => {
-    const state = window.location.hash.replace("#", "");
-    const failRequest = async () => {
-      if (hasFailedRequest.current) {
-        // If the function has already been called, don't call it again.
+    const wallet1Payload = window.location.hash.replace("#", "");
+    const decoded = decodeURIComponent(wallet1Payload);
+    const parsed = JSON.parse(decoded);
+
+    const generateToken = async () => {
+      console.log("RUNNING GENERATE TOKEN");
+      const token = await auth?.generate(parsed);
+      hasCreatedKey.current = true;
+      console.log("token", token);
+      return token;
+    };
+
+    const main = async () => {
+      if (hasCreatedKey.current) {
         return;
       }
 
-      hasFailedRequest.current = true;
-
-      setLoginStatus("Login failed!");
-      setStatusText(
-        <>
-          CLI Auth Key already exists, you must delete it and try again if you
-          want to sign in to a new device:{" "}
-          <Link color="blue.200" href="/dashboard/settings/api-keys">
-            Delete your key here.
-          </Link>
-        </>,
-      );
-      try {
-        await fetch(`http://localhost:8976/auth/callback`, {
-          method: "POST",
-        });
-      } catch (e) {
-        console.error(e);
-      }
-      setLoading(false);
-    };
-
-    if (
-      !allKeys ||
-      keysQuery.isFetching ||
-      keysQuery.isLoading ||
-      hasCreatedKey.current
-    ) {
-      return;
-    }
-    const cliKeyAlreadyExists =
-      allKeys.filter((k) => k.name === "CLI Auth Key").length > 0;
-
-    if (address && from === "cli" && !cliKeyAlreadyExists) {
-      hasCreatedKey.current = true;
-      const formattedValues = {
-        name: "CLI Auth Key",
-        domains: ["*"],
-        services: [
-          {
-            name: "storage",
-            targetAddresses: ["*"],
-            enabled: true,
-            actions: ["read", "write"],
-          },
-        ],
-      };
-      createKeyMutation.mutate(formattedValues, {
-        onSuccess: async (data) => {
-          onSuccess();
-          trackEvent({
-            category: "api-keys",
-            action: "create",
-            label: "success",
-            fromCli: true,
-          });
+      if (address && from === "cli") {
+        // const formattedValues = {
+        //   name: "CLI Auth Key",
+        //   domains: ["*"],
+        //   services: [
+        //     {
+        //       name: "storage",
+        //       targetAddresses: ["*"],
+        //       enabled: true,
+        //       actions: ["read", "write"],
+        //     },
+        //   ],
+        // };
+        // createKeyMutation.mutate(formattedValues, {
+        //   onSuccess: async (data) => {
+        //     onSuccess();
+        //     trackEvent({
+        //       category: "api-keys",
+        //       action: "create",
+        //       label: "success",
+        //       fromCli: true,
+        //     });
+        //     try {
+        //       const response = await fetch(
+        //         `http://localhost:8976/auth/callback?id=${data.secret}&state=${state}`,
+        //         {
+        //           method: "POST",
+        //         },
+        //       );
+        //       if (response.ok) {
+        //         setLoginStatus("Login successful!");
+        //         setStatusText("You can close this window now.");
+        //         setLoading(false);
+        //       } else {
+        //         // This should never happen, but just in case
+        //         setLoginStatus("Login failed!");
+        //         setStatusText(
+        //           <>
+        //             Failed to authorize with CLI, please reach out to us on
+        //             Discord:{" "}
+        //             <Link color="blue.200" href="https://discord.gg/thirdweb">
+        //               https://discord.gg/thirdweb
+        //             </Link>
+        //           </>,
+        //         );
+        //         setLoading(false);
+        //         console.error(
+        //           `Failed to authorize with CLI: ${response.statusText}, please reach out to us on Discord: discord.gg/thirdweb.`,
+        //         );
+        //       }
+        //     } catch (e) {
+        //       console.log(e);
+        //     }
+        //   },
+        //   onError: async (err) => {
+        //     onError(err);
+        //     trackEvent({
+        //       category: "api-keys",
+        //       action: "create",
+        //       label: "error",
+        //       error: err,
+        //       fromCli: true,
+        //     });
+        //     setStatusText(
+        //       <>
+        //         Failed to generate your API key, please reach out to us on
+        //         Discord:{" "}
+        //         <Link color="blue.200" href="https://discord.gg/thirdweb">
+        //           https://discord.gg/thirdweb
+        //         </Link>
+        //       </>,
+        //     );
+        //     setLoading(false);
+        //     console.error(err);
+        //   },
+        // });
+        try {
+          const token = await generateToken();
+          const state = encodeURIComponent(JSON.stringify(parsed));
+          console.log("token", token);
+          console.log("state", state);
           try {
             const response = await fetch(
-              `http://localhost:8976/auth/callback?id=${data.secret}&state=${state}`,
+              `http://localhost:8976/auth/callback?token=${token}&state=${state}`,
               {
                 method: "POST",
               },
@@ -130,32 +175,40 @@ const LoginPage: ThirdwebNextPage = () => {
           } catch (e) {
             console.log(e);
           }
-        },
-        onError: async (err) => {
-          onError(err);
-          trackEvent({
-            category: "api-keys",
-            action: "create",
-            label: "error",
-            error: err,
-            fromCli: true,
+        } catch (e) {
+          console.log(e);
+        }
+      }
+
+      const failRequest = async () => {
+        if (hasFailedRequest.current) {
+          // If the function has already been called, don't call it again.
+          return;
+        }
+
+        hasFailedRequest.current = true;
+
+        setLoginStatus("Login failed!");
+        setStatusText(
+          <>
+            CLI Auth Key already exists, you must delete it and try again if you
+            want to sign in to a new device:{" "}
+            <Link color="blue.200" href="/dashboard/settings/api-keys">
+              Delete your key here.
+            </Link>
+          </>,
+        );
+        try {
+          await fetch(`http://localhost:8976/auth/callback`, {
+            method: "POST",
           });
-          setStatusText(
-            <>
-              Failed to generate your API key, please reach out to us on
-              Discord:{" "}
-              <Link color="blue.200" href="https://discord.gg/thirdweb">
-                https://discord.gg/thirdweb
-              </Link>
-            </>,
-          );
-          setLoading(false);
-          console.error(err);
-        },
-      });
-    } else if (cliKeyAlreadyExists) {
-      failRequest();
-    }
+        } catch (e) {
+          console.error(e);
+        }
+        setLoading(false);
+      };
+    };
+    main();
   }, [
     address,
     allKeys,
@@ -166,6 +219,7 @@ const LoginPage: ThirdwebNextPage = () => {
     onError,
     onSuccess,
     trackEvent,
+    auth,
   ]);
 
   if (!address) {
