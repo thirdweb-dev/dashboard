3c9565c49ccbb96a6a50c31bf7e0a70ba7a6e46a
Marfuen <marfuen98@gmail.com>
2023-08-23 10:49:37 -0700

    update UI/UX

--- pages/cli/login.tsx
+++ pages/cli/login.tsx
@@ -1,139 +1,110 @@
 import { ConnectWallet } from "@3rdweb-sdk/react/components/connect-wallet";
 import { useAuthorizeWalletWithAccount } from "@3rdweb-sdk/react/hooks/useApi";
-import { Container, Divider, Flex, Spinner } from "@chakra-ui/react";
+import { Container, Flex, VStack } from "@chakra-ui/react";
 import { useAddress, useAuth } from "@thirdweb-dev/react";
 import { AppLayout } from "components/app-layouts/app";
-import { useTrack } from "hooks/analytics/useTrack";
 import { useRouter } from "next/router";
 import { PageId } from "page-id";
-import { ReactNode, useEffect, useRef, useState } from "react";
-import { Card, Heading, Link, Text } from "tw-components";
+import { ReactNode, useState } from "react";
+import { Button, Heading, Link, Text } from "tw-components";
 import { ThirdwebNextPage } from "utils/types";
 
 const LoginPage: ThirdwebNextPage = () => {
-  // Get state and payload from URL.
   const { payload } = useRouter().query;
   const { mutateAsync: authorizeWallet } = useAuthorizeWalletWithAccount();
 
   const address = useAddress();
-  const trackEvent = useTrack();
   const auth = useAuth();
 
-  const hasCreatedToken = useRef<boolean>(false);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [statusTitle, setStatusTitle] = useState<string>("Logging in...");
-  const [statusText, setStatusText] = useState<string | ReactNode>(
-    "Authorizing your account...",
+  const [loading, setLoading] = useState<boolean>(false);
+  const [errorText, setErrorText] = useState<string | ReactNode | undefined>(
+    undefined,
   );
 
-  useEffect(() => {
-    if (!window || !payload) {
-      return;
+  const generateToken = async () => {
+    if (!payload) {
+      console.error("**** Payload not detected from the url! ****");
+      return null;
     }
-
-    const state = window.location.hash.replace("#", "");
     const decodedPayload = decodeURIComponent(payload as string);
     const parsedPayload = JSON.parse(decodedPayload);
-
-    console.log("state", state);
-    console.log("payload", parsedPayload);
-
-    const generateToken = async () => {
-      const token = await auth?.generate(parsedPayload);
-      if (!token) {
-        return undefined;
-      }
-      hasCreatedToken.current = true;
-      try {
-        await authorizeWallet(token);
-      } catch (e) {
-        console.error(e);
-        setStatusTitle("Login failed!");
-        return undefined;
-      }
+    let token;
+    try {
+      token = await auth?.generate(parsedPayload);
+    } catch (e) {
+      console.error("Failed at generate", e);
+    }
+    if (!token) {
+      return null;
+    }
+    try {
+      await authorizeWallet(token);
       return token;
-    };
+    } catch (e) {
+      setLoading(false);
+      console.error(e);
+      return null;
+    }
+  };
 
-    const main = async () => {
-      if (hasCreatedToken.current) {
-        return;
+  const createToken = async () => {
+    const state = window.location.hash.replace("#", "");
+    setLoading(true);
+
+    try {
+      const token = await generateToken();
+      if (!token) {
+        setErrorText(
+          <Text color="red" fontSize="lg">
+            Failed to authorize with CLI, please reach out to us on Discord:{" "}
+            <Link color="blue.200" href="https://discord.gg/thirdweb">
+              https://discord.gg/thirdweb
+            </Link>
+          </Text>,
+        );
+        setLoading(false);
+        console.error(
+          `Failed to authorize with CLI, please reach out to us on Discord: discord.gg/thirdweb.`,
+        );
+        return null;
       }
-
-      if (address) {
-        try {
-          const token = await generateToken();
-          if (!token) {
-            setStatusTitle("Login failed!");
-            setStatusText(
-              <>
-                Failed to authorize with CLI, please reach out to us on Discord:{" "}
-                <Link color="blue.200" href="https://discord.gg/thirdweb">
-                  https://discord.gg/thirdweb
-                </Link>
-              </>,
-            );
-            setLoading(false);
-            console.error(
-              `Failed to authorize with CLI, please reach out to us on Discord: discord.gg/thirdweb.`,
-            );
-            return undefined;
-          }
-          try {
-            const response = await fetch(
-              `http://localhost:8976/auth/callback?token=${token}&state=${state}`,
-              {
-                method: "POST",
-              },
-            );
-            if (response.ok) {
-              setStatusTitle("Login successful!");
-              setStatusText("You can close this window now.");
-              setLoading(false);
-            } else {
-              // This should never happen, but just in case
-              setStatusTitle("Login failed!");
-              setStatusText(
-                <>
-                  Failed to authorize with CLI, please reach out to us on
-                  Discord:{" "}
-                  <Link color="blue.200" href="https://discord.gg/thirdweb">
-                    https://discord.gg/thirdweb
-                  </Link>
-                </>,
-              );
-              setLoading(false);
-              console.error(
-                `Failed to authorize with CLI: ${response.statusText}, please reach out to us on Discord: discord.gg/thirdweb.`,
-              );
-            }
-          } catch (e) {
-            console.error(e);
-          }
-        } catch (e) {
-          console.error(e);
-        }
+      const response = await fetch(
+        `http://localhost:8976/auth/callback?token=${token}&state=${state}`,
+        {
+          method: "POST",
+        },
+      );
+      if (response.ok) {
+        setErrorText(undefined);
+        setLoading(false);
+      } else {
+        // This should never happen, but just in case
+        setErrorText(
+          <Text color="red" fontSize="lg">
+            Failed to authorize with CLI, please reach out to us on Discord:{" "}
+            <Link color="blue.200" href="https://discord.gg/thirdweb">
+              https://discord.gg/thirdweb
+            </Link>
+          </Text>,
+        );
+        setLoading(false);
+        console.error(
+          `Failed to authorize with CLI: ${response.statusText}, please reach out to us on Discord: discord.gg/thirdweb.`,
+        );
       }
-    };
-    main();
-  }, [address, trackEvent, auth, payload, authorizeWallet]);
-
-  if (!address) {
-    return (
-      <Container maxW="lg">
-        <Card p={6} as={Flex} flexDir="column" gap={2}>
-          <Heading as="h2" size="title.sm">
-            Connect your wallet to get started
-          </Heading>
-          <Text>
-            In order to create and manage your developer API keys, you need to
-            sign-in with a wallet.
-          </Text>
-          <Divider my={4} />
-          <ConnectWallet ecosystem="evm" />
-        </Card>
-      </Container>
-    );
-  }
+    } catch (e) {
+      console.error(e);
+      setLoading(false);
+      setErrorText(
+        <Text color="red" fontSize="lg">
+          Failed to authorize with CLI, please reach out to us on Discord:{" "}
+          <Link color="blue.200" href="https://discord.gg/thirdweb">
+            https://discord.gg/thirdweb
+          </Link>
+        </Text>,
+      );
+    }
+  };
 
   return (
     <Container maxW="container.lg" overflow="hidden" h="full">
@@ -143,13 +114,27 @@ const LoginPage: ThirdwebNextPage = () => {
         justifyContent="center"
         alignItems="center"
         h="full"
-        gap={4}
+        gap={8}
       >
-        <Heading size="display.sm">{statusTitle}</Heading>
-        <Heading textAlign="center" size="label.2xl">
-          {statusText}
-        </Heading>
-        {loading && <Spinner />}
+        <VStack>
+          <Heading>
+            Select the account you want to authorize the CLI with
+          </Heading>
+          <small>
+            Note: By doing this, you are authorizing the CLI to bill you for
+            usage on the selected account.
+          </small>
+        </VStack>
+        <ConnectWallet ecosystem="evm" />
+        <Button
+          isDisabled={!address}
+          isLoading={loading}
+          onClick={createToken}
+          colorScheme="green"
+        >
+          Link CLI to Account
+        </Button>
+        {errorText}
       </Flex>
     </Container>
   );
