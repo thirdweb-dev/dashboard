ce000f50372cc717731f84eddedf8d8d2f0c468e
Joaquim Verges <joaquim.verges@gmail.com>
2023-08-24 11:53:31 -0700

    basic smart wallet page

--- contract-ui/tabs/code/components/code-overview.tsx
+++ contract-ui/tabs/code/components/code-overview.tsx
@@ -263,11 +263,20 @@ interface SnippetOptions {
   chainName?: string;
   rpcUrl?: string;
   address?: string;
+  clientId?: string;
 }
 
 export function formatSnippet(
   snippet: Record<CodeEnvironment, any>,
-  { contractAddress, fn, args, chainName, rpcUrl, address }: SnippetOptions,
+  {
+    contractAddress,
+    fn,
+    args,
+    chainName,
+    rpcUrl,
+    address,
+    clientId,
+  }: SnippetOptions,
 ) {
   const code = { ...snippet };
   const preSupportedSlugs = defaultChains.map((chain) => chain.slug);
@@ -278,6 +287,7 @@ export function formatSnippet(
       ?.replace(/{{contract_address}}/gm, contractAddress || "0x...")
       ?.replace(/{{factory_address}}/gm, contractAddress || "0x...")
       ?.replace(/{{wallet_address}}/gm, address)
+      ?.replace("YOUR_CLIENT_ID", clientId || "YOUR_CLIENT_ID")
 
       ?.replace(
         'import {{chainName}} from "@thirdweb-dev/chains";',
--- pages/dashboard/wallets/smart-wallet.tsx
+++ pages/dashboard/wallets/smart-wallet.tsx
@@ -1,11 +1,75 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, FormControl, Radio, RadioGroup, Stack } from "@chakra-ui/react";
 import { AppLayout } from "components/app-layouts/app";
 import { WalletsSidebar } from "core-ui/sidebar/wallets";
 import { PageId } from "page-id";
 import { ThirdwebNextPage } from "utils/types";
 import { Heading, Text } from "tw-components";
+import { useAddress } from "@thirdweb-dev/react";
+import { useMultiChainRegContractList } from "@3rdweb-sdk/react";
+import { useQuery } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import { useApiKeys } from "@3rdweb-sdk/react/hooks/useApi";
+import { CodeSegment } from "components/contract-tabs/code/CodeSegment";
+import { formatSnippet } from "contract-ui/tabs/code/components/code-overview";
+import { WALLETS_SNIPPETS } from "./wallet-sdk";
+import { useState } from "react";
+import { CodeEnvironment } from "components/contract-tabs/code/types";
+import { useChainSlug } from "hooks/chains/chainSlug";
+
+const useFactories = () => {
+  const walletAddress = useAddress();
+  const contracts = useMultiChainRegContractList(walletAddress);
+  return useQuery(
+    [
+      "dashboard-registry",
+      walletAddress,
+      "multichain-contract-list",
+      "factories",
+    ],
+    async () => {
+      if (!walletAddress || !contracts.data || contracts.data.length === 0) {
+        return [];
+      }
+
+      const contractWithExtensions = await Promise.all(
+        contracts.data.map(async (c) => {
+          const extensions =
+            "extensions" in c ? await c.extensions().catch(() => []) : [];
+          return {
+            contract: c,
+            extensions,
+          };
+        }),
+      );
+
+      const factories = contractWithExtensions.filter((c) =>
+        c.extensions.includes("AccountFactory"),
+      );
+
+      return factories;
+    },
+    {
+      enabled: !!walletAddress && !!contracts.data,
+    },
+  );
+};
+
+export type SmartWalletFormData = {
+  chainAndFactoryAddress: string;
+  clientId: string;
+};
 
 const DashboardWalletsSmartWallet: ThirdwebNextPage = () => {
+  const factories = useFactories();
+  const keysQuery = useApiKeys();
+  const form = useForm<SmartWalletFormData>();
+  const [selectedLanguage, setSelectedLanguage] =
+    useState<CodeEnvironment>("javascript");
+  const snippet = WALLETS_SNIPPETS.find((s) => s.id === "smart-wallet");
+  const chainSlug = useChainSlug(
+    form.watch("chainAndFactoryAddress")?.split("-")[0],
+  );
+
   return (
     <Flex flexDir="column" gap={16} mt={{ base: 2, md: 6 }}>
       <Flex flexDir="column" gap={4}>
@@ -14,6 +78,67 @@ const DashboardWalletsSmartWallet: ThirdwebNextPage = () => {
         </Heading>
         <Text>Easily add a smart wallet to your app.</Text>
       </Flex>
+      <FormControl as={Flex} flexDir={"column"} gap={12}>
+        <Flex flexDir="column" gap={4}>
+          <Heading size="title.md" as="h1">
+            Account Factories
+          </Heading>
+          {/* Probably needs to be a dropdown instead */}
+          <RadioGroup
+            onChange={(value: "testnet" | "mainnet") => {
+              form.setValue("chainAndFactoryAddress", value, {
+                shouldValidate: true,
+                shouldDirty: true,
+              });
+            }}
+            value={form.watch("chainAndFactoryAddress")}
+          >
+            <Stack direction="column" gap={4} mt={3}>
+              {factories.data?.map((f) => (
+                <Radio
+                  key={f.contract.address}
+                  value={`${f.contract.chainId}-${f.contract.address}`}
+                >
+                  {f.contract.chainId} - {f.contract.address}
+                </Radio>
+              ))}
+            </Stack>
+          </RadioGroup>
+        </Flex>
+        <Flex flexDir="column" gap={4}>
+          <Heading size="title.md" as="h1">
+            Client IDs
+          </Heading>
+          {/* Probably needs to be a dropdown instead */}
+          <RadioGroup
+            onChange={(value: "testnet" | "mainnet") => {
+              form.setValue("clientId", value, {
+                shouldValidate: true,
+                shouldDirty: true,
+              });
+            }}
+            value={form.watch("clientId")}
+          >
+            <Stack direction="column" gap={4} mt={3}>
+              {keysQuery.data?.map((f) => (
+                <Radio key={f.key} value={f.key}>
+                  {f.name} - {f.key}
+                </Radio>
+              ))}
+            </Stack>
+          </RadioGroup>
+        </Flex>
+      </FormControl>
+      <CodeSegment
+        snippet={formatSnippet(snippet?.supportedLanguages as any, {
+          contractAddress: form.watch("chainAndFactoryAddress")?.split("-")[1],
+          clientId: form.watch("clientId"),
+          chainName: chainSlug?.toString() || "ethereum",
+        })}
+        environment={selectedLanguage}
+        setEnvironment={setSelectedLanguage}
+        hideTabs
+      />
     </Flex>
   );
 };
