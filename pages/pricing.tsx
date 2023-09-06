ef55db8dfa6f37c9df559eedb5ef3ff9a2763306
Nacho Iacovino <nacho.iacovino@gmail.com>
2023-09-06 02:29:25 +0200

    Add table

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -3,6 +3,7 @@ import {
   Center,
   Container,
   Flex,
+  GridItem,
   Icon,
   ListItem,
   SimpleGrid,
@@ -24,9 +25,184 @@ import { IoCheckmarkCircle } from "react-icons/io5";
 
 const TRACKING_CATEGORY = "pricing-page";
 
+const SECTIONS = [
+  {
+    title: "Infrastructure",
+    icon: require("public/assets/product-icons/infrastructure.png"),
+    items: [
+      {
+        title: "RPCs",
+        starter: "100 Requests Per Second",
+        pro: "5000 Requests Per Second",
+      },
+      {
+        title: "Storage Gateway",
+        starter: "100 Requests Per Second",
+        pro: "5000 Requests Per Second",
+      },
+      {
+        title: "Storage Pinning",
+        starter: "Free up to 50 GB ($0.10 per GB after)",
+        pro: "Free up to 50 GB ($0.10 per GB after)",
+      },
+      {
+        title: "Storage Pinning File Size",
+        starter: "5GB (per file size)",
+        pro: "25 GB (per file size)",
+      },
+    ],
+  },
+  {
+    title: "Wallets",
+    icon: require("public/assets/product-icons/wallets.png"),
+    items: [
+      {
+        title: "Email Wallet (Self-Recovery)",
+        starter: "Unlimited",
+        pro: "Unlimited",
+      },
+      {
+        title: "Email Wallet (Managed Recovery)",
+        starter:
+          "Free up to 10,000 Monthly Active Wallets ($0.02 per Wallet after)",
+        pro: "Free up to 10,000 Monthly Active Wallets ($0.02 per Wallet after)",
+      },
+      {
+        title: "Device Wallets",
+        starter: "checkmark",
+        pro: "checkmark",
+      },
+      {
+        title: "Smart Wallets",
+        starter: "checkmark",
+        pro: "checkmark",
+      },
+    ],
+  },
+  {
+    title: "Payments",
+    icon: require("public/assets/product-icons/payments.png"),
+    items: [
+      {
+        title: "NFT Checkout Sellers",
+        starter: "Free",
+        pro: "Free",
+      },
+      {
+        title: "NFT Checkout Purchasers (By Fiat)",
+        starter: "4.9% + $0.30 fee per transaction",
+        pro: "4.9% + $0.30 fee per transaction",
+      },
+      {
+        title: "NFT Checkout Purchasers (By Crypto)",
+        starter: "1% for crypto payments",
+        pro: "1% for crypto payments",
+      },
+      {
+        title: "NFT Checkout Transaction limit",
+        starter: "$2,500 Per Transaction Limit",
+        pro: "$15,000 Per Transaction Limit",
+      },
+      {
+        title: "Smart Wallet - Paymaster",
+        starter: "10% premium on top of network fee",
+        pro: "10% premium on top of network fee",
+      },
+      {
+        title: "Smart Wallet - Bundler",
+        starter: "10% premium on top of network fee",
+        pro: "10% premium on top of network fee",
+      },
+      {
+        title: "Sponsored transactions",
+        starter: "10% premium on top of network fee",
+        pro: "10% premium on top of network fee",
+      },
+    ],
+  },
+  {
+    title: "Platform",
+    icon: require("public/assets/product-icons/dashboards.png"),
+    items: [
+      {
+        title: "Custom RPC / IPFS Domains",
+        starter: "--",
+        pro: "checkmark",
+      },
+      {
+        title: "Sponsored transactions",
+        starter: "--",
+        pro: "checkmark",
+      },
+      {
+        title: "White labelling",
+        comingSoon: true,
+        starter: "--",
+        pro: "checkmark",
+      },
+      {
+        title: "Team Seats",
+        comingSoon: true,
+        starter: "--",
+        pro: "checkmark",
+      },
+      {
+        title: "Appchain/Subnet Support",
+        comingSoon: true,
+        starter: "--",
+        pro: "checkmark",
+      },
+      {
+        title: "Sign Sign-on",
+        comingSoon: true,
+        starter: "--",
+        pro: "checkmark",
+      },
+      {
+        title: "HTML files for IPFS",
+        comingSoon: true,
+        starter: "--",
+        pro: "checkmark",
+      },
+    ],
+  },
+  {
+    title: "Support",
+    icon: require("public/assets/product-icons/support.png"),
+    items: [
+      {
+        title: "Infrastructure SLAs",
+        starter: "--",
+        pro: "99.9% Uptime",
+      },
+      {
+        title: "Customer support SLAs",
+        starter: "--",
+        pro: "24 hours Dedicated Support",
+      },
+      {
+        title: "Prioritized Customer Support",
+        starter: "--",
+        pro: "checkmark",
+      },
+      {
+        title: "Dedicated Slack support channel",
+        starter: "Discord Support Only",
+        pro: "checkmark",
+      },
+    ],
+  },
+];
+
 const Pricing: ThirdwebNextPage = () => {
   return (
-    <LandingLayout seo={{}}>
+    <LandingLayout
+      seo={{
+        title: "Build production-grade web3 apps at scale | thirdweb Pro",
+        description:
+          "Build brand loyalty programs that turn customeThe best way to build web3 apps for millions of users — with a robust infrastructure stack that scales as you grow. Learn more.",
+      }}
+    >
       <Container
         maxW="container.page"
         as={Flex}
@@ -97,7 +273,7 @@ const Pricing: ThirdwebNextPage = () => {
                 <CheckmarkItem text="Storage Pinning (First 50GB free)" />
                 <CheckmarkItem text="NFT Checkout" />
                 <CheckmarkItem text="Email Wallets" />
-                <CheckmarkItem text="Device Wallet" />
+                <CheckmarkItem text="Device Wallets" />
                 <CheckmarkItem text="Smart Wallets" />
               </Flex>
               <TrackedLinkButton
@@ -159,6 +335,107 @@ const Pricing: ThirdwebNextPage = () => {
             </Card>
           </SimpleGrid>
         </Container>
+        <Flex flexDir="column" gap={20}>
+          {SECTIONS.map((section) => (
+            <Flex flexDir="column" key={section.title} gap={4}>
+              <SimpleGrid columns={3}>
+                <Flex gap={2} alignItems="center">
+                  <ChakraNextImage src={section.icon} boxSize={5} alt="" />{" "}
+                  <Text color="white" size="body.xl">
+                    {section.title}
+                  </Text>
+                </Flex>
+                <Text size="body.xl" color="gray.700" textAlign="center">
+                  Starter
+                </Text>
+                <Text size="body.xl" color="gray.700" textAlign="center">
+                  Pro
+                </Text>
+              </SimpleGrid>
+              <Flex flexDir="column">
+                {section.items.map((item) => (
+                  <SimpleGrid columns={3} key={item.title} alignItems="center">
+                    <Card
+                      borderRadius="none"
+                      borderColor="gray.900"
+                      borderLeft="none"
+                    >
+                      <Text color="white" size="body.lg">
+                        {item.title}
+                      </Text>
+                    </Card>
+                    {item.starter === item.pro ? (
+                      <GridItem colSpan={2}>
+                        <Card
+                          borderRadius="none"
+                          borderColor="gray.900"
+                          borderRight="none"
+                        >
+                          <Text
+                            color="gray.700"
+                            size="body.lg"
+                            textAlign="center"
+                          >
+                            {item.starter === "checkmark" ? (
+                              <Icon
+                                as={IoCheckmarkCircle}
+                                boxSize={4}
+                                color="blue.500"
+                              />
+                            ) : (
+                              item.starter
+                            )}
+                          </Text>
+                        </Card>
+                      </GridItem>
+                    ) : (
+                      <>
+                        <Card borderRadius="none" borderColor="gray.900">
+                          <Text
+                            color="gray.700"
+                            size="body.lg"
+                            textAlign="center"
+                          >
+                            {item.starter === "checkmark" ? (
+                              <Icon
+                                as={IoCheckmarkCircle}
+                                boxSize={4}
+                                color="blue.500"
+                              />
+                            ) : (
+                              item.starter
+                            )}
+                          </Text>
+                        </Card>
+                        <Card
+                          borderRadius="none"
+                          borderColor="gray.900"
+                          borderRight="none"
+                        >
+                          <Text
+                            color="gray.700"
+                            size="body.lg"
+                            textAlign="center"
+                          >
+                            {item.pro === "checkmark" ? (
+                              <Icon
+                                as={IoCheckmarkCircle}
+                                boxSize={4}
+                                color="blue.500"
+                              />
+                            ) : (
+                              item.pro
+                            )}
+                          </Text>
+                        </Card>
+                      </>
+                    )}
+                  </SimpleGrid>
+                ))}
+              </Flex>
+            </Flex>
+          ))}
+        </Flex>
       </Container>
     </LandingLayout>
   );
