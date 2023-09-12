6129c21ccf2b120490909919164e776f5e74d4f4
Nacho Iacovino <nacho.iacovino@gmail.com>
2023-09-12 20:54:04 +0200

    Update images

--- components/landing-pages/dynamic-selector.tsx
+++ components/landing-pages/dynamic-selector.tsx
@@ -1,6 +1,6 @@
 import { LandingSectionHeading } from "./section-heading";
 import { LandingSectionHeadingProps } from "./types";
-import { Box, Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Flex, GridItem, SimpleGrid } from "@chakra-ui/react";
 import { ReactElement, useState } from "react";
 import { Text } from "tw-components";
 
@@ -12,14 +12,15 @@ interface SelectorItem {
 
 interface LandingDynamicSelectorProps extends LandingSectionHeadingProps {
   items: SelectorItem[];
+  gradient?: string;
 }
 
 export const LandingDynamicSelector: React.FC<LandingDynamicSelectorProps> = ({
   items,
   title,
+  gradient = "linear(to-r, #BFA3DA, #84309C, #C735B0)",
   blackToWhiteTitle,
 }) => {
-  const isMobile = useBreakpointValue({ base: true, md: false });
   const [selectedItemTitle, setSelectedItemTitle] = useState(items[0].title);
 
   const selectedItem =
@@ -31,49 +32,63 @@ export const LandingDynamicSelector: React.FC<LandingDynamicSelectorProps> = ({
         title={title}
         blackToWhiteTitle={blackToWhiteTitle}
       />
-      <Flex gap={12}>
-        <Flex flexDir="column" w={{ base: "100%", md: "80%" }}>
+      <SimpleGrid
+        borderRadius="xl"
+        borderColor="gray.900"
+        borderWidth={1}
+        columns={12}
+        overflow="hidden"
+      >
+        <GridItem
+          as={SimpleGrid}
+          columns={1}
+          w={{ base: "100%", md: "100%" }}
+          colSpan={{ base: 12, md: 4 }}
+        >
           {items.map((item, index) => (
-            <Flex key={index}>
+            <Flex
+              key={index}
+              borderColor="gray.900"
+              borderWidth={1}
+              p={8}
+              borderLeft="none"
+              _last={{ borderBottom: "none" }}
+              _first={{ borderTop: "none" }}
+            >
               <Flex
                 onClick={() => setSelectedItemTitle(item.title)}
                 cursor={{ base: "default", md: "pointer" }}
                 pointerEvents={{ base: "none", md: "all" }}
                 flexDir="column"
-                gap={6}
                 w="full"
               >
-                <Flex
-                  borderBottom="2px solid"
-                  borderBottomColor={
-                    selectedItemTitle === item.title || isMobile
-                      ? "primary.500"
-                      : "gray.800"
+                <Text
+                  size="label.lg"
+                  bgClip="text"
+                  bgGradient={
+                    selectedItemTitle === item.title
+                      ? gradient
+                      : "linear(to-r, #fff, #fff)"
                   }
-                  py={6}
+                  pb={4}
                 >
-                  <Text size="label.lg">{item.title}</Text>
-                </Flex>
-                <Box>
-                  <Text
-                    size="body.md"
-                    display={
-                      isMobile || selectedItemTitle === item.title
-                        ? "inherit"
-                        : "none"
-                    }
-                  >
-                    {item.description}
-                  </Text>
-                </Box>
+                  {item.title}
+                </Text>
+                <Text size="body.md">{item.description}</Text>
               </Flex>
             </Flex>
           ))}
-        </Flex>
-        <Flex display={{ base: "none", md: "inherit" }}>
+        </GridItem>
+        <GridItem
+          as={Flex}
+          display={{ base: "none", md: "inherit" }}
+          colSpan={8}
+          p={8}
+          alignItems="center"
+        >
           <Box ml={5}>{selectedItem.Component}</Box>
-        </Flex>
-      </Flex>
+        </GridItem>
+      </SimpleGrid>
     </Flex>
   );
 };
