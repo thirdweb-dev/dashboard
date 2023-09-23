6869d452a0f8275cbd88404c8b0ad0be3c589d6e
Nacho Iacovino <nacho.iacovino@gmail.com>
2023-09-23 02:22:55 +0200

    Now better logic for real

--- components/landing-pages/desktop-mobile-image.tsx
+++ components/landing-pages/desktop-mobile-image.tsx
@@ -1,17 +1,25 @@
 import { LandingDesktopMobileImageProps } from "./types";
-import { useBreakpointValue } from "@chakra-ui/react";
 import { ChakraNextImage } from "components/Image";
 
 export const LandingDesktopMobileImage: React.FC<
   LandingDesktopMobileImageProps
 > = ({ image, mobileImage }) => {
-  const isDesktop = useBreakpointValue({ base: false, md: true });
-
-  const imageToShow = !isDesktop && mobileImage ? mobileImage : image;
-
-  if (!imageToShow) {
-    return null;
-  }
-
-  return <ChakraNextImage src={imageToShow} alt="" />;
+  return (
+    <>
+      {mobileImage && (
+        <ChakraNextImage
+          src={mobileImage}
+          alt=""
+          display={{ base: "block", md: "none" }}
+        />
+      )}
+      {image && (
+        <ChakraNextImage
+          src={image}
+          alt=""
+          display={{ base: mobileImage ? "none" : "block", md: "block" }}
+        />
+      )}
+    </>
+  );
 };
