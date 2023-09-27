1e87cd51b60622a1d841604bd34d103d6c7f3a79
Nacho Iacovino <50103937+nachoiacovino@users.noreply.github.com>
2023-09-27 12:19:46 +0200

    /connect v2 (#1710)
    * Update all landing pages (#1697)
    
    * Stuff
    
    * Pushy push
    
    * wut
    
    * ?
    
    * ??
    
    * Amazing
    
    * New images
    
    * :)
    
    * Menu sections
    
    * Fix build
    
    * Better logic
    
    * local images
    
    * Storage hero
    
    * Now better logic for real
    
    * chore: update landing page copy (#1708)
    
    * chore: update env examples based on tair's DM
    
    * chore: update build page vercel comment
    
    * chore(storage): update copy from feedback
    
    * chore(deploy): update copy
    
    * chore(sponsored-transactions): update copy
    
    * chore(embedded-wallets): update copy
    
    * chore(publish): update copy
    
    * chore(checkout): update copy
    
    * chore(smart-contracts): update copy
    
    * chore: update hero images
    
    * chore: update mobile product hero
    
    * chore(build): update build icons
    
    * chore(interact): Update interact icons
    
    * chore(smart-contracts): add page icons
    
    * chore(publish): add publish icons
    
    * chore(deploy): add deploy icons
    
    * refactor(contracts): update icon contract pages
    
    * chore(connect): add connect icons
    
    * chore(embedded-wallets): add icons for embedded-wallets
    
    * chore(auth): add auth icons
    
    * chore(checkout): add checkout page icons
    
    * chore(sponsored-transaction): add icons for sponsored transactions
    
    * chore(storage): add storage page icons
    
    * chore(rpc-edge): add rpc-edge page icons
    
    * chore(checkout): update checkout icon
    
    * chore(nav): update nav icons
    
    * chore(contracts): update gradient to  #F213A4, #F97CCE
    
    * chore(payments): update gradient #00A876, #75FFD6
    
    * chore(wallets): update gradient to #3385FF, #7BB0FF
    
    * chore(connect): update copy
    
    ---------
    
    Co-authored-by: Jonas Daniels <jonas.daniels@outlook.com>
    Co-authored-by: Winston Yeo <44563205+ElasticBottle@users.noreply.github.com>
    Co-authored-by: ElasticBottle <winstonyeo99@yahoo.com>
    
    * Fix wrong link (#1709)
    
    Fix link
    
    * New sectio on /connect
    
    * Updated images
    
    ---------
    
    Co-authored-by: Jonas Daniels <jonas.daniels@outlook.com>
    Co-authored-by: Winston Yeo <44563205+ElasticBottle@users.noreply.github.com>
    Co-authored-by: ElasticBottle <winstonyeo99@yahoo.com>
    Co-authored-by: Manan Tank <manantankm@gmail.com>

---
+++ pages/connect.tsx
@@ -0,0 +1,250 @@
+import { Center, Container, Flex } from "@chakra-ui/react";
+import { LandingCardWithImage } from "components/landing-pages/card-with-image";
+import { LandingEndCTA } from "components/landing-pages/end-cta";
+import { LandingGridSection } from "components/landing-pages/grid-section";
+import { LandingHeroWithSideImage } from "components/landing-pages/hero-with-side-image";
+import { LandingIconSectionItem } from "components/landing-pages/icon-section-item";
+import { LandingLayout } from "components/landing-pages/layout";
+import { getAbsoluteUrl } from "lib/vercel-utils";
+import { PageId } from "page-id";
+import { Card, Heading, TrackedLink } from "tw-components";
+import { ThirdwebNextPage } from "utils/types";
+
+const TRACKING_CATEGORY = "connect-wallet-landing";
+
+const ConnectLanding: ThirdwebNextPage = () => {
+  return (
+    <LandingLayout
+      bgColor="#0F0F0F"
+      seo={{
+        title: "Web3 Onboarding, Built for the Next Billion Users",
+        description:
+          "Onboard your users to web3 with a beautiful Connect Wallet modal, customizable auth flows, and sign-in for web2 & web3 — in a few lines of code.",
+        openGraph: {
+          images: [
+            {
+              url: `${getAbsoluteUrl()}/assets/og-image/embedded-wallets.png`,
+              width: 1200,
+              height: 630,
+              alt: "Web3 Onboarding, Built for the Next Billion Users",
+            },
+          ],
+        },
+      }}
+    >
+      <Container
+        maxW="container.page"
+        as={Flex}
+        flexDir="column"
+        gap={{ base: "80px", md: "120px" }}
+      >
+        <LandingHeroWithSideImage
+          miniTitle="Connect"
+          title="Web3 onboarding,"
+          titleWithGradient="built for the next billion users"
+          subtitle="Onboard all of your users with a powerful Connect Wallet modal, flexible sign-in options for web2 & web3, and hooks for full customizability."
+          trackingCategory={TRACKING_CATEGORY}
+          ctaLink="/dashboard/wallets/connect"
+          contactUsTitle="Book Demo"
+          gradient="linear(to-r, #3385FF, #7BB0FF)"
+          image={require("public/assets/product-pages/hero/desktop-hero-connect-wallet.png")}
+          mobileImage={require("public/assets/product-pages/hero/mobile-hero-connect-wallet.png")}
+        />
+
+        <Flex flexDir="column" gap={6}>
+          <LandingGridSection
+            title={
+              <Center>
+                <Heading size="display.sm" color="white">
+                  Every way to connect a wallet
+                </Heading>
+              </Center>
+            }
+            desktopColumns={4}
+          >
+            <LandingCardWithImage
+              title="Web3 onboarding, for everyone"
+              description="Sign-in options for the most popular web3 wallets, and familiar web2 login flows."
+              image={require("public/assets/product-pages/connect/desktop-onboarding-everyone.png")}
+              mobileImage={require("public/assets/product-pages/connect/mobile-onboarding-everyone.png")}
+              TRACKING_CATEGORY={TRACKING_CATEGORY}
+              href="/dashboard/wallets/connect"
+            />
+            <LandingCardWithImage
+              title="Infinite customizability"
+              description="A Connect Wallet UI component with your branding, different modal styles, and everything else to make it yours."
+              image={require("public/assets/product-pages/connect/desktop-customizability.png")}
+              mobileImage={require("public/assets/product-pages/connect/mobile-customizability.png")}
+              TRACKING_CATEGORY={TRACKING_CATEGORY}
+              href="/dashboard/wallets/connect"
+            />
+          </LandingGridSection>
+          <LandingGridSection desktopColumns={4}>
+            <LandingCardWithImage
+              title="Built-in auth"
+              description={
+                <>
+                  Verify a user&apos;s onchain identity with web3-first
+                  authentication, using the SIWE (
+                  <TrackedLink
+                    href="https://eips.ethereum.org/EIPS/eip-4361"
+                    category={TRACKING_CATEGORY}
+                    label="siwe"
+                    color="blue.500"
+                    isExternal
+                  >
+                    Sign-in with Ethereum
+                  </TrackedLink>
+                  ) standard.
+                </>
+              }
+              image={require("public/assets/product-pages/connect/desktop-auth.png")}
+              mobileImage={require("public/assets/product-pages/connect/mobile-auth.png")}
+              TRACKING_CATEGORY={TRACKING_CATEGORY}
+              href="/auth"
+              colSpan={1}
+            />
+            <LandingCardWithImage
+              title="No wallet? No problem"
+              description="Onboard users with just an email, phone, social account, or passkeys — and create wallets for them."
+              image={require("public/assets/product-pages/connect/desktop-no-wallet.png")}
+              mobileImage={require("public/assets/product-pages/connect/mobile-no-wallet.png")}
+              TRACKING_CATEGORY={TRACKING_CATEGORY}
+              href="/embedded-wallets"
+            />
+            <LandingCardWithImage
+              title="Guest mode"
+              description="Allow anyone to use your app in seconds — with a wallet that's generated automatically when they press Continue as guest."
+              image={require("public/assets/product-pages/connect/desktop-guest.png")}
+              mobileImage={require("public/assets/product-pages/connect/mobile-guest.png")}
+              TRACKING_CATEGORY={TRACKING_CATEGORY}
+              href="/wallet/local-wallet"
+              colSpan={1}
+            />
+          </LandingGridSection>
+          <LandingGridSection desktopColumns={4}>
+            <LandingCardWithImage
+              title="Reliable connectors"
+              description="Integrate 170+ web3 wallets — including self-custodial, email, local, & smart wallets."
+              image={require("public/assets/product-pages/connect/desktop-reliable-connectors.png")}
+              mobileImage={require("public/assets/product-pages/connect/mobile-reliable-connectors.png")}
+              TRACKING_CATEGORY={TRACKING_CATEGORY}
+              href="https://portal.thirdweb.com/wallet"
+            />
+            <LandingCardWithImage
+              title="Everything a wallet needs"
+              description="Functions to send, receive, & view assets. ENS support, transaction history, & more."
+              image={require("public/assets/product-pages/connect/desktop-everything-wallet-needs.png")}
+              mobileImage={require("public/assets/product-pages/connect/mobile-everything-wallet-needs.png")}
+              TRACKING_CATEGORY={TRACKING_CATEGORY}
+              href="https://portal.thirdweb.com/wallet"
+              direction="horizontal"
+            />
+          </LandingGridSection>
+          <LandingGridSection desktopColumns={4}>
+            <LandingCardWithImage
+              title="Powerful hooks"
+              description="Flexible hooks, functions, and low-level tools for fully custom Connect Wallet experiences."
+              image={require("public/assets/product-pages/connect/desktop-hooks.png")}
+              mobileImage={require("public/assets/product-pages/connect/mobile-hooks.png")}
+              TRACKING_CATEGORY={TRACKING_CATEGORY}
+              href="/wallet-sdk"
+              colSpan={1}
+            />
+            <LandingCardWithImage
+              title="Account abstraction, made simple"
+              description="First-class support for account abstraction — with native smart wallet experiences that anyone can use."
+              image={require("public/assets/product-pages/connect/desktop-account-abstraction.png")}
+              mobileImage={require("public/assets/product-pages/connect/mobile-account-abstraction.png")}
+              TRACKING_CATEGORY={TRACKING_CATEGORY}
+              href="https://portal.thirdweb.com/smart-wallet"
+            />
+            <LandingCardWithImage
+              title="Wallet analytics"
+              description="Comprehensive wallet insights to understand how users are interacting with your app."
+              image={require("public/assets/product-pages/connect/desktop-analytics.png")}
+              mobileImage={require("public/assets/product-pages/connect/mobile-analytics.png")}
+              TRACKING_CATEGORY={TRACKING_CATEGORY}
+              href="/dashboard/wallets/connect"
+              colSpan={1}
+            />
+          </LandingGridSection>
+        </Flex>
+
+        <LandingGridSection>
+          <LandingIconSectionItem
+            icon={require("public/assets/product-pages-icons/wallets/icon-custom.svg")}
+            title="Your Connect Wallet experience"
+            description="Build custom onboarding flows with a powerful UI component — with web3 & web2 login options, personalized branding & themes, and everything you need to tailor it to your app."
+          />
+          <LandingIconSectionItem
+            icon={require("public/assets/product-pages-icons/wallets/icon-email-signin.svg")}
+            title="Instant onboarding for all"
+            description="Authenticate & onboard users easily — with just an email, phone, or social account. Generate wallets for your users under the hood, or empower them to create their first self-custodial wallet."
+          />
+          <LandingIconSectionItem
+            icon={require("public/assets/product-pages-icons/wallets/icon-simple-click.svg")}
+            title="Best-in-class DX"
+            description="Integrate with just a few lines of code — with an interactive builder, powerful hooks for full customization, and onchain analytics."
+          />
+        </LandingGridSection>
+        <LandingGridSection>
+          <Card p={8}>
+            <LandingIconSectionItem
+              icon={require("public/assets/product-pages-icons/wallets/icon-custom.svg")}
+              title="An intuitive onboarding experience"
+              description="Add the power of web3 to any app with our Connect Wallet UI component — with cross-platform support (Web, Mobile, Unity) and the smoothest user experience for every type of wallet."
+            />
+          </Card>
+          <Card p={8}>
+            <LandingIconSectionItem
+              icon={require("public/assets/product-pages-icons/wallets/icon-email-signin.svg")}
+              title="Web2 login flows"
+              description="Instantly onboard any user with just an email, phone number, social login, or passkeys — and automatically generate a wallet for them after they sign up."
+            />
+          </Card>
+          <Card p={8}>
+            <LandingIconSectionItem
+              icon={require("public/assets/product-pages-icons/wallets/icon-evm.svg")}
+              title="Cross-chain compatibility"
+              description="Build web3 apps on any (or many) chains with native EVM-compatibility — and the smoothest UX with automatic network switching."
+            />
+          </Card>
+          <Card p={8}>
+            <LandingIconSectionItem
+              icon={require("public/assets/product-pages-icons/wallets/icon-personalize.svg")}
+              title="Personalized user experience"
+              description="The Connect Wallet component automatically detects which wallets a user has installed on their browser, recommending them to select the wallet that they can get started with the most easily."
+            />
+          </Card>
+          <Card p={8}>
+            <LandingIconSectionItem
+              icon={require("public/assets/product-pages-icons/wallets/icon-secure.svg")}
+              title="Reliable connectors"
+              description="Use our connectors to integrate 170+ wallet providers into any app — including non-custodial wallets (MetaMask, Coinbase Wallet, Wallet Connect), email wallets, local wallets, & native Safe integration."
+            />
+          </Card>
+          <Card p={8}>
+            <LandingIconSectionItem
+              icon={require("public/assets/product-pages-icons/wallets/icon-guest.svg")}
+              title="Guest mode"
+              description="Onboard new users to any dApp in seconds, allowing them to 'Continue as guest' — at the press of a button or through a username-and-password flow — and automatically creating a wallet for them under the hood."
+            />
+          </Card>
+        </LandingGridSection>
+
+        <LandingEndCTA
+          title="Integrate in"
+          titleWithGradient="a few lines of code."
+          trackingCategory={TRACKING_CATEGORY}
+          ctaLink="/dashboard/wallets/connect"
+          gradient="linear(to-r, #3385FF, #7BB0FF)"
+        />
+      </Container>
+    </LandingLayout>
+  );
+};
+
+ConnectLanding.pageId = PageId.ConnectLanding;
+
+export default ConnectLanding;
