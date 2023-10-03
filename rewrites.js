863ddb69216988d0611e71ada4d8f4d865579084
Tair Asim <hi@tairasim.com>
2023-10-03 14:06:47 +0200

    fix

--- rewrites.js
+++ rewrites.js
@@ -2,21 +2,30 @@
 
 const FRAMER_SITE = "https://smooth-selfies-126433.framer.app";
 
+// FIXME: When all landing pages moved to framer,
+//        rewrite everything but dashboard routes instead
+
+const FRAMER_PAGES = ["/smart-contracts"];
+
 function rewrites() {
-  return [
-    {
-      source: "/",
-      destination: FRAMER_SITE,
-    },
-    {
-      source: "/thirdweb.eth",
-      destination: "/deployer.thirdweb.eth",
-    },
-    {
-      source: "/thirdweb.eth/:path*",
-      destination: "/deployer.thirdweb.eth/:path*",
-    },
-  ];
+  const framerPages = FRAMER_PAGES.map((p) => ({
+    source: p,
+    destination: FRAMER_SITE,
+  }));
+
+  return {
+    beforeFiles: [
+      ...framerPages,
+      {
+        source: "/thirdweb.eth",
+        destination: "/deployer.thirdweb.eth",
+      },
+      {
+        source: "/thirdweb.eth/:path*",
+        destination: "/deployer.thirdweb.eth/:path*",
+      },
+    ],
+  };
 }
 
 module.exports = rewrites;
