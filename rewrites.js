ff76c5dcf8e985ba5b99802d2e070fcbbf96ce9d
Tair Asim <hi@tairasim.com>
2023-10-03 14:33:23 +0200

    add solutions

--- rewrites.js
+++ rewrites.js
@@ -1,17 +1,26 @@
 /** @type {import('next').NextConfig['rewrites']} */
 
 const FRAMER_SITE = "https://smooth-selfies-126433.framer.app";
-
-// FIXME: When all landing pages moved to framer,
-//        rewrite everything but dashboard routes instead
-
-const FRAMER_PAGES = ["/smart-contracts"];
+const FRAMER_PAGES = ["/smart-contracts", "/solutions*"];
 
 function rewrites() {
-  const framerPages = FRAMER_PAGES.map((p) => ({
-    source: p,
-    destination: FRAMER_SITE,
-  }));
+  // FIXME: When all landing pages moved to framer,
+  //        rewrite everything but dashboard routes instead
+  const framerPages = FRAMER_PAGES.map((p) => {
+    if (p.endsWith("*")) {
+      const path = p.slice(0, -1);
+
+      return {
+        source: `${path}/:path*`,
+        destination: `${FRAMER_SITE}${path}/:path*`,
+      };
+    }
+
+    return {
+      source: p,
+      destination: `${FRAMER_SITE}${p}`,
+    };
+  });
 
   return {
     beforeFiles: [
