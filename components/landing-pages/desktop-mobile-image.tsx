aa47c88b264b3db0cbcbb00a7071eaff859f0bed
Manan Tank <manantankm@gmail.com>
2023-10-13 19:58:23 +0530

    Video improvements

--- components/landing-pages/desktop-mobile-image.tsx
+++ components/landing-pages/desktop-mobile-image.tsx
@@ -1,3 +1,4 @@
+import { Box } from "@chakra-ui/react";
 import { LandingDesktopMobileImageProps } from "./types";
 import { ChakraNextImage } from "components/Image";
 
@@ -25,14 +26,18 @@ export const LandingDesktopMobileImage: React.FC<
       )}
 
       {props.video && (
-        <video
+        <Box
+          as="video"
           loop
           playsInline
           preload="auto"
-          poster={image?.src}
+          width="550"
+          height="400"
           autoPlay
           src={props.video}
           muted
+          boxShadow="0 0 50px #0e0d0f"
+          borderRadius="50px"
         />
       )}
     </>
