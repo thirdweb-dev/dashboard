22321c40d6da46f0d70e9e6233fd3362339d2dbe
Manan Tank <manantankm@gmail.com>
2023-10-13 19:19:33 +0530

    Use video instead of lottie

--- components/landing-pages/desktop-mobile-image.tsx
+++ components/landing-pages/desktop-mobile-image.tsx
@@ -1,11 +1,9 @@
-import { Box } from "@chakra-ui/react";
 import { LandingDesktopMobileImageProps } from "./types";
 import { ChakraNextImage } from "components/Image";
-import Lottie from "lottie-react";
 
 export const LandingDesktopMobileImage: React.FC<
   LandingDesktopMobileImageProps
-> = ({ lottie, image, mobileImage, ...props }) => {
+> = ({ image, mobileImage, ...props }) => {
   return (
     <>
       {mobileImage && (
@@ -16,7 +14,7 @@ export const LandingDesktopMobileImage: React.FC<
           alt=""
         />
       )}
-      {!lottie && image && (
+      {!props.video && image && (
         <ChakraNextImage
           {...props}
           src={image}
@@ -24,14 +22,17 @@ export const LandingDesktopMobileImage: React.FC<
           display={{ base: mobileImage ? "none" : "block", md: "block" }}
         />
       )}
-      {lottie && (
-        <Box
-          w="100%"
-          h="100%"
-          display={{ base: mobileImage ? "none" : "block", md: "block" }}
-        >
-          <Lottie animationData={lottie} loop={true} />
-        </Box>
+
+      {props.video && (
+        <video
+          loop
+          playsInline
+          preload="auto"
+          poster={image?.src}
+          autoPlay
+          src={props.video}
+          muted
+        />
       )}
     </>
   );
