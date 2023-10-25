d5f03b03cd5c9f1d15ffbe92129b8e7afc426893
Nacho Iacovino <nacho.iacovino@gmail.com>
2023-10-25 03:45:58 +0200

    Exploration of swagger css commented

--- components/engine/explorer/engine-explorer.tsx
+++ components/engine/explorer/engine-explorer.tsx
@@ -12,11 +12,16 @@ interface EngineExplorerProps {
 export const EngineExplorer: React.FC<EngineExplorerProps> = ({ instance }) => {
   return (
     <ClientOnly ssr={null}>
-      <Box bg="white" borderRadius="xl">
+      {/*       <DarkMode> */}
+      <Box /* bg="backgroundDark" */ bg="#fff" borderRadius="xl">
         <SwaggerUI
           url={`${instance}${instance.endsWith("/") ? "" : "/"}json`}
+          docExpansion="none"
+          persistAuthorization={true}
+          withCredentials={true}
         />
       </Box>
+      {/*       </DarkMode> */}
     </ClientOnly>
   );
 };
