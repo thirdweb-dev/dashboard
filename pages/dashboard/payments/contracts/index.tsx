9435026bb38d656631c8f962ee265520d5936a6f
Edward Sun <eedwardsun@gmail.com>
2023-10-25 22:21:32 -0700

    Push up example with `paymentsSellerId`

--- pages/dashboard/payments/contracts/index.tsx
+++ pages/dashboard/payments/contracts/index.tsx
@@ -1,10 +1,24 @@
+import { useApiAuthToken } from "@3rdweb-sdk/react/hooks/useApi";
 import { AppLayout } from "components/app-layouts/app";
 import { PaymentsSidebar } from "core-ui/sidebar/payments";
+import {
+  ContractsByOwnerIdQueryVariables,
+  useContractsByOwnerIdQuery,
+} from "graphql/queries/__generated__/ContractsByOwnerId.generated";
 import { PageId } from "page-id";
 import { Link } from "tw-components";
 import { ThirdwebNextPage } from "utils/types";
 
 const PaymentsContracts: ThirdwebNextPage = () => {
+  const { paymentsSellerId } = useApiAuthToken();
+  const contracts = useContractsByOwnerIdQuery({
+    variables: {
+      ownerId: paymentsSellerId,
+    } as ContractsByOwnerIdQueryVariables,
+  });
+
+  console.log('contracts is ', contracts);
+  
   return (
     <div>
       Payments Contracts Index
