354c5ee137732a770eb5fa61920c6beff6e6ebcd
Tair Asim <hi@tairasim.com>
2023-10-31 13:18:35 +0100

    fix: use logged in user

--- @3rdweb-sdk/react/hooks/useLoggedInUser.ts
+++ @3rdweb-sdk/react/hooks/useLoggedInUser.ts
@@ -3,8 +3,9 @@ import { useAddress, useUser } from "@thirdweb-dev/react";
 export function useLoggedInUser(): ReturnType<typeof useUser> {
   const connectedAddress = useAddress();
   const user = useUser();
-  // ONLY RETURN THE USER IF THE ADDRESS MATCHES
-  if (user.user?.address === connectedAddress) {
+
+  // ONLY RETURN THE USER IF THE ADDRESS MATCHES AND USER LOGGED IN
+  if (user.isLoggedIn && user.user?.address === connectedAddress) {
     return user;
   }
 
