9a4f4c0ae8b20b699e4b6cb654e0e84c5bf5d323
Manan Tank <manantankm@gmail.com>
2023-10-31 20:39:27 +0530

    Localization Test

--- components/wallets/ConnectWalletPlayground/PreviewThirdwebProvider.tsx
+++ components/wallets/ConnectWalletPlayground/PreviewThirdwebProvider.tsx
@@ -1,17 +1,24 @@
-import { ThirdwebProvider, WalletConfig } from "@thirdweb-dev/react";
+import { ThirdwebProvider, WalletConfig, en, jp } from "@thirdweb-dev/react";
 import React from "react";
 import { THIRDWEB_DOMAIN, THIRDWEB_API_HOST } from "constants/urls";
 import { DASHBOARD_THIRDWEB_CLIENT_ID, isProd } from "constants/rpc";
 import { defaultChains } from "@thirdweb-dev/chains";
 import { StorageSingleton } from "lib/sdk";
 
+const localeIdToObj = {
+  en: en(),
+  jp: jp(),
+};
+
 export function PreviewThirdwebProvider(props: {
   authEnabled: boolean;
   supportedWallets: WalletConfig<any>[];
   children: React.ReactNode;
+  locale?: "en" | "jp";
 }) {
   return (
     <ThirdwebProvider
+      locale={props.locale ? localeIdToObj[props.locale] : undefined}
       activeChain="goerli"
       supportedWallets={
         props.supportedWallets.length > 0 ? props.supportedWallets : undefined
