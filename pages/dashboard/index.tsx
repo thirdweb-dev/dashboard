1f4790412a5fe221ff1dceb4792d53f0f15ad64c
Nacho Iacovino <nacho.iacovino@gmail.com>
2023-11-08 00:12:59 +0100

    New ftux product cards

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,71 +1,23 @@
-import {
-  DarkMode,
-  Flex,
-  GridItem,
-  SimpleGrid,
-  VStack,
-  useColorMode,
-} from "@chakra-ui/react";
+import { Flex, GridItem, SimpleGrid, VStack } from "@chakra-ui/react";
 import { useConnectionStatus } from "@thirdweb-dev/react";
 import { ClientOnly } from "components/ClientOnly/ClientOnly";
 import { FTUX } from "components/FTUX/FTUX";
-import { ChakraNextImage } from "components/Image";
 import { AppLayout } from "components/app-layouts/app";
 import { Changelog, ChangelogItem } from "components/dashboard/Changelog";
-import { NavigationCard } from "components/dashboard/NavigationCard";
+import { HomeProductCard } from "components/dashboard/HomeProductCard";
 import { OnboardingSteps } from "components/onboarding/Steps";
 import { PRODUCTS } from "components/product-pages/common/nav/data";
 import { GetStaticProps, InferGetStaticPropsType } from "next";
 import { PageId } from "page-id";
-import { Card, Heading, Text } from "tw-components";
+import { Heading } from "tw-components";
 import { ThirdwebNextPage } from "utils/types";
 
 const TRACKING_CATEGORY = "dashboard";
-
-const GET_STARTED_SECTIONS = [
-  {
-    title: "Deploy a contract",
-    description:
-      "Explore contracts from world-class web3 protocols & engineers- all deployable with 1-click to any chain.",
-    image: require("public/assets/dashboard/home-contracts.png"),
-    href: "/dashboard/contracts/deploy",
-    badge: "Contracts",
-    badgeColor: "#6820CB",
-  },
-  {
-    title: "Onboard users",
-    description:
-      "A complete toolkit for connecting wallets to apps. UI components that work out of the box.",
-    image: require("public/assets/dashboard/home-wallets.png"),
-    href: "/dashboard/wallets/connect",
-    badge: "Wallets",
-    badgeColor: "blue.500",
-  },
-  {
-    title: "Create a checkout link",
-    description:
-      "Create pre-built checkout links or embedded checkouts to sell NFTs.",
-    image: require("public/assets/dashboard/home-payments.png"),
-    href: "https://withpaper.com/product/checkouts",
-    badge: "Payments",
-    badgeColor: "green.500",
-  },
-  {
-    title: "Run Engine",
-    description:
-      "Engine is a backend HTTP server that calls smart contracts with your managed backend wallets.",
-    badge: "Web3 Backend",
-    badgeColor: "gray.700",
-    image: require("public/assets/dashboard/home-infra.png"),
-    href: "/dashboard/engine",
-  },
-];
 
 const Dashboard: ThirdwebNextPage = (
   props: InferGetStaticPropsType<typeof getStaticProps>,
 ) => {
   const connectionStatus = useConnectionStatus();
-  const { colorMode } = useColorMode();
 
   const showFTUX =
     connectionStatus !== "connected" && connectionStatus !== "connecting";
@@ -93,28 +45,22 @@ const Dashboard: ThirdwebNextPage = (
 
                         return (
                           <Flex key={section} gap={4} flexDir="column">
-                            <Heading size="title.sm" textTransform="capitalize">
+                            <Heading
+                              size="title.sm"
+                              textTransform="capitalize"
+                              color="faded"
+                            >
                               {section}
                             </Heading>
-                            {products.map((product) => (
-                              <Card
-                                key={section}
-                                p={0}
-                                overflow="hidden"
-                                bgColor="backgroundCardHighlight"
-                              >
-                                <Flex gap={2}>
-                                  {product.icon && (
-                                    <ChakraNextImage
-                                      alt=""
-                                      boxSize={7}
-                                      src={product.icon}
-                                    />
-                                  )}
-                                  <Text>{product.name}</Text>
-                                </Flex>
-                              </Card>
-                            ))}
+                            <SimpleGrid columns={{ base: 1, md: 3 }} gap={4}>
+                              {products.map((product) => (
+                                <HomeProductCard
+                                  key={product.name}
+                                  product={product}
+                                  TRACKING_CATEGORY={TRACKING_CATEGORY}
+                                />
+                              ))}
+                            </SimpleGrid>
                           </Flex>
                         );
                       },
