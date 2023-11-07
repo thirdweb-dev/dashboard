144dd3ada41cc7d078315072975719bd82925e61
easyrun42 <declaimyy@gmail.com>
2023-11-07 21:56:58 +0000

    landing page changes

--- pages/solutions/chains.tsx
+++ pages/solutions/chains.tsx
@@ -1,4 +1,6 @@
-import { Container, Flex } from "@chakra-ui/react";
+import { Box, Center, Container, Flex } from "@chakra-ui/react";
+import { ChakraNextImage } from "components/Image";
+import { LandingCardWithImage } from "components/landing-pages/card-with-image";
 import LandingCardWithImageBackground from "components/landing-pages/card-with-image-background";
 import { LandingEndCTA } from "components/landing-pages/end-cta";
 import { LandingGridSection } from "components/landing-pages/grid-section";
@@ -7,37 +9,13 @@ import { LandingHeroWithSideImage } from "components/landing-pages/hero-with-sid
 import { LandingIconSectionItem } from "components/landing-pages/icon-section-item";
 import { LandingLayout } from "components/landing-pages/layout";
 import { LandingOptionSelector } from "components/landing-pages/option-selector";
+import { LandingSectionHeading } from "components/landing-pages/section-heading";
 import { getAbsoluteUrl } from "lib/vercel-utils";
 import { PageId } from "page-id";
 import { Card, Heading, Text, TrackedLinkButton } from "tw-components";
 import { ThirdwebNextPage } from "utils/types";
 
 const TRACKING_CATEGORY = "chains-landing";
-
-const SELECTOR_ITEMS = [
-  {
-    title: "For Chains-as-a-Service Providers",
-    description:
-      "Enhance your developers' experience with building on your chain by providing access to dev tools across: contracts, wallets, payments, and infrastructure. Empower your developers to build production-ready web3 apps and games, faster.",
-    steps: [
-      "Contact us to get your chain approved to be added to thirdweb global chainlist.",
-      "Once added, a shareable dynamic landing page will be generated for your chain.",
-      "Subnet developers can easily deploy contracts to their subnet in just a few clicks and access SDKs to integrate contracts into their apps",
-    ],
-    products: ["explore", "interact"],
-  },
-  {
-    title: "For Appchain Developers",
-    description:
-      "Start building production-ready web3 apps and games on top of your subnet with our dev tools across: contracts, wallets, payments, and infrastructure.",
-    steps: [
-      "After appchain developers create their subnet on an approved chains-as-a-service platform, a dynamic landing page is generated with SDKs, contract deployment and infrastructure for their subnet.",
-      "Appchain developer deploys contract specific to their appchain.",
-      "Appchain developer build web3 apps using thirdweb product suite: contracts, wallets, payments, infrastructure products.",
-    ],
-    products: ["explore", "interact"],
-  },
-];
 
 const SolutionsChains: ThirdwebNextPage = () => {
   return (
@@ -63,13 +41,13 @@ const SolutionsChains: ThirdwebNextPage = () => {
         maxW="container.page"
         as={Flex}
         flexDir="column"
-        gap={{ base: "80px", md: "120px" }}
+        gap={{ base: "60px", md: "180px" }}
       >
         <LandingHeroWithSideImage
-          miniTitle="Chains & Appchains"
-          title="Instant developer tools on your chain"
+          miniTitle="Chains"
+          title="Empower developers, on your chain"
           titleWithGradient=""
-          subtitle="The easiest way for developers to build on your EVM chain (or appchain) — with wallets, smart contracts, payments, and infrastructure for them to scale web3 apps."
+          subtitle="The easiest way for developers to build on your EVM chain — with wallets, contracts, payments, and infrastructure for them to scale web3 apps."
           trackingCategory={TRACKING_CATEGORY}
           ctaLink="/contact-us"
           noContactUs
@@ -79,74 +57,129 @@ const SolutionsChains: ThirdwebNextPage = () => {
           mobileImage={require("public/assets/product-pages/hero/mobile-hero-chains.png")}
         />
 
-        <LandingGridSection>
-          <LandingIconSectionItem
-            icon={require("public/assets/solutions-pages/icons/icon-build.svg")}
-            title="Flexible"
-            description="Our products are designed to be fully composable to give your developers flexibility in how they want to build. Use thirdweb managed infrastructure services or bring your own permissioned RPCs."
+        <LandingGridSection
+          title={
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              w="full"
+              marginBottom="38px"
+            >
+              <LandingSectionHeading
+                title="Everything developers need to build apps"
+                blackToWhiteTitle=""
+              />
+            </Box>
+          }
+          desktopColumns={4}
+        >
+          <LandingCardWithImage
+            title="Wallets"
+            description="Create backend wallets you can programatically use with automatic nonce and gas management. Eliminate gas spikes, stuck transactions and network instability."
+            image={require("public/assets/landingpage/account-abstraction-desktop.png")}
+            mobileImage={require("public/assets/landingpage/account-abstraction-mobile.png")}
+            TRACKING_CATEGORY={TRACKING_CATEGORY}
+            href="https://portal.thirdweb.com/wallet"
+            direction="horizontal"
           />
-          <LandingIconSectionItem
-            icon={require("public/assets/solutions-pages/icons/icon-ship.svg")}
-            title="Build faster"
-            description="Make it easier for developers to onboard onto your chain by providing an all-in-one developer tools, including: contracts, wallets, payments, and infrastructure."
+
+          <LandingCardWithImage
+            title="Smart Contracts"
+            description="Deploy, read, & write to any smart contract across any EVM-compatible blockchain — and build with thirdweb's audited smart contracts."
+            image={require("public/assets/landingpage/smart-contract-audits-desktop.png")}
+            mobileImage={require("public/assets/landingpage/smart-contract-audits-mobile.png")}
+            TRACKING_CATEGORY={TRACKING_CATEGORY}
+            href="/explore"
           />
-          <LandingIconSectionItem
-            icon={require("public/assets/solutions-pages/icons/icon-tool.svg")}
-            title="Developer-first experience"
-            description="Offer your developers most intuitive SDKs detect extensions in your contracts to handle common contract operations for developers. Extensions also unlock Dashboard UI functionality and data feeds."
+
+          <LandingCardWithImage
+            title="Gasless Transactions"
+            description="Onboard users in an instant & create seamless web3 UX by sponsoring gas fees — for any & all transactions."
+            image={require("public/assets/landingpage/transaction-fee-desktop.png")}
+            mobileImage={require("public/assets/landingpage/transaction-fee-mobile.png")}
+            TRACKING_CATEGORY={TRACKING_CATEGORY}
+            href="/sponsored-transactions"
+            colSpan={1}
+          />
+          <LandingCardWithImage
+            title="Infrastructure"
+            description="Built-in infrastructure so you don't have to worry about RPCs, storage, bundlers or paymasters."
+            image={require("public/assets/landingpage/infastructure-desktop.png")}
+            mobileImage={require("public/assets/landingpage/infastructure-mobile.png")}
+            TRACKING_CATEGORY={TRACKING_CATEGORY}
+            href="/rpc-edge"
+            direction="horizontal"
+          />
+          <LandingCardWithImage
+            title="Account Abstraction"
+            description="Create managed smart wallets with shared custody between the backend wallet & a user's EOA wallet."
+            image={require("public/assets/landingpage/account-desktop.png")}
+            mobileImage={require("public/assets/landingpage/account-mobile.png")}
+            TRACKING_CATEGORY={TRACKING_CATEGORY}
+            href="/account-abstraction"
+            direction="horizontal"
+            colSpan={1}
           />
         </LandingGridSection>
 
-        <LandingOptionSelector
-          items={SELECTOR_ITEMS}
-          TRACKING_CATEGORY={TRACKING_CATEGORY}
-          blackToWhiteTitle=""
-          title="What You Can Build"
-        />
-
-        <LandingGridSection desktopColumns={4}>
+        <LandingGridSection
+          desktopColumns={4}
+          title={
+            <Center>
+              <Heading fontSize={48}>
+                Your chain&nbsp;s application layer, solved
+              </Heading>
+            </Center>
+          }
+        >
           <Card p={8}>
             <LandingIconSectionItem
               icon={require("public/assets/solutions-pages/icons/icon-ship.svg")}
-              title="Deploy"
-              description="Offer your Deploy contracts directly to your appchain."
+              title="Launch Ecosystem-Ready"
+              description="Give developers a full web3 developer stack and production-grade infrastructure to build apps and games on your chain, from day one."
             />
           </Card>
           <Card p={8}>
             <LandingIconSectionItem
-              icon={require("public/assets/solutions-pages/icons/icon-contract.svg")}
-              title="Any Contracts"
-              description="Build your own contracts with Build or discover ready-to-deploy contracts in Explore."
+              icon={require("public/assets/solutions-pages/icons/accility.svg")}
+              title="Accelerate Activity"
+              description="Grow active wallets & transaction activity on your chain. Reduce time-to-market for developers with and accelerate your ecosystem."
             />
           </Card>
           <Card p={8}>
             <LandingIconSectionItem
-              icon={require("public/assets/solutions-pages/icons/icon-dashboard.svg")}
-              title="Fully managed infrastructure"
-              description="Fully managed infrastructure services in a single ecosystem that enables developers to build for scale."
+              icon={require("public/assets/solutions-pages/icons/support.svg")}
+              title="World-Class Support"
+              description="Give developers access to best-in-class learning resources and support from professional web3 developers."
             />
           </Card>
           <Card p={8}>
             <LandingIconSectionItem
-              icon={require("public/assets/solutions-pages/icons/icon-smart-wallet.svg")}
-              title="Flexible wallet experiences"
-              description="Onboard web2 and web3 users with fully customizable connect wallet components."
+              icon={require("public/assets/solutions-pages/icons/worldclass.svg")}
+              title="Innovation, Built-In"
+              description="Don't let developers fall behind: Give them first access to new standards, infrastructure, and tools before anyone else."
             />
           </Card>
         </LandingGridSection>
 
         <LandingCardWithImageBackground
-          image={require("public/assets/landingpage/coinbase-event.png")}
+          image={require("public/assets/landingpage/ava.png")}
         >
-          <Flex flexDir="column" gap="27px" maxWidth="600px">
+          <Flex flexDir="column" gap="16px" maxWidth="600px">
+            <ChakraNextImage
+              width={128}
+              height={38}
+              src={require("/public/assets/product-icons/avacloud_tw.svg")}
+              alt="avacloud"
+            />
             <Heading fontSize="xx-large" fontWeight="600" color="white">
-              Launch production-ready subnets with thirdweb and AvaCloud
+              Launch Production-Ready Subnets with thirdweb and AvaCloud
             </Heading>
             <Text fontSize="medium" fontWeight="400" color="white">
-              Thirdweb&apos;s integration into AvaCloud enables anyone to build
-              comprehensive apps with tools for Wallets, Payments, Contracts and
-              infrastructure, while also running customizable blockchains which
-              maintain their own security.
+              thirdweb&apos;s integration into AvaCloud empowers anyone to
+              deploy production-ready subnets — and empower developers to build
+              end-to-end apps on them, from day one.
             </Text>
             <TrackedLinkButton
               variant="outline"
@@ -159,9 +192,10 @@ const SolutionsChains: ThirdwebNextPage = () => {
               }}
               py={6}
               category={TRACKING_CATEGORY}
-              label="coinbase-case-study"
+              label="solutions-chain-case-study"
               href="https://blog.thirdweb.com/case-studies/coinbase-brings-onchain-experiences-to-life"
               maxW="fit-content"
+              marginTop="10px"
             >
               Learn More
             </TrackedLinkButton>
@@ -169,12 +203,12 @@ const SolutionsChains: ThirdwebNextPage = () => {
         </LandingCardWithImageBackground>
 
         <LandingEndCTA
-          title="Start building"
-          titleWithGradient="today."
+          title="Empower developers to grow your ecosystem"
+          titleWithGradient=""
           trackingCategory={TRACKING_CATEGORY}
-          noCta={true}
-          contactUsLink="https://docs.google.com/forms/d/e/1FAIpQLSfzwj6mr7mZjntv5C8HD9CfMXkIupuOqBSWa-XIWepJnnCEKg/viewform"
+          ctaLink="https://portal.thirdweb.com/gamingkit"
           gradient="linear(to-r, #F213A4, #F97CCE)"
+          noCta
         />
       </Container>
     </LandingLayout>
