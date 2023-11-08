c21ad41fd05211c1563063d03135fcba31ab32bc
easyrun42 <declaimyy@gmail.com>
2023-11-08 20:23:11 +0000

    prettier

--- components/homepage/sections/HeroSection.tsx
+++ components/homepage/sections/HeroSection.tsx
@@ -85,8 +85,9 @@ export const HeroSection = () => {
             textAlign={{ base: "center", lg: "left" }}
             maxW="500px"
           >
-            Onboard users with wallets, build & deploy smart contracts, accept fiat
-            with payments, and scale apps with infrastructure — on any EVM chain.
+            Onboard users with wallets, build & deploy smart contracts, accept
+            fiat with payments, and scale apps with infrastructure — on any EVM
+            chain.
           </Heading>
 
           <LightMode>
