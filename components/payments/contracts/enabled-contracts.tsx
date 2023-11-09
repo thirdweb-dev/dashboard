69b07aef8fc31850aebad12c8f3f937590341694
Nacho Iacovino <nacho.iacovino@gmail.com>
2023-11-09 21:18:12 +0100

    Get contract name for payment-enabled contracts

--- components/payments/contracts/enabled-contracts.tsx
+++ components/payments/contracts/enabled-contracts.tsx
@@ -1,10 +1,7 @@
-import { Center, Flex, LinkBox, LinkOverlay, Stack } from "@chakra-ui/react";
+import { Center, Flex, Stack } from "@chakra-ui/react";
 import { Card, Text, Heading } from "tw-components";
-import {
-  PaperChainToChainId,
-  usePaymentsEnabledContracts,
-} from "@3rdweb-sdk/react/hooks/usePayments";
-import { AddressCopyButton } from "tw-components/AddressCopyButton";
+import { usePaymentsEnabledContracts } from "@3rdweb-sdk/react/hooks/usePayments";
+import { PaymentEnabledCard } from "./payment-enabled-card";
 
 export const EnabledContracts = () => {
   const { data: paymentEnabledContracts } = usePaymentsEnabledContracts();
@@ -22,47 +19,8 @@ export const EnabledContracts = () => {
           </Center>
         </Card>
       ) : (
-        paymentEnabledContracts?.map((c) => (
-          <LinkBox key={c.id}>
-            <Card
-              p={6}
-              as={Flex}
-              flexDir="column"
-              gap={4}
-              transition="border-color 200ms ease, box-shadow 200ms ease, transform 200ms ease"
-              _hover={{
-                borderColor: "blue.500",
-                boxShadow: "0 0 16px hsl(215deg 100% 60% / 30%)",
-              }}
-            >
-              <Flex flexDir="column">
-                <Text color="bgBlack">
-                  <LinkOverlay
-                    href={`/${PaperChainToChainId[c.chain]}/${
-                      c.address
-                    }/payments`}
-                  >
-                    {c.display_name}
-                  </LinkOverlay>
-                </Text>
-                <Text>{c.chain}</Text>
-              </Flex>
-              <Flex gap={6}>
-                <Flex flexDir="column" gap={1}>
-                  <Text>Contract ID</Text>
-                  <AddressCopyButton
-                    address={c.id}
-                    size="xs"
-                    title="contract ID"
-                  />
-                </Flex>
-                <Flex flexDir="column" gap={1}>
-                  <Text>Contract Address</Text>
-                  <AddressCopyButton address={c.address} size="xs" />
-                </Flex>
-              </Flex>
-            </Card>
-          </LinkBox>
+        paymentEnabledContracts?.map((contract) => (
+          <PaymentEnabledCard key={contract.id} contract={contract} />
         ))
       )}
     </Flex>
