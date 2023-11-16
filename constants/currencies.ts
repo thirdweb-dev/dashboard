414cb679d168f04e4e750a97f724f4cce4a73b7f
gerrysaporito <gerrysaporito@gmail.com>
2023-11-16 09:30:02 +0900

    updated address

--- constants/currencies.ts
+++ constants/currencies.ts
@@ -63,7 +63,7 @@ const Polygon: CurrencyMetadata[] = [
   {
     address: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
     name: "USD Coin (Bridged)",
-    symbol: "USDCe",
+    symbol: "USDC.e",
   },
   {
     address: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
