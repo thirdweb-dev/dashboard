23c1db2caf73d3519eaa373ffd7e6e54bf6ec287
samina <57885104+saminacodes@users.noreply.github.com>
2023-12-20 14:52:13 -0700

    Update engine-relayer.tsx
    Signed-off-by: samina <57885104+saminacodes@users.noreply.github.com>

--- components/engine/relayer/engine-relayer.tsx
+++ components/engine/relayer/engine-relayer.tsx
@@ -19,7 +19,7 @@ export const EngineRelayer: React.FC<EngineRelayerProps> = ({
         <Heading size="title.md">Relayers</Heading>
         <Text>
           Use relayers to submit transactions from your backend wallets on
-          behalf of your users. 
+          behalf of your users.
           <Link
             href="https://portal.thirdweb.com/engine/relayer"
             color="primary.500"
