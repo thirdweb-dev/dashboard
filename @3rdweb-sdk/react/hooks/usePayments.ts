65e0e24645f67fc2e46a2d11213d0977692d5b4c
gerrysaporito <gerrysaporito@gmail.com>
2024-01-19 14:25:08 -0800

    lint

--- @3rdweb-sdk/react/hooks/usePayments.ts
+++ @3rdweb-sdk/react/hooks/usePayments.ts
@@ -211,7 +211,7 @@ const ChainSymbolToChainName: Record<string, string> = {
   MATIC: "Matic",
   AVAX: "Avalanche",
   AGOR: "Arbitrum Goerli Ether",
-  XAI: "XAI"
+  XAI: "XAI",
 };
 
 export const ChainIdToSupportedCurrencies: Record<number, CurrencyMetadata[]> =
@@ -423,16 +423,16 @@ export type CreateUpdateCheckoutInput = {
   brandDarkMode?: boolean;
   brandButtonShape?: "full" | "lg" | "none";
   brandColorScheme?:
-  | "gray"
-  | "red"
-  | "orange"
-  | "yellow"
-  | "green"
-  | "teal"
-  | "blue"
-  | "cyan"
-  | "purple"
-  | "pink";
+    | "gray"
+    | "red"
+    | "orange"
+    | "yellow"
+    | "green"
+    | "teal"
+    | "blue"
+    | "cyan"
+    | "purple"
+    | "pink";
   thirdwebClientId: string;
   checkoutId?: string;
 };
@@ -451,7 +451,8 @@ export function usePaymentsCreateUpdateCheckout(contractAddress: string) {
       return fetchFromPaymentsAPI<CreateUpdateCheckoutInput>(
         token,
         "POST",
-        `${apiDate}/shareable-checkout-link${input?.checkoutId ? `/${input.checkoutId} ` : ""
+        `${apiDate}/shareable-checkout-link${
+          input?.checkoutId ? `/${input.checkoutId} ` : ""
         }`,
         input,
       );
@@ -979,12 +980,12 @@ export function usePaymentsWebhooksById(paymentsSellerId: string) {
 
       return data && data?.webhook.length > 0
         ? (data.webhook.map((webhook) => ({
-          id: webhook.id,
-          sellerId: webhook.seller_id,
-          url: webhook.url,
-          isProduction: webhook.is_production,
-          createdAt: new Date(webhook.created_at),
-        })) as PaymentsWebhooksType[])
+            id: webhook.id,
+            sellerId: webhook.seller_id,
+            url: webhook.url,
+            isProduction: webhook.is_production,
+            createdAt: new Date(webhook.created_at),
+          })) as PaymentsWebhooksType[])
         : ([] as PaymentsWebhooksType[]);
     },
     { enabled: !!paymentsSellerId },
