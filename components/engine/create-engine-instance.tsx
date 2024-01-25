49f5514b60a4731d299263c0c32040f343536ef9
Phillip Ho <arcoraven@gmail.com>
2024-01-24 18:57:05 -0800

    add payment modal

--- components/engine/create-engine-instance.tsx
+++ components/engine/create-engine-instance.tsx
@@ -1,6 +1,6 @@
+import { AccountStatus, useAccount } from "@3rdweb-sdk/react/hooks/useApi";
 import {
   Flex,
-  VStack,
   Icon,
   Modal,
   ModalBody,
@@ -8,9 +8,13 @@ import {
   ModalContent,
   ModalHeader,
   ModalOverlay,
+  UseDisclosureReturn,
+  VStack,
   useDisclosure,
   useToast,
 } from "@chakra-ui/react";
+import { OnboardingBilling } from "components/onboarding/Billing";
+import { OnboardingModal } from "components/onboarding/Modal";
 import { THIRDWEB_API_HOST } from "constants/urls";
 import { useTrack } from "hooks/analytics/useTrack";
 import { FiPlus } from "react-icons/fi";
@@ -24,9 +28,66 @@ interface CreateEngineInstanceButtonProps {
 export const CreateEngineInstanceButton = ({
   refetch,
 }: CreateEngineInstanceButtonProps) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cloudHostedModalDisclosure = useDisclosure();
+  const paymentDisclosure = useDisclosure();
+  const trackEvent = useTrack();
+
+  return (
+    <>
+      <Button
+        onClick={() => {
+          trackEvent({
+            category: "engine",
+            action: "click",
+            label: "add-engine-instance",
+          });
+          cloudHostedModalDisclosure.onOpen();
+        }}
+        colorScheme="blue"
+        leftIcon={<Icon as={FiPlus} boxSize={4} />}
+      >
+        Create instance
+      </Button>
+
+      {cloudHostedModalDisclosure.isOpen && (
+        <RequestCloudHostedEngineModal
+          disclosure={cloudHostedModalDisclosure}
+          onConfirm={refetch}
+          onAddPaymentMethod={() => {
+            cloudHostedModalDisclosure.onClose();
+            paymentDisclosure.onOpen();
+          }}
+        />
+      )}
+
+      <OnboardingModal
+        isOpen={paymentDisclosure.isOpen}
+        onClose={paymentDisclosure.onClose}
+      >
+        <OnboardingBilling
+          onSave={paymentDisclosure.onClose}
+          onCancel={paymentDisclosure.onClose}
+        />
+      </OnboardingModal>
+    </>
+  );
+};
+
+const RequestCloudHostedEngineModal = ({
+  disclosure,
+  onConfirm,
+  onAddPaymentMethod,
+}: {
+  disclosure: UseDisclosureReturn;
+  onConfirm: () => void;
+  onAddPaymentMethod: () => void;
+}) => {
   const trackEvent = useTrack();
   const toast = useToast();
+  const accountQuery = useAccount();
+
+  const hasValidPayment =
+    accountQuery.data?.status === AccountStatus.ValidPayment;
 
   const onClickConfirm = async () => {
     trackEvent({
@@ -45,6 +106,13 @@ export const CreateEngineInstanceButton = ({
         body: JSON.stringify({}),
       });
       if (!res.ok) {
+        if (res.status === 409) {
+          toast({
+            status: "warning",
+            description: `You already requested a cloud-hosted Engine. Please contact sales@thirdweb.com if you have questions.`,
+          });
+          return;
+        }
         throw new Error(`Unexpected status ${res.status}`);
       }
 
@@ -52,7 +120,7 @@ export const CreateEngineInstanceButton = ({
         status: "success",
         description: "Thank you! Our team will reach out shortly.",
       });
-      refetch();
+      onConfirm();
     } catch (e) {
       toast({
         status: "error",
@@ -63,95 +131,91 @@ export const CreateEngineInstanceButton = ({
   };
 
   return (
-    <>
-      <Button
-        onClick={() => {
-          trackEvent({
-            category: "engine",
-            action: "click",
-            label: "add-engine-instance",
-          });
-          onOpen();
-        }}
-        colorScheme="blue"
-        leftIcon={<Icon as={FiPlus} boxSize={4} />}
-      >
-        Create Instance
-      </Button>
+    <Modal isOpen={disclosure.isOpen} onClose={disclosure.onClose} isCentered>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalCloseButton />
 
-      <Modal isOpen={isOpen} onClose={onClose} isCentered>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalCloseButton />
-
-          <ModalHeader>Create Engine Instance</ModalHeader>
-          <ModalBody>
-            <Card
-              w="full"
-              as={Flex}
-              gap={10}
-              flexDir="column"
-              p={{ base: 6, md: 10 }}
-              h="full"
-              borderColor="gray.800"
-              mb={4}
-            >
-              <Flex flexDir="column" gap={6}>
-                <Flex flexDir="column" gap={3}>
-                  <Flex gap={2}>
-                    <Heading as="h3" size="title.md" textTransform="capitalize">
-                      Cloud-Hosted Engine
-                    </Heading>
-                  </Flex>
-                  <Text maxW={320}>Host Engine on thirdweb with no setup.</Text>
-                </Flex>
-                <Flex alignItems="flex-end" gap={2}>
-                  <Heading size="title.md" lineHeight={1}>
-                    $99
+        <ModalHeader>Create Engine Instance</ModalHeader>
+        <ModalBody>
+          <Card
+            w="full"
+            as={Flex}
+            gap={10}
+            flexDir="column"
+            p={{ base: 6, md: 10 }}
+            h="full"
+            borderColor="gray.800"
+            mb={4}
+          >
+            <Flex flexDir="column" gap={6}>
+              <Flex flexDir="column" gap={3}>
+                <Flex gap={2}>
+                  <Heading as="h3" size="title.md" textTransform="capitalize">
+                    Cloud-Hosted Engine
                   </Heading>
-                  <Text size="body.lg">/ month</Text>
                 </Flex>
+                <Text maxW={320}>Host Engine on thirdweb with no setup.</Text>
               </Flex>
-              <Flex
-                flexDir="column"
-                gap={3}
-                grow={1}
-                alignItems="flex-start"
-                color="accent.900"
-              >
-                <Text color="accent.900" fontWeight="medium">
-                  Includes:
-                </Text>
-
-                {[
-                  "Isolated server & database",
-                  "On-call monitoring from thirdweb",
-                  "No long-term commitment",
-                ].map((f) => (
-                  <Flex key={f} gap={2}>
-                    <Icon as={IoCheckmarkCircle} boxSize={5} mt={0.5} />
-                    <Text>{f}</Text>
-                  </Flex>
-                ))}
+              <Flex alignItems="flex-end" gap={2}>
+                <Heading size="title.md" lineHeight={1}>
+                  $99
+                </Heading>
+                <Text size="body.lg">/ month</Text>
               </Flex>
+            </Flex>
+            <Flex
+              flexDir="column"
+              gap={3}
+              grow={1}
+              alignItems="flex-start"
+              color="accent.900"
+            >
+              <Text color="accent.900" fontWeight="medium">
+                Includes:
+              </Text>
 
-              <VStack>
+              {[
+                "Isolated server & database",
+                "On-call monitoring from thirdweb",
+                "No long-term commitment",
+              ].map((f) => (
+                <Flex key={f} gap={2}>
+                  <Icon as={IoCheckmarkCircle} boxSize={5} mt={0.5} />
+                  <Text>{f}</Text>
+                </Flex>
+              ))}
+            </Flex>
+
+            <VStack>
+              {hasValidPayment ? (
+                <>
+                  <Button
+                    onClick={onClickConfirm}
+                    colorScheme="blue"
+                    py={6}
+                    w="full"
+                  >
+                    Confirm
+                  </Button>
+                  <Text size="body.sm">
+                    We will reach out within 1 business day.
+                  </Text>
+                </>
+              ) : (
                 <Button
-                  onClick={onClickConfirm}
+                  onClick={onAddPaymentMethod}
                   colorScheme="blue"
                   py={6}
                   w="full"
                 >
-                  Confirm
+                  Add payment method
                 </Button>
-                <Text size="body.sm">
-                  We will reach out within 1 business day.
-                </Text>
-              </VStack>
-            </Card>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
-    </>
+              )}
+            </VStack>
+          </Card>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
   );
 };
