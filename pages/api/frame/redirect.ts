1b868fca3880b7a169d49b555f517271c59dd1bf
easyrun42 <declaimyy@gmail.com>
2024-02-01 17:55:45 +0000

    add nextjs-cors

--- pages/api/frame/redirect.ts
+++ pages/api/frame/redirect.ts
@@ -1,9 +1,16 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import NextCors from "nextjs-cors";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
+  // eslint-disable-next-line new-cap
+  await NextCors(req, res, {
+    methods: ["POST"],
+    origin: "*",
+  });
+
   if (req.method !== "POST") {
     return res.status(400).json({ error: "invalid method" });
   }
