2c65b3cbc576e7bf34a3f95aa50345c1ac35469a
Yash <kumaryashcse@gmail.com>
2024-02-28 02:40:37 +0530

    sdk dev build 5660

--- components/contract-components/hooks.ts
+++ components/contract-components/hooks.ts
@@ -641,6 +641,7 @@ export function useCustomContractDeployMutation(
             zkSigner,
             StorageSingleton,
             chainId,
+            { compilerOptions: { compilerType: "zksolc" } },
           );
         } else {
           if (data.deployDeterministic) {
