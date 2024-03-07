b569e8bcfd6b525b20eaef079ac2d20aaaff6229
Nacho Iacovino <nacho.iacovino@gmail.com>
2024-03-07 01:30:40 +0100

    ??

--- components/onboarding/ClaimCreditsModal.tsx
+++ components/onboarding/ClaimCreditsModal.tsx
@@ -151,7 +151,9 @@ export const ClaimCreditsModal: React.FC<ClaimCreditsModalProps> = ({
                       <UnorderedList>
                         <Text as={ListItem}>10k monthly active wallets</Text>
                         <Text as={ListItem}>User analytics</Text>
-                        <Text as={ListItem}>Custom Auth and Custom Branding</Text>
+                        <Text as={ListItem}>
+                          Custom Auth and Custom Branding
+                        </Text>
                       </UnorderedList>
                     </Flex>
                     <LinkButton
