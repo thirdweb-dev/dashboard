84a1f6ec8148078e4ce9439409c1149c4274b260
Nacho Iacovino <nacho.iacovino@gmail.com>
2024-03-08 18:23:23 +0100

    kk

--- components/settings/Account/Billing/index.tsx
+++ components/settings/Account/Billing/index.tsx
@@ -20,7 +20,7 @@ import { OnboardingBilling } from "components/onboarding/Billing";
 import { OnboardingModal } from "components/onboarding/Modal";
 import { FiExternalLink } from "react-icons/fi";
 import { useLocalStorage } from "hooks/useLocalStorage";
-import { BillingCredits } from "./Credits";
+import { BillingCredits } from "./PlanCard";
 
 interface BillingProps {
   account: Account;
@@ -41,9 +41,9 @@ export const Billing: React.FC<BillingProps> = ({ account }) => {
   const [stepsCompleted, setStepsCompleted] = useState<
     | undefined
     | {
-        account: boolean;
-        payment: boolean;
-      }
+      account: boolean;
+      payment: boolean;
+    }
   >();
 
   const [downgradePlan, setDowngradePlan] = useState<AccountPlan | undefined>();
