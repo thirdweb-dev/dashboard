8c0416c90a2feb8c5d758a3deb48aa30778fb75e
Nacho Iacovino <nacho.iacovino@gmail.com>
2024-03-14 21:42:53 +0100

    Dont need this

--- pages/api/build-id.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-const handler = (_req: NextApiRequest, res: NextApiResponse): void => {
-  res.status(200).json({
-    buildId: process.env.BUILD_ID,
-  });
-};
-
-export default handler;
