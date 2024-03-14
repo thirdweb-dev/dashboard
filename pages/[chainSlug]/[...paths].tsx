0335012c743dd21b4106c4a078b511628ccecfb7
Nacho Iacovino <nacho.iacovino@gmail.com>
2024-03-14 22:57:47 +0100

    format

--- pages/[chainSlug]/[...paths].tsx
+++ pages/[chainSlug]/[...paths].tsx
@@ -282,11 +282,13 @@ const ContractPage: ThirdwebNextPage = () => {
 export default ContractPage;
 ContractPage.pageId = PageId.DeployedContract;
 ContractPage.getLayout = (page, props: EVMContractProps) => {
-  const displayName = `${props.contractMetadata?.name ||
+  const displayName = `${
+    props.contractMetadata?.name ||
     shortenIfAddress(props.contractInfo?.contractAddress) ||
     "Contract"
-    }${props.contractMetadata?.symbol ? ` (${props.contractMetadata.symbol})` : ""
-    }`;
+  }${
+    props.contractMetadata?.symbol ? ` (${props.contractMetadata.symbol})` : ""
+  }`;
 
   const ogImage = ContractOG.toUrl({
     displayName: props.contractMetadata?.name || "",
@@ -300,8 +302,9 @@ ContractPage.getLayout = (page, props: EVMContractProps) => {
     .replace("Testnet", "")
     .trim();
   const url = `${THIRDWEB_DOMAIN}/${props.contractInfo?.chainSlug}/${props.contractInfo?.contractAddress}/`;
-  const SEOTitle = `${displayName} | ${cleanedChainName ? `${cleanedChainName} ` : ""
-    }Smart Contract`;
+  const SEOTitle = `${displayName} | ${
+    cleanedChainName ? `${cleanedChainName} ` : ""
+  }Smart Contract`;
 
   let SEOdescription = "";
 
@@ -310,14 +313,17 @@ ContractPage.getLayout = (page, props: EVMContractProps) => {
     props.detectedExtension === "erc721" ||
     props.detectedExtension === "erc1155"
   ) {
-    SEOdescription = `View tokens, source code, transactions, balances, and analytics for the ${displayName} smart contract${cleanedChainName ? ` on ${cleanedChainName}` : ""
-      }.`;
+    SEOdescription = `View tokens, source code, transactions, balances, and analytics for the ${displayName} smart contract${
+      cleanedChainName ? ` on ${cleanedChainName}` : ""
+    }.`;
   } else if (props.detectedExtension === "erc20") {
-    SEOdescription = `View ERC20 tokens, transactions, balances, source code, and analytics for the ${displayName} smart contract${cleanedChainName ? ` on ${cleanedChainName}` : ""
-      }.`;
+    SEOdescription = `View ERC20 tokens, transactions, balances, source code, and analytics for the ${displayName} smart contract${
+      cleanedChainName ? ` on ${cleanedChainName}` : ""
+    }.`;
   } else {
-    SEOdescription = `View tokens, transactions, balances, source code, and analytics for the ${displayName} smart contract${cleanedChainName ? ` on ${cleanedChainName}` : ""
-      }.`;
+    SEOdescription = `View tokens, transactions, balances, source code, and analytics for the ${displayName} smart contract${
+      cleanedChainName ? ` on ${cleanedChainName}` : ""
+    }.`;
   }
 
   return (
@@ -339,13 +345,13 @@ ContractPage.getLayout = (page, props: EVMContractProps) => {
             description: SEOdescription,
             images: ogImage
               ? [
-                {
-                  url: ogImage.toString(),
-                  alt: ``,
-                  width: 1200,
-                  height: 630,
-                },
-              ]
+                  {
+                    url: ogImage.toString(),
+                    alt: ``,
+                    width: 1200,
+                    height: 630,
+                  },
+                ]
               : undefined,
             url,
           }}
