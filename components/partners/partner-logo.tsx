c2441142d4a504e4e73735740d77081cba20e830
Juan Leal <114959779+juandolealt@users.noreply.github.com>
2024-03-27 07:52:00 -0400

    fix
    Signed-off-by: Juan Leal <114959779+juandolealt@users.noreply.github.com>

--- components/partners/partner-logo.tsx
+++ components/partners/partner-logo.tsx
@@ -105,6 +105,7 @@ const PARTNER_LOGO_MAP = {
   treasure: {
     img: require("./logos/treasure.png"),
     filter: undefined,
+  },
   xai: {
     img: require("./logos/xai.png"),
     filter: undefined,
