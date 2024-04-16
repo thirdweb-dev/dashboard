3c941f3ff1f356ed6997020c7e4ea9fbae9b2792
Manan Tank <manantankm@gmail.com>
2024-04-16 21:48:45 +0530

    use prettier

--- src/components/wallets/ConnectWalletPlayground/usePlaygroundWallets.tsx
+++ src/components/wallets/ConnectWalletPlayground/usePlaygroundWallets.tsx
@@ -1,7 +1,7 @@
 import {
-	type WalletConfig,
-	embeddedWallet,
-	smartWallet,
+  type WalletConfig,
+  embeddedWallet,
+  smartWallet,
 } from "@thirdweb-dev/react";
 import { useState } from "react";
 import { walletInfoRecord, type WalletId } from "./walletInfoRecord";
@@ -12,70 +12,70 @@ type WalletSelection = Record<WalletId, boolean | "recommended">;
 type AuthOption = "google" | "email" | "facebook" | "apple" | "phone";
 
 export function usePlaygroundWallets(defaultWalletSelection: WalletSelection) {
-	const [smartWalletOptions, setSmartWalletOptions] = useState({
-		factoryAddress: "0xa033aa4a35b9aa70fd3bbae95744110cee12d545",
-		enabled: false,
-		gasless: true,
-	});
+  const [smartWalletOptions, setSmartWalletOptions] = useState({
+    factoryAddress: "0xa033aa4a35b9aa70fd3bbae95744110cee12d545",
+    enabled: false,
+    gasless: true,
+  });
 
-	const [socialOptions, setSocialOptions] = useState<AuthOption[]>([
-		"email",
-		"google",
-		"apple",
-		"facebook",
-		"phone",
-	]);
+  const [socialOptions, setSocialOptions] = useState<AuthOption[]>([
+    "email",
+    "google",
+    "apple",
+    "facebook",
+    "phone",
+  ]);
 
-	const [walletSelection, setWalletSelection] = useState<
-		Record<WalletId, boolean | "recommended">
-	>(defaultWalletSelection);
+  const [walletSelection, setWalletSelection] = useState<
+    Record<WalletId, boolean | "recommended">
+  >(defaultWalletSelection);
 
-	const enabledWallets = Object.entries(walletSelection)
-		.filter((x) => x[1])
-		.map((x) => x[0] as WalletId);
+  const enabledWallets = Object.entries(walletSelection)
+    .filter((x) => x[1])
+    .map((x) => x[0] as WalletId);
 
-	const supportedWallets: WalletConfig<any>[] = enabledWallets.map(
-		(walletId) => {
-			if (walletId === "Email Wallet") {
-				walletInfoRecord[walletId].component = embeddedWallet({
-					recommended: !!walletSelection[walletId],
-					auth: {
-						options: socialOptions,
-					},
-				});
-			}
+  const supportedWallets: WalletConfig<any>[] = enabledWallets.map(
+    (walletId) => {
+      if (walletId === "Email Wallet") {
+        walletInfoRecord[walletId].component = embeddedWallet({
+          recommended: !!walletSelection[walletId],
+          auth: {
+            options: socialOptions,
+          },
+        });
+      }
 
-			// set recommended
-			walletInfoRecord[walletId].component.recommended =
-				walletSelection[walletId] === "recommended";
+      // set recommended
+      walletInfoRecord[walletId].component.recommended =
+        walletSelection[walletId] === "recommended";
 
-			// wrap with account abstraction
-			const walletConfig = walletInfoRecord[walletId].component;
+      // wrap with account abstraction
+      const walletConfig = walletInfoRecord[walletId].component;
 
-			return smartWalletOptions.enabled
-				? smartWallet(walletConfig, {
-						factoryAddress: smartWalletOptions.factoryAddress,
-						gasless: smartWalletOptions.gasless,
-						bundlerUrl: isProd
-							? "https://11155111.bundler.thirdweb.com"
-							: "https://11155111.bundler.thirdweb-dev.com",
-						// eslint-disable-next-line inclusive-language/use-inclusive-words
-						paymasterUrl: isProd
-							? "https://11155111.bundler.thirdweb.com"
-							: "https://11155111.bundler.thirdweb-dev.com",
-					})
-				: walletConfig;
-		},
-	);
+      return smartWalletOptions.enabled
+        ? smartWallet(walletConfig, {
+            factoryAddress: smartWalletOptions.factoryAddress,
+            gasless: smartWalletOptions.gasless,
+            bundlerUrl: isProd
+              ? "https://11155111.bundler.thirdweb.com"
+              : "https://11155111.bundler.thirdweb-dev.com",
+            // eslint-disable-next-line inclusive-language/use-inclusive-words
+            paymasterUrl: isProd
+              ? "https://11155111.bundler.thirdweb.com"
+              : "https://11155111.bundler.thirdweb-dev.com",
+          })
+        : walletConfig;
+    },
+  );
 
-	return {
-		walletSelection,
-		setWalletSelection,
-		enabledWallets,
-		supportedWallets,
-		smartWalletOptions,
-		setSmartWalletOptions,
-		socialOptions,
-		setSocialOptions,
-	};
+  return {
+    walletSelection,
+    setWalletSelection,
+    enabledWallets,
+    supportedWallets,
+    smartWalletOptions,
+    setSmartWalletOptions,
+    socialOptions,
+    setSocialOptions,
+  };
 }
