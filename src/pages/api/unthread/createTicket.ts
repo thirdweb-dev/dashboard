09966efd89a7de191e85b78efa2c39708ecbe1ff
Kien Ngo <github@kien.ngo>
2024-05-16 00:05:10 -0400

    Remove unwanted file

--- src/pages/api/unthread/createTicket.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { errorResponse } from "utils/api";
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== "POST") return res.status(404).end();
-  
-
-}
