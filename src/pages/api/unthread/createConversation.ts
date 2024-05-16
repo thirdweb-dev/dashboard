aed000610b196ea5a3f40a5a82bed029507e2049
Kien Ngo <github@kien.ngo>
2024-05-16 09:46:25 -0400

    Update

--- src/pages/api/unthread/createConversation.ts
+++ src/pages/api/unthread/createConversation.ts
@@ -1,4 +1,5 @@
-import { CreateTicketInput } from "@3rdweb-sdk/react/hooks/useApi";
+import { Account, CreateTicketInput } from "@3rdweb-sdk/react/hooks/useApi";
+import { THIRDWEB_API_HOST } from "constants/urls";
 import { NextRequest, NextResponse } from "next/server";
 import { errorResponse } from "utils/api";
 
@@ -11,10 +12,31 @@ const handler = async (req: NextRequest) => {
     return errorResponse("Invalid method", 400);
   }
   try {
-    // need to add auth check here //
-    const email = "need-email-here";
-    const name = "need-account-name-here";
-    const customerId = "need-customer-id";
+    // const authRequest = await fetch(`${THIRDWEB_API_HOST}/v1/account/me`, {
+    //   method: "GET",
+    //   credentials: "include",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     cookie: req.cookies.toString(),
+    //   },
+    // });
+    // const authResponse = await authRequest.json();
+    // if (!authResponse.data) {
+    //   return errorResponse("Unable to authenticate", 400);
+    // }
+    // const planToCustomerId: Record<string, string> = {
+    //   free: process.env.UNTHREAD_FREE_TIER_ID!,
+    //   growth: process.env.UNTHREAD_GROWTH_TIER_ID!,
+    //   pro: process.env.UNTHREAD_PRO_TIER_ID!,
+    // };
+    // const account = authResponse.data as Account;
+    // const { email, name } = account;
+    // const customerId = planToCustomerId?.[account.plan] || undefined;
+
+    // todo: delete when auth is done
+    const email = "";
+    const name = "";
+    const customerId = "";
 
     const incomingFormData: FormData = await req.formData();
     const files = incomingFormData.getAll("files") as File[];
@@ -23,9 +45,13 @@ const handler = async (req: NextRequest) => {
       return errorResponse("Invalid message", 400);
     }
     const input = JSON.parse(metadata) as CreateTicketInput;
-    const messageContent = formatUnthreadMessage(input, email, name);
+    const markdown = formatUnthreadMessage(
+      input,
+      email ?? "N/A",
+      name ?? "N/A",
+    );
 
-    const emailTitle =
+    const title =
       input.product && input["extraInfo_Problem_Area"]
         ? `${input.product}: ${input.extraInfo_Problem_Area}`
         : `New ticket from ${name} (${email})`;
@@ -39,15 +65,15 @@ const handler = async (req: NextRequest) => {
         triageChannelId: process.env.UNTHREAD_TRIAGE_CHANNEL_ID,
         // Email support@thirdweb.com
         emailInboxId: process.env.UNTHREAD_EMAIL_INBOX_ID,
-        title: emailTitle,
-        markdown: messageContent,
+        title,
+        markdown,
         status: "open",
         type: "email",
         onBehalfOf: {
           email,
           name,
         },
-        customerId: customerId || undefined,
+        customerId: customerId,
       }),
     );
 
@@ -81,7 +107,7 @@ export default handler;
  *
  * Example of the final content:
  * -- begin --
- * # Email: user@gmail.com
+ * # Email: user@email.com
  * # Address: 0xC88...
  * # Product: Connect
  * # Problem Area: Connect wallet issue
