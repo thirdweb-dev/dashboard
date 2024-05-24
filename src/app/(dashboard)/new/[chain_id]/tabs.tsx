d5670407108d593d41cbfa5ca6a5288c8d0a5a45
Manan Tank <manantankm@gmail.com>
2024-05-25 04:35:25 +0530

    tab style adjustments

--- src/app/(dashboard)/new/[chain_id]/tabs.tsx
+++ src/app/(dashboard)/new/[chain_id]/tabs.tsx
@@ -40,28 +40,31 @@ export function ChainPageTabs(props: { chainSlug: string }) {
   const layoutSegment = useSelectedLayoutSegment() || "";
   return (
     <div className="relative">
-      <div className="overflow-x-auto">
-        <div className="flex">
+      <div className="overflow-x-auto pb-[4px]">
+        <div className="flex pb-[4px] relative">
           {tabs.map((tab) => {
+            const isActive = layoutSegment === tab.segment;
+
             return (
               <Button asChild key={tab.name} variant="ghost">
                 <Link
                   href={`/new/${props.chainSlug}/${tab.segment}`}
                   className={cn(
-                    "border-b-2 rounded-none hover:bg-transparent px-3 lg:px-4 font-medium text-sm lg:text-md",
-                    layoutSegment === tab.segment
-                      ? "border-primary "
-                      : "border-transparent opacity-50 hover:opacity-100",
+                    "rounded-lg hover:bg-secondary px-3 font-medium text-sm lg:text-md relative h-auto",
+                    !isActive && "opacity-50 hover:opacity-100",
                   )}
                 >
                   {tab.name}
+                  {isActive && (
+                    <div className="absolute bottom-[-8px] z-10  w-full h-[2px] bg-foreground rounded-lg"></div>
+                  )}
                 </Link>
               </Button>
             );
           })}
         </div>
       </div>
-      <div className="h-[2px] bg-foreground bottom-0 left-0 right-0 opacity-10 absolute"></div>
+      <div className="h-[1px] bg-border bottom-[4px] left-0 right-0"></div>
     </div>
   );
 }
