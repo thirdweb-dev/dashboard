5d9034bd28bc1bef63216f9992755ba1dffbbe5f
Jonas Daniels <jonas.daniels@outlook.com>
2024-06-07 13:59:21 -0700

    Add Xai chain details and banner to layout

---
+++ src/app/(dashboard)/(chain)/[chain_id]/temp-chain-details/xai/metadata.ts
@@ -0,0 +1,4 @@
+export const xaiMetadata = {
+  about:
+    "Xai provides a user experience tailored for traditional gamers, abstracting web3 interaction on the backend. Game developers benefit from larger contract limits, while enjoying fast and cost-effective transactions similar to other Arbitrum chains. Xai ensures the same level of security as Ethereum and proudly operates as a completely carbon-neutral platform.",
+};
