e491534229ddf32c3a82a68b7248f12dff437fd8
Manan Tank <manantankm@gmail.com>
2024-06-07 19:44:15 +0530

    Remove extra ThirdwebProvider

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,15 +1,14 @@
-import { ThirdwebProvider } from "thirdweb/react";
 import { DashboardHeader } from "../components/Header/DashboardHeader";
 import { DashboardFooter } from "../components/DashboardFooter";
 
 export default function DashboardLayout(props: { children: React.ReactNode }) {
   return (
-    <ThirdwebProvider>
+    <>
       <div className="flex flex-col h-full">
         <DashboardHeader />
         <main className="grow">{props.children}</main>
         <DashboardFooter />
       </div>
-    </ThirdwebProvider>
+    </>
   );
 }
